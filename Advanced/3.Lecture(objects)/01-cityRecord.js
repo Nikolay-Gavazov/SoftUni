61b5329d7385551c96aa6463fb3ab0c51ddd730b
Nikolay <nikeca1991@gmail.com>
2023-01-16 23:55:00 +0200

    .

--- Advanced/3.Lecture(objects)/01-cityRecord.js
+++ Advanced/3.Lecture(objects)/01-cityRecord.js
@@ -3,7 +3,7 @@ function cityRecord(name, population, treasury){
     let city = {}
     city.name = name;
     city.population = population;
-    city. treasury = treasury;
+    city.treasury = treasury;
 
     return city;
 
---
+++ Advanced/3.Lecture(objects)/05-assemblyLine.js
@@ -0,0 +1,19 @@
+const assemblyLine = createAssemblyLine();
+
+    const myCar = {
+    
+    make: 'Toyota',
+    
+    model: 'Avensis'
+    
+    };
+
+    assemblyLine.hasClima(myCar);
+
+    console.log(myCar.temp);
+    
+    myCar.tempSettings = 18;
+    
+    myCar.adjustTemp();
+    
+    console.log(myCar.temp);
