a88ecd2b8ab822b20ed0f984db49f08fcce08ccf
Nikolay <nikeca1991@gmail.com>
2023-01-16 20:32:22 +0200

    .

--- Advanced/2.Homework/05-extractIncreasingSubsetFromArray.js
+++ Advanced/2.Homework/05-extractIncreasingSubsetFromArray.js
@@ -1,16 +1,14 @@
 function extractIncreasingSubsetFromArray(array) {
     let biggest = array.shift();
-    let result = [];
-    result.push(biggest);
+    let result = [biggest];
 
     array.forEach(el => {
-        let currentNum = el;
-        if (currentNum >= biggest) {
-            result.push(currentNum);
-            biggest = currentNum;
+        if (el >= biggest) {
+            result.push(el);
+            biggest = el;
         }
     });
-
+    
     return result;
 }
-extractIncreasingSubsetFromArray([1,3,8,4,10, 12, 3, 2, 24])
+console.log(extractIncreasingSubsetFromArray([1,3,8,4,10, 12, 3, 2, 24]));
