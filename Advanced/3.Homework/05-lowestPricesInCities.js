f846bd24c08770e98c173a0a27ea61a13581c70a
Nikolay <nikeca1991@gmail.com>
2023-01-20 01:26:35 +0200

    .

--- Advanced/3.Homework/05-lowestPricesInCities.js
+++ Advanced/3.Homework/05-lowestPricesInCities.js
@@ -1,17 +1,21 @@
 function lowestPricesInCities(arr) {
 
     const obj = [];
-    
+
     arr.forEach(el => {
         let [town, product, price] = el.split(' | ');
-        if(!obj[product]) {
-        obj[product] = {}
+        if (!obj[product]) {
+            obj[product] = {};
+        }
         obj[product][town] = Number(price);
-        }  
-        console.log(town, product, price);
     });
 
-    
+    let result = [];
+    for (let key in obj) {
+        let sorted = Object.entries(obj[key]).sort((a, b) => a[1] - b[1]);
+        let [town, price] = sorted[0];
+        console.log(`${key} -> ${price} (${town})`);
+    }
 
 }
 lowestPricesInCities(['Sample Town | Sample Product | 1000',
@@ -24,6 +28,6 @@ lowestPricesInCities(['Sample Town | Sample Product | 1000',
 
     'Sofia | Peach | 2',
 
-    'New York | Sample Product | 1000.1',
+    'New York | Sample Product | 9001',
 
     'New York | Burger | 10'])
