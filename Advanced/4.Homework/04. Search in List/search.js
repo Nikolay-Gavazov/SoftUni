5666ec8ee861ecf9ff3b622c412a2761fea22a58
Nikolay <nikeca1991@gmail.com>
2023-01-25 23:43:58 +0200

    .

--- Advanced/4.Homework/04. Search in List/search.js
+++ Advanced/4.Homework/04. Search in List/search.js
@@ -1,4 +1,21 @@
 function search() {
-   const towns = Array.from(document.querySelectorAll('#towns li'));
-   console.log(towns[0].innerText);
+   const el = Array.from(document.querySelectorAll('#towns li'));
+   const input = document.getElementById('searchText').value;
+   const result = document.getElementById('result');
+   let count = 0;
+   el.forEach(el => {
+      console.log(el.textContent);
+      if (el.textContent.includes(input)) {
+         el.style.fontWeight = 'bold';
+         el.style.textDecoration = 'underline';
+         count++;
+      } else {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = 'none';
+      }
+
+   });
+
+   result.innerText = `${count} matches found`;
+
 }
