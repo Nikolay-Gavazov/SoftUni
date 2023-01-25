f7d046a064bc17e0a56196ab55b5829e50467344
Nikolay <nikeca1991@gmail.com>
2023-01-25 23:58:08 +0200

    .

--- Advanced/4.Homework/05. Table - Search Engine/solution.js
+++ Advanced/4.Homework/05. Table - Search Engine/solution.js
@@ -1,8 +1,12 @@
 function solve() {
+   const input = document.getElementById('searchField');
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      Array.from(document.querySelectorAll('tbody tr')).forEach(el => {
+         if(el.textContent.includes(input.value)){
+            console.log(el.textContent);
+         }
+      })
    }
 }
