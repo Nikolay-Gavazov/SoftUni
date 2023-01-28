41ca13b24049635b37f3cdcdbd460da0dae345e8
Nikolay <nikeca1991@gmail.com>
2023-01-28 13:57:46 +0200

    .

--- Advanced/4.Homework/08. Generate Report/report.js
+++ Advanced/4.Homework/08. Generate Report/report.js
@@ -1,4 +1,18 @@
 function generateReport() {
-    const result = document.getElementById('output');
-    
+    const output = document.getElementById('output');
+
+    const resultArr = [];
+    const checks = Array.from(document.querySelectorAll('thead tr th input'));
+    const trs = Array.from(document.querySelectorAll('tbody tr'));
+
+    trs.forEach(el => {
+        const current = {};
+        Array.from(el.querySelectorAll('td')).forEach((x, i) => {
+            if (checks[i].checked) {
+                current[checks[i].name] = x.textContent;
+            }
+        })
+        resultArr.push(current);
+    });
+    output.value = JSON.stringify(resultArr)
 }
