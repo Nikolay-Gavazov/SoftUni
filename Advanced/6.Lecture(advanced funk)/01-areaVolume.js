b323ee3a0e019a981f22759c8f9c43217e9bd59f
Nikolay <nikeca1991@gmail.com>
2023-01-30 23:36:24 +0200

    .

---
+++ Advanced/6.Lecture(advanced funk)/01-areaVolume.js
@@ -0,0 +1,27 @@
+function solve(area, vol, input){
+    let inputObj = JSON.parse(input);
+
+    function calc(obj){
+        const areaObj = Math.abs(area.call(obj));
+        const volumeObj = Math.abs(vol.call(obj));
+
+        return {area: areaObj, volume: volumeObj};
+    }
+    return inputObj.map(calc)
+}
+console.log(solve(area, vol, `[
+
+    {"x":"1","y":"2","z":"10"},
+    
+    {"x":"7","y":"7","z":"10"},
+    
+    {"x":"5","y":"2","z":"10"}
+    
+    ]`))
+
+    function area(){
+        return Math.abs(this.x * this.y);
+    }
+    function vol(){
+        return Math.abs(this.x * this.y * this.z);
+    }
---
+++ Advanced/6.Lecture(advanced funk)/02-add.js
@@ -0,0 +1,10 @@
+function solution(a) {
+    return function add5(b) {
+        return a + b
+    }
+}
+let add5 = solution(5);
+
+console.log(add5(2));
+
+console.log(add5(3));
