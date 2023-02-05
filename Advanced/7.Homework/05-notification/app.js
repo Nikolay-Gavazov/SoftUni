6a819e0866b5d1317aedf64c6b151f822412e516
Nikolay <nikeca1991@gmail.com>
2023-02-06 01:06:14 +0200

    .

---
+++ Advanced/7.Homework/05-notification/app.js
@@ -0,0 +1,8 @@
+function notify(message) {
+  const div = document.getElementById('notification');
+  div.style.display = 'block';
+  div.textContent = message;
+  div.addEventListener('click', ()=>{
+    div.style.display = 'none';
+  })
+}
