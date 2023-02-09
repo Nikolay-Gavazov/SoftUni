945dfe3559984eb6de62520dac8efd2b620d3d9a
Nikolay <nikeca1991@gmail.com>
2023-02-09 23:25:13 +0200

    .

---
+++ Advanced/8.Homework/05-lengthLimit.js
@@ -0,0 +1,42 @@
+class Stringer{
+    constructor(innerString, innerLength){
+        this.innerString = innerString;
+        this.innerLength = innerLength;
+    }
+    decrease(length){
+        this.innerLength -= length;
+        if(this.innerLength < 0){
+            this.innerLength = 0;
+        }
+    }
+    increase(length){
+        this.innerLength += length;
+    }
+
+    toString(){
+        if(this.innerLength == 0){
+            return '...';
+        }else if(this.innerLength < this.innerString.length){
+            return `${this.innerString.substring(0, this.innerLength)}...`;
+        }else{
+            return this.innerString;
+        }
+    }
+}
+
+
+let test = new Stringer("Test", 5);
+
+console.log(test.toString()); // Test
+
+test.decrease(3);
+
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+
+console.log(test.toString()); // ...
+
+test.increase(4);
+
+console.log(test.toString()); // Test
