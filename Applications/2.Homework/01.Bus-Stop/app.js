ca73661bbda5a43034c8d59be84409a647d249c5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-02-27 23:32:02 +0200

    .

--- Applications/2.Homework/01.Bus-Stop/app.js
+++ Applications/2.Homework/01.Bus-Stop/app.js
@@ -1,3 +1,17 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById('stopId').value;
+    const submitBtn = document.getElementById('submit');
+    const buses = document.getElementById('buses');
+    const stopName = document.getElementById('stopName');
+
+
+    try {
+        const responce = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+        const data = await responce.json();
+        console.log(responce);
+        stopName.textContent = data.name;
+
+    } catch (error) {
+        
+    }
 }
