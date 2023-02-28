def3f06f60f67a0e51d91a0f021ed8145ab00878
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-02-28 11:32:53 +0200

    .

--- Applications/2.Homework/01.Bus-Stop/app.js
+++ Applications/2.Homework/01.Bus-Stop/app.js
@@ -1,16 +1,21 @@
 async function getInfo() {
     const stopId = document.getElementById('stopId').value;
-    const submitBtn = document.getElementById('submit');
     const buses = document.getElementById('buses');
     const stopName = document.getElementById('stopName');
-
+    buses.innerHTML = ''
 
     try {
         const responce = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
-        if(!responce.ok == false) throw Error;
+        if(responce.ok == false) throw Error;
+        
         const data = await responce.json();
-        console.log(data);
+        console.log(data.buses);
         stopName.textContent = data.name;
+        Object.entries(data.buses).forEach(([busId, time]) => {
+            const li = document.createElement('li');
+            li.innerHTML = `Bus ${busId} arrives in ${time} minutes`;
+            buses.appendChild(li);           
+        });
 
     } catch (error) {
         stopName.textContent = 'Error';
--- Applications/2.Homework/02.Bus-Schedule/app.js
+++ Applications/2.Homework/02.Bus-Schedule/app.js
@@ -1,11 +1,40 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const info = document.querySelector('.info');
+
+    let stopId = 'depot';
+    let stopName;
+
+   async function depart() {
+    try {
+        const responce = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`);
+        if(responce.ok == false) throw Error;
+        
+        const data = await responce.json();
+        stopName = data.name;
+        info.textContent = `Next stop ${stopName}`;
+        stopId = data.next
+        
+    } catch (error) {
+        info.textContent = 'Error';
+        departBtn.disabled = true;
+        arriveBtn.disabled = true;
+    }
+
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${stopName}`;
+
+
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
