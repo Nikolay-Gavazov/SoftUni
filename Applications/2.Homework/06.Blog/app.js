4b67a8304dafb7f849d234532b86a8a20d9fd4d1
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-02 11:38:01 +0200

    .

--- Applications/2.Homework/06.Blog/app.js
+++ Applications/2.Homework/06.Blog/app.js
@@ -5,10 +5,12 @@ function attachEvents() {
     const viewBtn = document.getElementById('btnViewPost');
     viewBtn.addEventListener('click', view);
     const options = document.getElementById('posts');
+    const h1 = document.getElementById('post-title');
     const p = document.getElementById('post-body');
     const ul = document.getElementById('post-comments');
-    const details = {}
-
+    
+    const details = {};
+    const title = {};
 
     async function load(){
         options.replaceChildren();
@@ -20,6 +22,7 @@ function attachEvents() {
             for(const el in data){
                 
                 details[el] = data[el].body
+                title[el] = data[el].title;
                 const option = document.createElement('option');
                 option.value = data[el].id;
                 option.textContent = data[el].title;
@@ -31,11 +34,15 @@ function attachEvents() {
     }
 
     async function view(){
+        h1.textContent = 'Post Details';
+        p.textContent = '';
+        ul.replaceChildren();
         try {
             const responce = await fetch(`http://localhost:3030/jsonstore/blog/comments/`)
             if(responce.ok == false) throw Error;
         
             const data = await responce.json();
+            h1.textContent = title[options.value];
             p.textContent = details[options.value];
             for(const el in data){
                 if(data[el].postId == options.value){
