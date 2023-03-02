512a6cd9e4c4004e4cd5990837df034860380d18
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-02 16:16:06 +0200

    .

--- Applications/3.Homework/01.Messenger/app.js
+++ Applications/3.Homework/01.Messenger/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const textArea = document.getElementById('messages');
+    const nameInput = document.getElementsByName('author')[0];
+    const messageInput = document.getElementsByName('content')[0];
+
+    document.getElementById('refresh').addEventListener('click', refresh);
+    document.getElementById('submit').addEventListener('click', submit);
+
+    let messages = [];
+    async function refresh(){
+        textArea.textContent = '';
+        try {
+            const res = await fetch('http://localhost:3030/jsonstore/messenger');
+            if(!res.ok) throw Error;
+
+            const data = await res.json();
+            
+            for(const el in data){
+                console.log(data[el]);
+                messages.push(`${data[el].author}: ${data[el].content}`);
+            }
+            textArea.textContent = messages.join('\n');
+            messages = [];
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    async function submit(){
+        try {
+            if(!nameInput.value || !messageInput.value){
+                throw new Error('all fields must be filled');
+            }
+            const data = {author: nameInput.value , content: messageInput.value};
+
+            const res = await fetch('http://localhost:3030/jsonstore/messenger', {
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(data)
+            })
+            
+            if(!res.ok) throw Error;
+
+            
+        } catch (error) {
+            alert(error.message);
+        }
+    }
 }
 
 attachEvents();
