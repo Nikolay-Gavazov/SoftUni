f2ce93887b83fa6e7aed487456020cdfd7c79d2e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-02 12:13:50 +0200

    .

--- Applications/2.Homework/06.Blog/app.js
+++ Applications/2.Homework/06.Blog/app.js
@@ -1,33 +1,27 @@
-
 function attachEvents() {
-    const loadBtn = document.getElementById('btnLoadPosts');
-    loadBtn.addEventListener('click', load);
-    const viewBtn = document.getElementById('btnViewPost');
-    viewBtn.addEventListener('click', view);
+    document.getElementById('btnLoadPosts').addEventListener('click', load);
+    document.getElementById('btnViewPost').addEventListener('click', view);
     const options = document.getElementById('posts');
     const h1 = document.getElementById('post-title');
     const p = document.getElementById('post-body');
     const ul = document.getElementById('post-comments');
-    
+
     const details = {};
     const title = {};
 
-    async function load(){
-        h1.textContent = 'Post Details';
-        p.textContent = '';
-        ul.replaceChildren();
-        options.replaceChildren();
+    async function load() {
         try {
             const responce = await fetch(' http://localhost:3030/jsonstore/blog/posts');
-            if(responce.ok == false) throw Error;
+            if (responce.ok == false) throw Error;
 
             const data = await responce.json();
-            for(const el in data){
-                
+            options.innerHTML = '';
+            for (const el in data) {
+
                 details[el] = data[el].body
                 title[el] = data[el].title;
                 const option = document.createElement('option');
-                option.value = data[el].id;
+                option.value = el;
                 option.textContent = data[el].title;
                 options.appendChild(option);
             }
@@ -36,32 +30,26 @@ function attachEvents() {
         }
     }
 
-    async function view(){
-        h1.textContent = 'Post Details';
-        p.textContent = '';
-        ul.replaceChildren();
+    async function view() {
         try {
             const responce = await fetch(`http://localhost:3030/jsonstore/blog/comments/`)
-            if(responce.ok == false) throw Error;
-        
+            if (responce.ok == false) throw Error;
+
             const data = await responce.json();
             h1.textContent = title[options.value];
-            p.textContent = details[options.value];
-            for(const el in data){
-                if(data[el].postId == options.value){
+            p.innerHTML = `${details[options.value]}`;
+            ul.innerHTML = '';
+            for (const el in data) {
+                if (data[el].postId == options.value) {
                     const li = document.createElement('li');
-                    li.id = data[el].id;
                     li.textContent = data[el].text;
                     ul.appendChild(li);
                 }
             }
-        options.innerHTML = '';
         } catch (error) {
             alert(error.massege)
         }
     }
-
-
 }
 
 attachEvents();
