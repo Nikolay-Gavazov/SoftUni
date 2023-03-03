8c81f8b44a39a1f9e42e34b1e25f376fe0064ce1
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-03 22:55:12 +0200

    .

--- Applications/3.Homework/05.Fisher-Game/src/register.js
+++ Applications/3.Homework/05.Fisher-Game/src/register.js
@@ -1,1 +1,20 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById('logout').style.display = 'none';
+const registerForm = document.querySelector('form');
+const data = new FormData(registerForm)
+const email = data.get('email');
+const pass = data.get('password');
+const repeat = data.get('rePass');
+const nofitication = registerForm.querySelector('.notification');
+const regBtn = registerForm.children[4];
+regBtn.addEventListener('click', register);
+
+
+async function register(e){
+    e.preventDefault();
+    try {
+        if(!email || !pass || !repeat) throw Error('All fields are requared!')
+    } catch (error) {
+        alert(error.message)
+    }
+}
+console.log(registerForm.children[0].value);
