5503419a7c8043a04dfab1bb160d2313076a7b2e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-06 17:11:58 +0200

    .

--- Applications/3.Homework/06.Furniture/homeLogged.js
+++ Applications/3.Homework/06.Furniture/homeLogged.js
@@ -1,15 +1,23 @@
 document.getElementById('logoutBtn').addEventListener('click', logout);
 window.addEventListener('load', loadProducts);
+
+const userData = JSON.parse(localStorage.getItem('userData'));
+
 const createForm = document.querySelector('form');
 createForm.action = 'none';
 createForm.addEventListener('submit', create);
 
 const productTbody = document.querySelector('.table > tbody');
 const buyBtn = document.querySelector('.col-md-12 > button');
+buyBtn.addEventListener('click', buy);
 
-buyBtn.addEventListener('click', buy)
-
-const userData = JSON.parse(localStorage.getItem('userData'));
+const orderDiv = document.querySelector('.orders');
+const boughtFurnitureP = orderDiv.querySelectorAll('p')[0];
+boughtFurnitureP.innerHTML = '';
+const totalPriceP = orderDiv.querySelectorAll('p')[1];
+totalPriceP.innerHTML = '';
+const orderBtn = orderDiv.querySelector('button');
+orderBtn.addEventListener('click', order);
 
 async function logout(e) {
     e.preventDefault()
@@ -51,6 +59,7 @@ async function create(e) {
             throw new Error(error.message)
         }
         createForm.reset();
+        loadProducts()
     } catch (error) {
         alert(error.message);
     }
@@ -67,7 +76,6 @@ async function loadProducts(e) {
 
         const data = await res.json();
         data.forEach(el => {
-            console.log(el);
             const tr = document.createElement('tr');
             tr.innerHTML = `
             <td>
@@ -87,8 +95,9 @@ async function loadProducts(e) {
             <input type="checkbox"/>
             </td>
             `;
-            
+            productTbody.appendChild(tr);
         });
+
     } catch (error) {
         alert(error.message);
     }
@@ -97,5 +106,64 @@ async function loadProducts(e) {
 
 async function buy(e) {
     e.preventDefault();
-    console.log(e.target);
+    const orders = {
+        boughtFurniture: [],
+        totalPrice: 0
+    }
+
+    const checkBoxes = Array.from(productTbody.querySelectorAll('input'));
+    
+    const checked = checkBoxes.filter(e => e.checked == true).forEach(el => {
+        const tr = el.parentElement.parentElement;
+        const [img, name, price, factor] = Array.from(tr.children);
+        orders.boughtFurniture.push(name.textContent.trim());
+        orders.totalPrice += Number(price.textContent);
+    });
+    try {
+        if(orders.boughtFurniture.length == 0) throw Error('You didnt check anithing!');
+        alert('Do you send the order?')
+        const res = await fetch('http://localhost:3030/data/orders', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json',
+                'X-Authorization': userData.token
+            },
+            body: JSON.stringify({
+                _ownerId: userData.id,
+                boughtFurniture: orders.boughtFurniture,
+                totalPrice: orders.totalPrice                
+            })
+        });
+        if(!res.ok){
+            const error = await res.json();
+            throw Error(error.message)
+        }
+
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+async function order(e){
+    e.preventDefault()
+    try {
+        const res = await fetch(`http://localhost:3030/data/orders?where=_ownerId%3D{${userData.id}}`)
+        if(!res.ok){
+            const error = await res.json();
+            throw Error(error.message)
+        }
+        
+        const data = await res.json()
+        console.log(data);
+    } catch (error) {
+        alert(error.message)
+    }
+    boughtFurnitureP.innerHTML = `
+    <p>Bought furniture: <span></span></p>
+    `;
+    totalPriceP.innerHTML = `
+    <p>Total price: <span>$</span></p>
+    `;
+    
 }
