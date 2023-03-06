c271936a8a45e564c8034ea4df838ca041bb410b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-06 11:58:56 +0200

    .

--- Applications/3.Homework/06.Furniture/homeLogged.js
+++ Applications/3.Homework/06.Furniture/homeLogged.js
@@ -1,5 +1,5 @@
 document.getElementById('logoutBtn').addEventListener('click', logout);
-
+window.addEventListener('load', loadProducts);
 const createForm = document.querySelector('form');
 createForm.action = 'none';
 createForm.addEventListener('submit', create);
@@ -11,10 +11,10 @@ buyBtn.addEventListener('click', buy)
 
 const userData = JSON.parse(localStorage.getItem('userData'));
 
-async function logout(e){
+async function logout(e) {
     e.preventDefault()
-    const res = await fetch('http://localhost:3030/users/logout',{
-        headers:{
+    const res = await fetch('http://localhost:3030/users/logout', {
+        headers: {
             'X-Authorization': userData.token
         }
     });
@@ -22,37 +22,80 @@ async function logout(e){
     window.location = ('./home.html');
 }
 
-async function create(e){
+async function create(e) {
     e.preventDefault()
     const createData = new FormData(e.target);
     const name = createData.get('name');
-    const price = createData.get('price');
-    const factor = createData.get('factor');
+    let price = createData.get('price');
+    let factor = createData.get('factor');
     const img = createData.get('img');
-
     try {
-        if(!name || ! price || ! factor || !img) throw new Error('All fields are requared!');
-        if(Number.isNaN(price))throw new Error('Price must be a Number!')
-        if(Number.isNaN(factor))throw new Error('Factor must be a Number!')
+        if (!name || !price || !factor || !img) throw new Error('All fields are requared!');
+        if (price != Number(price)) throw new Error('Price must be a Number!')
+        if (factor != Number(factor)) throw new Error('Factor must be a Number!')
         const res = await fetch('http://localhost:3030/data/furniture', {
             method: 'post',
-            headers:{'Content-type': 'application/json',
-            'X-Authorization': userData.token
-        },
-        body: JSON.stringify({
-            name,
-            price: Number(price),
-            factor: Number(factor),
-            img
+            headers: {
+                'Content-type': 'application/json',
+                'X-Authorization': userData.token
+            },
+            body: JSON.stringify({
+                name,
+                price: Number(price),
+                factor: Number(factor),
+                img
+            })
         })
-        })
-
+        if (!res.ok) {
+            const error = await res.json()
+            throw new Error(error.message)
+        }
+        createForm.reset();
     } catch (error) {
-       alert(error.message); 
+        alert(error.message);
     }
 }
 
-async function buy(e){
+async function loadProducts(e) {
+    productTbody.replaceChildren();
+    try {
+        const res = await fetch('http://localhost:3030/data/furniture');
+        if (!res.ok) {
+            const error = await res.json();
+            throw new Error(error.message);
+        }
+
+        const data = await res.json();
+        data.forEach(el => {
+            console.log(el);
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+            <td>
+            <img
+            src="${el.img}">
+            </td>
+            <td>
+            <p>${el.name}</p>
+            </td>
+            <td>
+            <p>${el.price}</p>
+            </td>
+            <td>
+            <p>${el.factor}</p>
+            </td>
+            <td>
+            <input type="checkbox"/>
+            </td>
+            `;
+            
+        });
+    } catch (error) {
+        alert(error.message);
+    }
+
+}
+
+async function buy(e) {
     e.preventDefault();
     console.log(e.target);
 }
