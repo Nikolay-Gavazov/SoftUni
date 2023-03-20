31f381ce61844c4c49bd1c3bfb286067e091d9a2
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-20 15:54:31 +0200

    .

--- Applications/6.Homework/05.Table-Search/solution.js
+++ Applications/6.Homework/05.Table-Search/solution.js
@@ -1,9 +1,7 @@
-window.addEventListener('load', solve)
-import { html } from "../../../node_modules/lit-html/lit-html.js";
+window.addEventListener('load', loadItems)
+import { html, render } from "../../../node_modules/lit-html/lit-html.js";
 import { getData } from "./src/api.js";
 
-function solve() {
-   loadItems()
    document.querySelector('#searchBtn').addEventListener('click', onClick);
    const input = document.getElementById('searchField');
    const tbody = document.querySelector('tbody');
@@ -11,21 +9,23 @@ function solve() {
    const template = (data) => html `
    ${data.map(el => html`
    <tr>
-                <td>${el.firstName} ${el.lastName}</td>
-                <td>${el.email}</td>
-                <td>${el.course}</td>
-            </tr>
+      <td>${el.firstName} ${el.lastName}</td>
+      <td>${el.email}</td>
+      <td>${el.course}</td>
+   </tr>
    `)}
    `
 
 
   async function loadItems(){
-      const items = await getData();
-
+      const items = await getData()
+      const result = template(Object.values(items));
+      render(result, tbody)
    }
 
 
    function onClick() {
+      console.log('c');
       Array.from(document.querySelectorAll('tbody tr')).forEach(el => {
          if (el.textContent.toLocaleLowerCase().includes(input.value.toLocaleLowerCase().trim())) {
             el.classList.add('select')
@@ -36,6 +36,3 @@ function solve() {
       input.value = '';
 
    }
-
-
-}
