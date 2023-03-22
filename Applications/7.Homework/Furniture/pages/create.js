f6100132ff334c10839f2e0017412fff51e715d8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-03-22 12:10:39 +0200

    .

--- Applications/7.Homework/Furniture/pages/create.js
+++ Applications/7.Homework/Furniture/pages/create.js
@@ -1,5 +1,5 @@
 import { html } from "../../../../node_modules/lit-html/lit-html.js";
-
+import { createFurniture } from "../src/data.js";
 const createTemplate = (onSubmit) => html`
     <div class="row space-top">
             <div class="col-md-12">
@@ -16,11 +16,11 @@ const createTemplate = (onSubmit) => html`
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-model">Model</label>
-                        <input class="form-control is-valid" id="new-model" type="text" name="model">
+                        <input class="form-control" id="new-model" type="text" name="model">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-year">Year</label>
-                        <input class="form-control is-invalid" id="new-year" type="number" name="year">
+                        <input class="form-control" id="new-year" type="number" name="year">
                     </div>
                     <div class="form-group">
                         <label class="form-control-label" for="new-description">Description</label>
@@ -50,7 +50,71 @@ export async function createPage(ctx){
     ctx.render(createTemplate(onSubmit));
 
     async function onSubmit(event){
-        event.preventDefault()
-        console.log(document.getElementById('new-year').value);
+        event.preventDefault();
+        const make = document.getElementById('new-make');
+        const model = document.getElementById('new-model');
+        const year = document.getElementById('new-year');
+        const description = document.getElementById('new-description');
+        const price = document.getElementById('new-price');
+        const image = document.getElementById('new-image');
+        const material = document.getElementById('new-material');
+
+        if(make.value.length < 4){
+            make.className = 'form-control is-invalid';
+            return alert('Make must be at least 4 symbols long');
+        }else{
+            make.className = 'form-control is-valid'
+        }
+
+        if(model.value.length < 4){
+            model.className = 'form-control is-invalid';
+            return alert('Model must be at least 4 symbols long');
+        }else{
+            model.className = 'form-control is-valid'
+        }
+
+        if(year.value < 1950 || year.value > 2050){
+            year.className = 'form-control is-invalid';
+            return alert('Year must be between 1950 and 2050');
+        }else{
+            year.className = 'form-control is-valid'
+        }
+
+        if(description.value < 10){
+            description.className = 'form-control is-invalid';
+            return alert('Description must be more than 10 symbols');
+        }else{
+            description.className = 'form-control is-valid';
+        }
+
+        if(price.value < 0){
+            price.className = 'form-control is-invalid';
+            return alert('Price must be a positive number');
+        }else{
+            price.className = 'form-control is-valid';
+        }
+
+        if(image.value == ''){
+            image.className = 'form-control is-invalid';
+            return alert('Image URL is required');
+        }else{
+            image.className = 'form-control is-valid';
+        }
+        console.log(year.value);
+        const data = {
+            make: make.value,
+            model: model.value, 
+            year: year.value, 
+            description: description.value, 
+            price: Number(price.value), 
+            img: image.value, 
+            material: material.value
+        }
+        try {
+            await createFurniture(data);
+            ctx.page.redirect('/');
+        } catch (error) {
+            alert(error.message);
+        }
     }
 }
