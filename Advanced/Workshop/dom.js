25586492cbba282a4ec1004ceb30e9b0e43dbded
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-04-05 14:53:23 +0300

    .

--- Advanced/Workshop/dom.js
+++ Advanced/Workshop/dom.js
@@ -1,5 +1,5 @@
 
-import { colors } from './cards.js';
+import { Foundation, Stock, Waste, colors } from './cards.js';
 
 const suits = {
     clubs: '&clubs;',
@@ -18,9 +18,10 @@ const faces = {
     7: '7',
     8: '8',
     9: '9',
-    10: 'J',
-    11: 'Q',
-    12: 'K',
+    10: '10',
+    11: 'J',
+    12: 'Q',
+    13: 'K'
 }
 
 /**
@@ -32,9 +33,18 @@ export function createDeckElement(deck){
     const element = document.createElement('article');
     element.className = 'deck';
 
-    for(let i = 0; i < deck.size; i++){
-        const card = deck.cards[i];
-        const top = i == deck.topIndex;
+    let cards = deck.cards;
+
+    if(deck.size > 1 && (deck instanceof Stock || deck instanceof Waste || deck instanceof Foundation)){
+        const visibleCount = Math.ceil((deck.size - 1) / 5);
+        cards = new Array(visibleCount);
+        cards.fill ({faceUp: false});
+        cards.push(deck.top);
+    }
+
+    for(let i = 0; i < cards.length; i++){
+        const card = cards[i];
+        const top = i == cards.length - 1;
         element.appendChild(createCard(card, top))
     }
     return element
@@ -51,7 +61,7 @@ function createCard(card, top){
 
     let content = '';
 
-    if(card.faceUp == true){
+    if(card.faceUp){
         element.classList.add(colors[card.suit])
         content = `${suits[card.suit]}${faces[card.face]}`
     }else{
