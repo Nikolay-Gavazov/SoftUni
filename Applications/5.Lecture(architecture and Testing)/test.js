7a37ad00c0e7ce0aa616ff478d3d134986d294ad
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-04-25 09:49:46 +0300

    .

---
+++ Applications/5.Lecture(architecture and Testing)/test.js
@@ -0,0 +1,19 @@
+const {chromium} = require('playwright-chromium');
+const {expect} = require('chai');
+
+let browser, page;
+
+describe('E2E tests', async function(){
+    this.timeout(10000);
+
+    before(async () => {browser = await chromium.launch(); });
+    after (async () => { await browser.close();});
+    beforeEach(async () => {page = await browser.newPage();});
+    afterEach(async () => {await page.close();});
+
+    it('make screenshot', async () => {
+        await page.goto('https://abv.bg');
+        await page.screenshot({path: 'test.png'});
+        expect(1).to.equal(1);
+    })
+})
