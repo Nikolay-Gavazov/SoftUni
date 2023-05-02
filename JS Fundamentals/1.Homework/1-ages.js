f5f625ec1d9925f83566d3941ef4ee63c07caa03
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-03 00:04:35 +0300

    .

--- JS Fundamentals/1.Homework/1-ages.js
+++ JS Fundamentals/1.Homework/1-ages.js
@@ -1,18 +1,18 @@
-function ages(input){
+function ages(input) {
 
     let ages = Number(input);
-    
-    if(ages >=0 && ages <=2){
+
+    if (ages >= 0 && ages <= 2) {
         console.log('baby');
-    }else if(ages > 2 && ages <= 13){
+    } else if (ages > 2 && ages <= 13) {
         console.log('child');
-    }else if(ages > 13 && ages <= 19){
+    } else if (ages > 13 && ages <= 19) {
         console.log('teenager');
-    }else if(ages > 19 && ages <= 65){
+    } else if (ages > 19 && ages <= 65) {
         console.log('adult');
-    }else if(ages >= 66){
+    } else if (ages >= 66) {
         console.log('elder');
-    }else{
+    } else {
         console.log('out of bounds');
     }
 
