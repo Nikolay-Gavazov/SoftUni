aa32e72465cc643ae5c53be2d81997f924cbe8b5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-08 15:21:37 +0300

    .

--- JS Fundamentals/1.Lecture/9-numbersFromNTo1.js
+++ JS Fundamentals/1.Lecture/9-numbersFromNTo1.js
@@ -1,7 +1,7 @@
-function numbersFromNTo1(num){
+function numbersFromNTo1(num) {
 
-    for(let i = num; i >= 1; i--){
+    for (let i = num; i >= 1; i--) {
         console.log(i);
     }
 
-}numbersFromNTo1(5)
+} numbersFromNTo1(5)
