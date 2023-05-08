b0ff581276037d29652253d7921febac93c56d1c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-08 15:22:29 +0300

    .

--- JS Fundamentals/1.MoreExercise/1-sortNumbers.js
+++ JS Fundamentals/1.MoreExercise/1-sortNumbers.js
@@ -1,16 +1,16 @@
-function sortNumbers(a, b, c){
+function sortNumbers(a, b, c) {
 
-    if(a >= b && b >= c){
-        [a, b, c] = [a, b , c];
-    }else if(a <= b && b <= c){
+    if (a >= b && b >= c) {
+        [a, b, c] = [a, b, c];
+    } else if (a <= b && b <= c) {
         [a, b, c] = [c, b, a];
-    }else if(a <= c && c <= b){
+    } else if (a <= c && c <= b) {
         [a, b, c] = [b, c, a];
-    }else if(a >= c && c >= b){
+    } else if (a >= c && c >= b) {
         [a, b, c] = [a, c, b];
-    }else if(c >= a && a >= b){
+    } else if (c >= a && a >= b) {
         [a, b, c] = [c, a, b]
-    }else if(b >= a && a >= c){
+    } else if (b >= a && a >= c) {
         [a, b, c] = [b, a, c]
     }
 
@@ -18,4 +18,4 @@ function sortNumbers(a, b, c){
     console.log(b);
     console.log(c);
 }
-sortNumbers(0,0,2)
+sortNumbers(0, 0, 2)
