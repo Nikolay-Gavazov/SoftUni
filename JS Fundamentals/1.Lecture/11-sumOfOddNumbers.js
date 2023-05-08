7adba02628262726ddfe80c2bff3b720a443ac11
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-08 15:22:08 +0300

    .

--- JS Fundamentals/1.Lecture/11-sumOfOddNumbers.js
+++ JS Fundamentals/1.Lecture/11-sumOfOddNumbers.js
@@ -1,13 +1,13 @@
-function sumOfOddNumbers(num){
+function sumOfOddNumbers(num) {
 
     let result = 1;
     let sum = 0;
 
-    for(let i = 1; i<=num; i++){
+    for (let i = 1; i <= num; i++) {
         console.log(result);
         sum += result;
-        result +=2;
-        
+        result += 2;
+
     }
 
     console.log(`Sum: ${sum}`);
