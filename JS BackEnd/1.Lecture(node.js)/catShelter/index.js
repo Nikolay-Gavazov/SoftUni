b1d988913250787a66d8bbcdd542d4332816f13c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-11 13:18:39 +0300

    .

--- JS BackEnd/1.Lecture(node.js)/catShelter/index.js
+++ JS BackEnd/1.Lecture(node.js)/catShelter/index.js
@@ -1,9 +1,21 @@
 const http = require('http');
-const homePage = require('./views/home/index')
+const homePage = require('./views/home/index');
+const style = require('./content/styles/site')
 
 
 const server = http.createServer(async (req, res) => {
-    res.write(homePage);
+    const url = req.url;
+
+    if(url == '/'){
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(homePage);
+    }else if(url == '/styles/site.css'){
+        res.write(style);
+    }else if(url == '/cats/add-breed'){
+
+    }
 
     res.end();
 });
