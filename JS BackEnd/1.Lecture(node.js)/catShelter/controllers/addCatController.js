f6de7d8d34f4a7a81e26b09eea4806e776a6f91e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-14 00:40:03 +0300

    .

--- JS BackEnd/1.Lecture(node.js)/catShelter/controllers/addCatController.js
+++ JS BackEnd/1.Lecture(node.js)/catShelter/controllers/addCatController.js
@@ -11,7 +11,14 @@ function addCatController(req, res){
 function createCat(req, res){
     const form = new IncomingForm();
     form.parse(req, (err, fields) =>{
-        cats.push(fields);
+        const id = 'xxxxxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
+        cats.push({
+            id,
+            name: fields.name,
+            description: fields.description,
+            img: fields.img,
+            breed: fields.breed
+        });
     });
 
     res.writeHead(301, {
