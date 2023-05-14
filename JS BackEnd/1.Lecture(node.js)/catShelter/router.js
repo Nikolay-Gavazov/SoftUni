2179443f03ab173444adf4103b917470aa62ec59
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-15 01:00:22 +0300

    .

--- JS BackEnd/1.Lecture(node.js)/catShelter/router.js
+++ JS BackEnd/1.Lecture(node.js)/catShelter/router.js
@@ -1,44 +1,44 @@
 const routes = {};
 
-function main(req, res){
+function main(req, res) {
 
     const url = new URL(req.url, `http://${req.headers.host}`);
 
     let handler;
     const actions = routes[url.pathname];
 
-    if(actions){
+    if (actions) {
         handler = actions[req.method];
     }
 
-    if(typeof handler == 'function'){
+    if (typeof handler == 'function') {
         handler(req, res);
-    }else{
+    } else {
         defaultController(req, res)
     }
 }
 
-function register(method, pathname, handler){
-    if(routes[pathname] == undefined){
+function register(method, pathname, handler) {
+    if (routes[pathname] == undefined) {
         routes[pathname] = {}
     }
     routes[pathname][method] = handler;
 }
 
-function get(pathname, handler){
+function get(pathname, handler) {
     register('GET', pathname, handler)
 }
 
-function post(pathname, handler){
+function post(pathname, handler) {
     register('POST', pathname, handler)
 }
 
 
-function defaultController(req, res){
-    
+function defaultController(req, res) {
+
     res.statusCode = 404;
-        res.write('<h2>Not found</h2>');
-        res.end();
+    res.write('<h2>Not found</h2>');
+    res.end();
 }
 
 module.exports = {
