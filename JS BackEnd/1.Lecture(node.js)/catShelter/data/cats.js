feba5526f032d954ef5f39c6acb0eb44de2d6544
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-14 22:17:00 +0300

    .

--- JS BackEnd/1.Lecture(node.js)/catShelter/data/cats.js
+++ JS BackEnd/1.Lecture(node.js)/catShelter/data/cats.js
@@ -1,6 +1,5 @@
-module.exports = [
-    {
-        id: 1,
+module.exports = {
+   '12x43z01': {
         name: 'Sharo',
         description: 'Dominant and aggressive to other cats.',
         img: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg',
@@ -27,4 +26,4 @@ module.exports = [
         img: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
         breed: 'Angore Cat'
     }
-]
+}
