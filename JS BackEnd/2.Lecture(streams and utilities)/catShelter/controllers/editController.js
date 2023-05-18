dc65dab9688789c19457416bee3355c51fa6afd0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-18 22:51:02 +0300

    Update editController.js

--- JS BackEnd/2.Lecture(streams and utilities)/catShelter/controllers/editController.js
+++ JS BackEnd/2.Lecture(streams and utilities)/catShelter/controllers/editController.js
@@ -4,18 +4,18 @@ const { loadFragment, render } = require('../view');
 async function editController(req, res) {
     const cat = await getItem(getParams(req, 'id'), 'cats');
     const breeds = await getData('breeds');
-    
+
     loadFragment('editCat', fragment => {
         const html = fragment
-        .replace('{{{id}}}', cat._id)
-        .replace('{{{name}}}', cat.name)
-        .replace('{{{description}}}', cat.description)
-        .replace('{{{img}}}', cat.img)
-        .replace(
-            '{{{breeds}}}',
-            breeds.map(el => `
+            .replace('{{{id}}}', cat._id)
+            .replace('{{{name}}}', cat.name)
+            .replace('{{{description}}}', cat.description)
+            .replace('{{{img}}}', cat.img)
+            .replace(
+                '{{{breeds}}}',
+                breeds.map(el => `
             <option value="${el.breed}">${el.breed}</option>`).join('\n'))
-            res.html(render(html));  
+        res.html(render(html));
     });
 }
 
