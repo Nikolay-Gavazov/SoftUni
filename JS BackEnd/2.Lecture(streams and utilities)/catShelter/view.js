66e598d95387786148267535bfdbc9baf159f052
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-18 22:49:33 +0300

    Update view.js

--- JS BackEnd/2.Lecture(streams and utilities)/catShelter/view.js
+++ JS BackEnd/2.Lecture(streams and utilities)/catShelter/view.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 const layout = fs.readFileSync('./views/layout.html').toString();
 
-function loadFragment(name, callBack){
+function loadFragment(name, callBack) {
     fs.readFile(`./views/${name}.html`, (err, data) => callBack(data.toString()));
 }
 
-function render(body){
+function render(body) {
     return layout.replaceAll('{{{body}}}', body);
 }
 
