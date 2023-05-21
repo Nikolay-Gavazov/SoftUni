2b8937593779b2c80604f30b7547fa2876d6e69c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-22 00:32:00 +0300

    Update app.js

--- JS Applications/2.Lecture(asynchronous)/02.Github-Repos/scripts/app.js
+++ JS Applications/2.Lecture(asynchronous)/02.Github-Repos/scripts/app.js
@@ -2,21 +2,21 @@ async function loadRepos() {
 	const username = document.getElementById('username').value;
 	const repos = document.getElementById('repos');
 	const url = `https://api.github.com/users/${username}/repos`;
-   
-	try{
+
+	try {
 		const response = await fetch(url);
-		if(response.ok == false){
-		throw `${response.status}: ${response.statusText}`;
-		
+		if (response.ok == false) {
+			throw `${response.status}: ${response.statusText}`;
+
 		}
 		const data = await response.json();
 		repos.replaceChildren();
-		for(let el of data){
+		for (let el of data) {
 			const li = document.createElement('li');
 			li.innerHTML = `<a href='${el.html_url}'>${el.full_name}</a>`;
 			repos.appendChild(li);
 		}
-	}catch(error){
+	} catch (error) {
 		repos.innerHTML = `<p>${error}</p>`;
-}
+	}
 }
