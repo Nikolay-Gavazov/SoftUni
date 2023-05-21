0c007940d8d9304a3b4149ecdb5d59a20d4e43ef
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-22 00:32:23 +0300

    Update app.js

--- JS Applications/2.Lecture(asynchronous)/03.Gighub-Commits/app.js
+++ JS Applications/2.Lecture(asynchronous)/03.Gighub-Commits/app.js
@@ -1,27 +1,27 @@
 async function loadCommits() {
 
-    const username = document.getElementById('username').value;
+	const username = document.getElementById('username').value;
 	const repos = document.getElementById('repo').value;
-    const result = document.getElementById('commits');
+	const result = document.getElementById('commits');
 
 	const url = `https://api.github.com/repos/${username}/${repos}/commits`;
-   
-	try{
+
+	try {
 		const response = await fetch(url);
-		if(response.ok == false){
-		throw `Error: ${response.status} (Not Found)`;
-		
+		if (response.ok == false) {
+			throw `Error: ${response.status} (Not Found)`;
+
 		}
 		const data = await response.json();
 		result.replaceChildren();
-		for(let el of data){
-            console.log(el);
+		for (let el of data) {
+			console.log(el);
 			const li = document.createElement('li');
 			li.innerHTML = `${el.commit.author.name}: ${el.commit.message}`;
 			result.appendChild(li);
 		}
-	}catch(error){
+	} catch (error) {
 		result.innerHTML = `<li>${error}</li>`;
-}
+	}
 
 }
