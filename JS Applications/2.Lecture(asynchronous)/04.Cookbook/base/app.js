b52b19c4779fe84f0c71e12487c09f7814fcd2f0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-22 00:32:50 +0300

    Update app.js

--- JS Applications/2.Lecture(asynchronous)/04.Cookbook/base/app.js
+++ JS Applications/2.Lecture(asynchronous)/04.Cookbook/base/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', start);
 
-function start(e){
+function start(e) {
     e.preventDefault();
     loadRepos();
     const main = document.querySelector('main');
@@ -8,16 +8,16 @@ function start(e){
 
 
     async function loadRepos() {
-        
-        try{
+
+        try {
             const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
-            if(response.ok == false){
-            throw `${response.status}: ${response.statusText}`;
-            
+            if (response.ok == false) {
+                throw `${response.status}: ${response.statusText}`;
+
             }
             const data = await response.json();
             main.replaceChildren();
-            for(let el in data){
+            for (let el in data) {
                 const article = document.createElement('article');
                 article.className = 'preview';
                 article.id = data[el]._id;
@@ -29,25 +29,25 @@ function start(e){
                 <img src="${data[el].img}">
             </div>
                 `;
-            article.addEventListener('click', recipeInfo);
-            main.appendChild(article);
+                article.addEventListener('click', recipeInfo);
+                main.appendChild(article);
             }
-        }catch(error){
+        } catch (error) {
             main.innerHTML = `<p>${error}</p>`;
-    }
+        }
 
 
-    async function recipeInfo(e){
-        e.preventDefault();
-        try {
-            const res = await fetch(`http://localhost:3030/jsonstore/cookbook/details/${e.target.id}`);
-            if(!res.ok) throw Error 
+        async function recipeInfo(e) {
+            e.preventDefault();
+            try {
+                const res = await fetch(`http://localhost:3030/jsonstore/cookbook/details/${e.target.id}`);
+                if (!res.ok) throw Error
 
-            const data = await res.json();
-            const ingredients = data.ingredients;
-            const steps = data.steps;
-            const article = document.createElement('article');
-            article.innerHTML += `
+                const data = await res.json();
+                const ingredients = data.ingredients;
+                const steps = data.steps;
+                const article = document.createElement('article');
+                article.innerHTML += `
         <h2>${data.name}</h2>
         <div class="band">
             <div class="thumb">
@@ -61,21 +61,21 @@ function start(e){
         <div class="description">
             <h3>Preparation:</h3>
         </div>`;
-        for(const el in ingredients){
-            const li = document.createElement('li');
-            li.textContent = ingredients[el];
-            article.querySelector('ul').appendChild(li);
+                for (const el in ingredients) {
+                    const li = document.createElement('li');
+                    li.textContent = ingredients[el];
+                    article.querySelector('ul').appendChild(li);
+                }
+                for (const el in steps) {
+                    const p = document.createElement('p');
+                    p.textContent = steps[el];
+                    article.querySelector('.description').appendChild(p);
+                }
+                main.replaceChildren();
+                main.appendChild(article)
+            } catch (error) {
+                alert(error.message);
+            }
         }
-        for(const el in steps){
-            const p = document.createElement('p');
-            p.textContent = steps[el];
-            article.querySelector('.description').appendChild(p);
-        }
-        main.replaceChildren();
-        main.appendChild(article)
-        } catch (error) {
-            alert(error.message);
-        }
-    }   
     }
 }
