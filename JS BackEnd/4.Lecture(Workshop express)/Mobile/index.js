f3db0cc108c6617c40fb421630a7a08e15c92b34
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-25 10:02:16 +0300

    install express and hbs

--- JS BackEnd/4.Lecture(Workshop express)/Mobile/index.js
+++ JS BackEnd/4.Lecture(Workshop express)/Mobile/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use('/static', express.static('static'));
+
+app.listen(3000, () => console.log('Server is listening on port 3000'));
+
