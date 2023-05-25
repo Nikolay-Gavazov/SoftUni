d5bf3015ef9a8466745a506e70728f91915ff60d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-25 10:13:50 +0300

    bind routing

--- JS BackEnd/4.Lecture(Workshop express)/Mobile/index.js
+++ JS BackEnd/4.Lecture(Workshop express)/Mobile/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const { home } = require('./controllers/homePage');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
+
+app.get('/', home)
 
 app.listen(3000, () => console.log('Server is listening on port 3000'));
 
