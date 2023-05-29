b5834102b2d6f065b61883e8ed70e731453dcb99
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-29 17:32:22 +0300

    Update index.js

--- JS BackEnd/myProjects/Mobile/models/index.js
+++ JS BackEnd/myProjects/Mobile/models/index.js
@@ -1,2 +1,23 @@
 const mongoose = require('mongoose');
 
+const connectionStr = 'mongodb://127.0.0.1:27017/mobile';
+
+const Car = require('./Car');
+
+async function init(){
+    try {
+        await mongoose.connect(connectionStr, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('DB connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('DB Error');
+            console.error(err);
+        })
+    } catch (error) {
+        console.error('DB Error');
+        process.exit(1);
+    }
+}
