5cf08f193387f6e5abf987e3869d154e2a9f72d3
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-29 17:41:14 +0300

    Update index.js

--- JS BackEnd/myProjects/Mobile/index.js
+++ JS BackEnd/myProjects/Mobile/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 
 const carsService = require('./services/cars');
+const db = require('./models/index');
 
 const { home } = require('./controllers/homePage');
 const { about } = require('./controllers/aboutPage');
@@ -11,33 +12,39 @@ const { deletePage } = require('./controllers/deletePage');
 const edit = require('./controllers/editPage');
 const create = require('./controllers/createPage');
 
-const app = express();
+async function start(){
+    await db();
+    const app = express();
 
-app.engine('hbs', hbs.create({
-    extname: '.hbs'
-}).engine);
+    app.engine('hbs', hbs.create({
+        extname: '.hbs'
+    }).engine);
+    
+    app.set('view engine', 'hbs');
+    
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/static', express.static('static'));
+    app.use(carsService());
+    
+    
+    app.get('/', home);
+    app.get('/about', about);
+    app.get('/details/:id', details);
+    app.get('/delete/:id', deletePage);
+    
+    app.route('/create')
+        .get(create.get)
+        .post(create.post);
+    
+    app.route('/edit/:id')
+        .get(edit.get)
+        .post(edit.post)
+    
+    app.all('*', notFound)
+    
+    app.listen(3000, () => console.log('Server is listening on port 3000'));
+}
+start();
 
-app.set('view engine', 'hbs');
 
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
-app.use(carsService());
-
-
-app.get('/', home);
-app.get('/about', about);
-app.get('/details/:id', details);
-app.get('/delete/:id', deletePage);
-
-app.route('/create')
-    .get(create.get)
-    .post(create.post);
-
-app.route('/edit/:id')
-    .get(edit.get)
-    .post(edit.post)
-
-app.all('*', notFound)
-
-app.listen(3000, () => console.log('Server is listening on port 3000'));
 
