cfd498881548082c8350aaec3d82198313040c20
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-29 17:16:23 +0300

    Update createPage.js

--- JS BackEnd/myProjects/Mobile/controllers/createPage.js
+++ JS BackEnd/myProjects/Mobile/controllers/createPage.js
@@ -4,13 +4,19 @@ module.exports = {
     },
     async post(req, res) {
         const data = req.body;
-        await req.storage.createCar({
+        const car = {
             name: data.name,
             description: data.description,
             imageUrl: data.imageUrl,
             price: Number(data.price)
 
-        });
-        res.redirect('/')
+        };
+        try {
+            await req.storage.createCar(car);
+            res.redirect('/');
+        } catch (error) {
+            console.log('Error creating record');
+            res.redirect('/create');
+        }
     }
 }
