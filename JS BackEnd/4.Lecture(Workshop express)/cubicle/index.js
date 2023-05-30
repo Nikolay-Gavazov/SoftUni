1bd8baa9065ee4b5b874e992be2290aae9452b45
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 11:21:45 +0300

    add handlebars and express to the app

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/index.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/index.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 
+
+
+async function start(){
+
+    const app = express();
+
+    app.engine('hbs', hbs.create({
+        extname: '.hbs'
+    }).engine);
+
+    app.set('view engine', 'hbs');
+
+    app.use(express.urlencoded({extended: true}));
+    app.use('/static', express.static('static'));
+    app.use
+}
+start()
