3e72580fc06d1d0c90c52904f71fbb664d2eb415
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-31 00:41:06 +0300

    add query param to be visible in the home template

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/home.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/home.js
@@ -1,6 +1,6 @@
 module.exports = {
     async home(req, res){
         const cubes = await req.cube.getAll(req.query);
-        res.render('index', {cubes, title: 'Cubicle'});
+        res.render('index', {cubes, title: 'Cubicle', query: req.query});
     }
 }
