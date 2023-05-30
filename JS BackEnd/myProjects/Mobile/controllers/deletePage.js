4a9909c3e3aa1da66be86f1d151efc575bc1e5a0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 16:21:12 +0300

    Update deletePage.js

--- JS BackEnd/myProjects/Mobile/controllers/deletePage.js
+++ JS BackEnd/myProjects/Mobile/controllers/deletePage.js
@@ -1,7 +1,7 @@
 module.exports = {
     async deletePage(req, res) {
         const id = req.params.id;
-        console.log(id);
+
         await req.storage.deleteCar(id);
 
         res.redirect('/')
