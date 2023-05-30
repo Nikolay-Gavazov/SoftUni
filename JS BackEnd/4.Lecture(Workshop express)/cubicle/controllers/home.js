68b51fac1cc57e5bc3da70fe02d2017d60b29a6a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 15:05:52 +0300

    Update home.js

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/home.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/home.js
@@ -1,6 +1,6 @@
 module.exports = {
     async home(req, res){
         const cubes = await req.storage.getAll(req.query);
-        res.render('index', {title: 'Cubicle'});
+        res.render('index', {cubes, title: 'Cubicle'});
     }
 }
