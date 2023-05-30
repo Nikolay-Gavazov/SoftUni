06fb147fd530d073b1b78a44c4f20637e1c621da
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 12:54:16 +0300

    create DB

---
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/models/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connectionStr = 'mongodb://127.0.0.1:27017/Cubicle';
+
+const Cube = require('./Cube');
+
+async function init(){
+    try {
+        await mongoose.connect(connectionStr, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('DB connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('DB error');
+            console.error(err);
+        })
+    } catch (error) {
+        console.error('DB error:', error);
+        process.exit(1);
+    }
+}
