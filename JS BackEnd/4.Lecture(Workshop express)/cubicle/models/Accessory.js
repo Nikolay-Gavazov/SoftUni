851ab4976e5cdcad362c037cb316ea9c758d0a9c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-31 00:43:02 +0300

    format and update

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/models/Accessory.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/models/Accessory.js
@@ -1,11 +1,11 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const accessorySchema = new Schema({
-    name: {type: String, required: true},
-    description: {type: String},
-    imageUrl: {type: String},
+    name: { type: String, required: true },
+    description: { type: String },
+    imageUrl: { type: String },
 });
 
-const Accessory = model('accessory', accessorySchema);
+const Accessory = model('Accessory', accessorySchema);
 
 module.exports = Accessory;
