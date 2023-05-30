31bf4aec28902a053fb4cf648b9fbf6c3c6a1fa6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 14:53:51 +0300

    create getAll function

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/data/service.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/data/service.js
@@ -1,9 +1,28 @@
+const Cube = require('../models/Cube');
 
+async function getAll(query){
+    const options = {};
+
+    if(query.search){
+        options.name = new RegExp(query.search, 'i');
+    }
+    if(query.from){
+        options.difficultyLevel = {$gte: Number(query.from)};
+    }
+    if(query.to){
+        if(!options.difficultyLevel){
+            options.difficultyLevel = {};
+        }
+        options.difficultyLevel.$lte = Number(query.from);
+    }
+    const cubes = await Cube.find(options);
+    return cubes
+}
 
 
 module.exports = () => (req, res, next) => {
-    req.starage = {
-
+    req.storage = {
+        getAll
     };
     next();
 }
