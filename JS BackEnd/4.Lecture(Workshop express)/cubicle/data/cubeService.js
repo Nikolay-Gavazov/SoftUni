eac9c58537ec39bf857abec30bc6c997bdcb156f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-31 00:42:33 +0300

    formating

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/data/cubeService.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/data/cubeService.js
@@ -1,16 +1,16 @@
 const Cube = require('../models/Cube');
 
-async function getAll(query){
+async function getAll(query) {
     const options = {};
 
-    if(query.search){
+    if (query.search) {
         options.name = new RegExp(query.search, 'i');
     }
-    if(query.from){
-        options.difficultyLevel = {$gte: Number(query.from)};
+    if (query.from) {
+        options.difficultyLevel = { $gte: Number(query.from) };
     }
-    if(query.to){
-        if(!options.difficultyLevel){
+    if (query.to) {
+        if (!options.difficultyLevel) {
             options.difficultyLevel = {};
         }
         options.difficultyLevel.$lte = Number(query.to);
@@ -19,31 +19,37 @@ async function getAll(query){
     return cubes
 };
 
-async function getById(id){
-    const data = await Cube.findById(id);
+async function getById(id) {
+    const data = await Cube.findById(id).populate('accessories');
 
-    if(data){
+    if (data) {
         return data;
-    }else{
+    } else {
         return undefined;
     }
 };
 
-async function createItem(data){
+async function createItem(data) {
     const cube = dataParcer(data);
     await Cube.create(cube);
 };
 
-async function deleteItem(id){
+async function deleteItem(id) {
     await Cube.findByIdAndDelete(id);
 };
 
-async function editItem(id, data){
+async function editItem(id, data) {
     const cube = dataParcer(data);
     await Cube.findByIdAndUpdate(id, cube);
 }
 
-function dataParcer(data){
+async function attachAccessory(cubeId, accessoryId) {
+    const cube = await Cube.findById(cubeId);
+    cube.accessories.push(accessoryId);
+    await cube.save();
+}
+
+function dataParcer(data) {
     return {
         name: data.name,
         description: data.description,
@@ -58,7 +64,8 @@ module.exports = () => (req, res, next) => {
         getById,
         createItem,
         deleteItem,
-        editItem
+        editItem,
+        attachAccessory
     };
     next();
 }
