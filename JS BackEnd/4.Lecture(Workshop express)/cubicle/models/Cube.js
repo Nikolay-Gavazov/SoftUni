07cd94b6a9a36aba7a3af895f743b372708a21d4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-31 00:43:11 +0300

    format and update

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/models/Cube.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/models/Cube.js
@@ -1,10 +1,14 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
 
 const cubeSchema = new Schema({
-    name: {type: String, required: true},
-    description: {type: String},
-    imageUrl: {type: String},
-    difficultyLevel: {type: Number}
+    name: { type: String, required: true },
+    description: { type: String },
+    imageUrl: { type: String },
+    difficultyLevel: { type: Number },
+    accessories: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Accessory'
+    }]
 });
 
 const Cube = model('Cube', cubeSchema);
