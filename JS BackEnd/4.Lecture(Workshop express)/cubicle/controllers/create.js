6a74c2f6deace16411867c00337981ac917e4377
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-05-30 15:46:34 +0300

    make create function works

--- JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/create.js
+++ JS BackEnd/4.Lecture(Workshop express)/cubicle/controllers/create.js
@@ -1,8 +1,22 @@
 module.exports = {
     async get(req, res){
-        res.render('create');
+        res.render('create', {title: 'Create a cube'});
     },
     async post(req, res){
-        
+        const data = req.body;
+        const cube = {
+            name: data.name,
+            description: data.description,
+            imageUrl: data.imageUrl,
+            difficultyLevel: Number(data.difficultyLevel)
+        };
+        console.log(cube);
+        try {
+            await req.storage.createItem(cube);
+            res.redirect('/');
+        } catch (error) {
+            console.log(error);
+            res.redirect('/create');
+        }
     }
 }
