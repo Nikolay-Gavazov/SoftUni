ab1fc7a1f0e8080659de129073914d37dbd24ca0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-03 02:18:34 +0300

    Update register.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/register.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/register.js
@@ -1,15 +1,29 @@
-const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('../jwt-to-promise');
+const secret = 'lapamChushki';
+
 module.exports = {
     async get(req, res) {
         res.render('registerPage', { title: 'Register' });
     },
     async post(req, res) {
-        const {username, password} = req.body;
-
+        const {username, password, repeatPassword} = req.body;
+        if(password != repeatPassword){
+            res.redirect('/register');
+            return;
+        }
+        const salt = await bcrypt.genSalt(3);
+        const hash = await bcrypt.hash(password, salt);
+        const payload = {username};
+        const user = {
+            username,
+            password,
+            hash
+        }
         try {
-            await req.accessory.createItem(accessory);
+            const token = await jwt.sign(payload, secret, { expiresIn: '2d'});
+            await req.user.createData(user);
+            res.cookie('token', token);
             res.redirect('/');
         } catch (error) {
             console.log(error);
