77206973a427d297c4e48c5b53e7959d0bc13f0f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-02 23:32:17 +0300

    Update index.js

--- JS BackEnd/5.Lecture(Session and Authentication)/index.js
+++ JS BackEnd/5.Lecture(Session and Authentication)/index.js
@@ -1,26 +1,56 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('./jwt-custom');
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+const secret = 'lapamChushki';
 
 const users = {};
 
-//Authentication is an important part of application security
-//--It serves to verify whether the client is who or what it declares itself to be.
-//--It's built on several layers of abstraction
-//--Cookies -> Sessions -> Security
-//--Authentication is a cross-cutting concern, best handled away from business logic.
-//--Request -> Authentication -> Business Logic -> Response
-
-//Authorization
-//--The process of determining what a user is permitted to do on a computer or network
-
-//Authentication and hash
+//JSON Web Token(JWT) is an open standart that defines a compact
+//and self-contained way for securely transmitting information
+//between parties as a JSON object
+//--This information can be verified and trusted because it is digitally signed
+//--JWTs can be signed using a secret or a public/private key
+//pair using RSA or ECDSA
+//--JSON Web Tokens are useful for Authorization and
+//Information Exchange
 
 app.get('/', (req, res) => {
-    res.send(users)
+
+
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <header>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
+        <a href="/profile">Profile</a>
+    </nav>
+</header>
+        <h1>Home Page</h1>
+    </body>
+    </html>
+        `)
 });
+
+app.get('/verify/:token', (req, res) => {
+    const token = req.params.token;
+    const payload = jwt.verify(token, 'NotSoSecretButSecret');
+
+    res.send(payload)
+})
 
 app.get('/register', (req, res) => {
     res.send(`
@@ -33,6 +63,15 @@ app.get('/register', (req, res) => {
     <title>Document</title>
 </head>
 <body>
+<header>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
+        <a href="/profile">Profile</a>
+    </nav>
+</header>
+<h1>Register Page</h1>
     <form action="/register" method="post">
         <label for="username">Username</label>
         <input type="text" name="username" id="username">
@@ -46,6 +85,7 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', async (req, res) => {
+    let _id = 'xxxxxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
     const {username, password} = req.body;
 
     const salt = await bcrypt.genSalt(3);
@@ -69,6 +109,15 @@ app.get('/login', (req, res) => {
     <title>Document</title>
 </head>
 <body>
+<header>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/register">Register</a>
+        <a href="/login">Login</a>
+        <a href="/profile">Profile</a>
+    </nav>
+</header>
+<h1>Login Page</h1>
     <form action="/login" method="post">
         <label for="username">Username</label>
         <input type="text" name="username" id="username">
@@ -88,11 +137,34 @@ app.post('/login', async (req, res) => {
     const isValid = await bcrypt.compare(password, hash);
 
     if(isValid){
-        res.send('Successfull Login')
+        const payload = { username }
+        try {
+            const token = await jwt.sign(payload, secret, { expiresIn: '2d'});
+            res.cookie('token', token);
+            res.redirect('/profile');
+        } catch (error) {
+            res.redirect('/404');
+        }
     }else{
-        res.send('Password or Username dont match')
+        res.status(401).send('Password or Username dont match')
     }
     
+})
+
+app.get('/profile', async(req, res) => {
+    const token = req.cookies.token;
+
+    if(token){
+        try {
+            const payload = await jwt.verify(token, secret);
+
+            return res.send(`<p>Hello ${payload.username} </p>`)
+        } catch (error) {
+            return res.status(401).send('Unauthorized');
+        }
+    }else{
+        res.redirect('/login');
+    }
 })
 
 app.listen(3000, () => console.log('server is listening on port 3000'))
