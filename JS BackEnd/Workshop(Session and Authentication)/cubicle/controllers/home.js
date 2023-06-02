a4b81ee125030845f33aef34845af924c6db382e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-03 02:17:50 +0300

    add option for logged in users

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/home.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/home.js
@@ -1,6 +1,7 @@
 module.exports = {
     async home(req, res){
+        const user = await req.user.checkUser(req);
         const cubes = await req.cube.getAll(req.query);
-        res.render('index', {cubes, title: 'Cubicle', query: req.query});
+        res.render('index', {cubes, title: 'Cubicle', user, query: req.query});
     }
 }
