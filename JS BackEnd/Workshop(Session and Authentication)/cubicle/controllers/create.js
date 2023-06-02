0c8ae7362449b6b4bf238dd6adc41420d09d7fc0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-03 02:16:52 +0300

    Update create.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/create.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/create.js
@@ -1,14 +1,17 @@
 module.exports = {
     async get(req, res) {
-        res.render('create', { title: 'Create a cube' });
+        const user = await req.user.checkUser(req);
+        res.render('create', { title: 'Create a cube', user });
     },
     async post(req, res) {
+        const user = await req.user.checkUser(req);
         const data = req.body;
         const cube = {
             name: data.name,
             description: data.description,
             imageUrl: data.imageUrl,
-            difficultyLevel: Number(data.difficultyLevel)
+            difficultyLevel: Number(data.difficultyLevel),
+            ownerId: user._id
         };
 
         try {
