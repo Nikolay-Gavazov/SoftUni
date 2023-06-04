405cc4fd7f186ec31db85ed0624e3717194a39ff
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-04 23:58:24 +0300

    Update userService.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/data/userService.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/data/userService.js
@@ -2,39 +2,39 @@ const User = require("../models/User");
 const jwt = require('../jwt-to-promise');
 const secret = 'lapamChushki';
 
-async function getById(id){
+async function getById(id) {
     const user = await User.findById(id);
     return user;
 };
 
-async function checkUser(req){
-        const token = req.cookies.token;
-        let user = null;
-        if(token){
+async function checkUser(req) {
+    const token = req.cookies.token;
+    let user = null;
+    if (token) {
         const payload = await jwt.verify(token, secret);
-        user = payload.username;   
-        }
-        return user;
+        user = payload.username;
+    }
+    return user;
 }
 
-async function getUser(username){
-    const data = await User.findOne({username: username});
+async function getUser(username) {
+    const data = await User.findOne({ username: username });
     return data;
 }
 
-async function createData(data){
-    const user = await User.find({username: data.username});
-    if(user.length > 0){
+async function createData(data) {
+    const user = await User.find({ username: data.username });
+    if (user.length > 0) {
         return new Error('This username is taken');
     }
     await User.create(data);
 };
 
-async function editData(id, data){
+async function editData(id, data) {
     await User.findByIdAndUpdate(id, data);
 };
 
-async function deleteData(id){
+async function deleteData(id) {
     await User.findByIdAndDelete(id);
 }
 
