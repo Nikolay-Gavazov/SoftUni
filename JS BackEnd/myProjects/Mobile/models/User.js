439aba4d08ab7c6e2c4c5b3070afd50c8f549044
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 15:32:44 +0300

    Update User.js

--- JS BackEnd/myProjects/Mobile/models/User.js
+++ JS BackEnd/myProjects/Mobile/models/User.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose');
 
 const userSchema = new Schema({
     username: {type: String, required: true},
-    password: {type: String},
+    password: {type: String, required: true},
 });
 
 const User = model('User', userSchema);
