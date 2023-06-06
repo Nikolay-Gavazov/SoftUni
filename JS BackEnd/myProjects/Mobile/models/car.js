2ec55213ad285689d5dd8d9a6f9a8257b525c6ee
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 15:32:39 +0300

    Update car.js

--- JS BackEnd/myProjects/Mobile/models/car.js
+++ JS BackEnd/myProjects/Mobile/models/car.js
@@ -1,15 +1,15 @@
-const {Schema, model} = require('mongoose');
+const {Schema, model, default: mongoose} = require('mongoose');
 
 const carSchema = new Schema({
     name: {type: String, required: true},
     description: {type: String},
     imageUrl: {type: String},
     price: {type: Number},
-    accessories: [{
-        type: mongoose.Types.ObjectId,
-        ref: 'Accessory'
-    }],
-    ownerId: {type: String}
+    // accessories: [{
+    //     type: mongoose.Types.ObjectId,
+    //     ref: 'Accessory'
+    // }],
+    // ownerId: {type: String}
 });
 
 const Car = model('Car', carSchema);
