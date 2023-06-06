f27f36e645c288b8e4f93888cd365db81d7caa7a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 16:15:38 +0300

    make functionality for login
    add bcrypt to check the hash

--- JS BackEnd/myProjects/Mobile/controllers/login.js
+++ JS BackEnd/myProjects/Mobile/controllers/login.js
@@ -8,5 +8,23 @@ module.exports = {
     },
     async post(req, res){
         const {username, password} = req.body;
+
+        const user = await req.user.getUser(username);
+        const hash = !user ? '' : user.password;
+        const isValid = await bcrypt.compare(password, hash);
+
+        if(isValid) {
+            const payload = {username};
+            try {
+                const token = await jwt.sign(payload, secret, {expiresIn :'2d'});
+                res.cookie('token', token);
+                res.redirect('/');
+            } catch (error) {
+                console.log(error);
+                res.redirect('/login')
+            }
+        }else{
+            res.render('loginPage', {title: 'Login', error: 'Username or password dont match'});
+        }
     }
 }
