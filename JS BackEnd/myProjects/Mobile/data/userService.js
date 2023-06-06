f87bcf9933fb6516a56dd5722e893839687fc5f0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 12:37:04 +0300

    make create, get, check user functions

---
+++ JS BackEnd/myProjects/Mobile/data/userService.js
@@ -0,0 +1,44 @@
+const User = require('../models/User');
+const jwt = require('../jwt-to-promise');
+const secret = 'samoLevski';
+
+async function createUserData(data){
+    const user = await User.find({username: data.username});
+    if(user.length > 0){
+        return new Error('This username is taken');
+    }
+    await User.create(data);
+};
+
+async function getUser(username){
+    const data = await User.findOne({username: username});
+    return data;
+};
+
+async function getById(id){
+    const user = await User.findById(id);
+    if(user){
+        return user;
+    }else{
+        return undefined;
+    }
+};
+
+async function checkUser(req){
+    const token = req.cookies.token;
+    let user = null;
+    if(token) {
+        const payload = await jwt.verify(token, secret);
+        user = payload.username;
+    }
+    return user;
+};
+
+module.exports = () => (req, res, next) => {
+    req.user = {
+        getById,
+        checkUser,
+        getUser,
+        createUserData
+    }
+}
