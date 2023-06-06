1b2019e5f0a5ea5df6d54ba44c7fd4de9258d394
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 15:36:26 +0300

    Update login.js

--- JS BackEnd/myProjects/Mobile/controllers/login.js
+++ JS BackEnd/myProjects/Mobile/controllers/login.js
@@ -1,1 +1,12 @@
 const bcrypt = require('bcrypt');
+const jwt = require('../jwt-to-promise');
+const secret = 'samoLevski';
+
+module.exports = {
+    async get(req, res){
+        res.render('loginPage', {title: 'Login'});
+    },
+    async post(req, res){
+        const {username, password} = req.body;
+    }
+}
