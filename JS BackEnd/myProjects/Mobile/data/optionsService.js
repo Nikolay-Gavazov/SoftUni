19189b251ddd443df5bd4d8273f36f71d33d2a71
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 16:15:53 +0300

    Create optionsService.js

---
+++ JS BackEnd/myProjects/Mobile/data/optionsService.js
@@ -0,0 +1,29 @@
+const Option = require('../models/Option');
+
+async function getAll(){
+    const data = await Option.find({});
+    return data;
+};
+
+async function createoption(data){
+    await Option.create(data);
+};
+
+async function getRest(ids){
+    const data = await Option.find({_id: { $nin: ids } });
+
+    if(data){
+        return data;
+    }else{
+        return undefined;
+    }
+};
+
+module.exports = () => (req, res, next) => {
+    req.option = {
+        getAll,
+        createoption,
+        getRest
+    }
+    next();
+}
