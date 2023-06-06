e44e8474cfc51c3d91bd89333d2ef65344eaaf18
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 18:23:26 +0300

    add user to navigation and options

--- JS BackEnd/myProjects/Mobile/controllers/detailsPage.js
+++ JS BackEnd/myProjects/Mobile/controllers/detailsPage.js
@@ -2,8 +2,11 @@ module.exports = {
     async details(req, res) {
         const id = req.params.id;
         const car = await req.storage.getById(id);
+        const user = await req.user.getUser(await req.user.checkUser(req));
+        const isOwner = user.id == car.ownerId;
+        const options = req.option.getAll();
         if (car) {
-            res.render('details', { title: `Mobile - ${car.name}`, car })
+            res.render('details', { title: `Mobile - ${car.name}`, car , isOwner, options, user})
         } else {
             res.redirect('/404')
         }
