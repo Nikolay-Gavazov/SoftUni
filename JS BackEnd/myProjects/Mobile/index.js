448df3b438cf4f7f3846e1a5220aa1aa1ac84a62
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 15:32:31 +0300

    add new services
    add cookie-parser, user service, accessories service, login page

--- JS BackEnd/myProjects/Mobile/index.js
+++ JS BackEnd/myProjects/Mobile/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 
 const carsService = require('./data/carsService');
+const accessoriesService = require('./data/accessoryService');
+const userService = require('./data/userService');
 const db = require('./data/database');
 
 const { home } = require('./controllers/homePage');
@@ -10,8 +13,10 @@ const { about } = require('./controllers/aboutPage');
 const { details } = require('./controllers/detailsPage');
 const { notFound } = require('./controllers/notFound');
 const { deletePage } = require('./controllers/deletePage');
+const { logout } = require('./controllers/logout');
 const edit = require('./controllers/editPage');
 const create = require('./controllers/createPage');
+const register = require('./controllers/register');
 
 
 async function start(){
@@ -27,12 +32,16 @@ async function start(){
     app.use(express.urlencoded({ extended: true }));
     app.use('/static', express.static('static'));
     app.use(carsService());
+    app.use(userService());
+    app.use(accessoriesService());
+    app.use(cookieParser());
     app.use(fileUpload({createParentPath: true}));
     
     app.get('/', home);
     app.get('/about', about);
     app.get('/details/:id', details);
     app.get('/delete/:id', deletePage);
+    app.get('/logout', logout)
     
     app.route('/create')
         .get(create.get)
@@ -40,7 +49,11 @@ async function start(){
     
     app.route('/edit/:id')
         .get(edit.get)
-        .post(edit.post)
+        .post(edit.post);
+
+    app.route('/register')
+        .get(register.get)
+        .post(register.post);
 
     app.all('*', notFound)
     
