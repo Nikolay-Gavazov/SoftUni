6a663ad26c9a56438216d03373f718e768cec92c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 12:19:09 +0300

    Update accessoryService.js

--- JS BackEnd/myProjects/Mobile/data/accessoryService.js
+++ JS BackEnd/myProjects/Mobile/data/accessoryService.js
@@ -5,3 +5,24 @@ async function getAll(){
     return data;
 };
 
+async function createAccessory(data){
+    await Accessory.create(data);
+};
+
+async function getRest(ids){
+    const data = await Accessory.find({_id: { $nin: ids } });
+
+    if(data){
+        return data;
+    }else{
+        return undefined;
+    }
+};
+
+module.exports = () => (req, res, next) => {
+    req.accessory = {
+        getAll,
+        createAccessory,
+        getRest
+    }
+}
