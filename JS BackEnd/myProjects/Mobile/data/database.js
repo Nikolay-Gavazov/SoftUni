14d301382bbfbade9f01f331736fe9b320dbf756
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-06 11:47:19 +0300

    Create database.js

---
+++ JS BackEnd/myProjects/Mobile/data/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const connectionStr = 'mongodb://127.0.0.1:27017/mobile';
+
+require('../models/Car');
+
+async function init(){
+    try {
+        await mongoose.connect(connectionStr, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('DB connected');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('DB Error');
+            console.error(err);
+        })
+    } catch (error) {
+        console.error('DB Error');
+        process.exit(1);
+    }
+}
+
+module.exports = init;
