f2ff2e18dc4ce801066f676b8e848c2937af1071
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-07 23:03:19 +0300

    Update home.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/home.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/home.js
@@ -1,7 +1,6 @@
 module.exports = {
     async home(req, res){
-        const user = await req.user.checkUser(req);
         const cubes = await req.cube.getAll(req.query);
-        res.render('index', {cubes, title: 'Cubicle', user, query: req.query});
+        res.render('index', {cubes, title: 'Cubicle', user: req.userData, query: req.query});
     }
 }
