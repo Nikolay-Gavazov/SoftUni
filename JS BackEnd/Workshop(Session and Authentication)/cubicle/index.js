258ce49f863f32834a1273c2dab724e8389044cb
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-07 23:03:43 +0300

    Update index.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/index.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/index.js
@@ -69,6 +69,6 @@ async function start() {
 
     app.all('*', notFound);
 
-    app.listen(3000, () => console.log('Server is listening on port 3000'))
-}
+    app.listen(5000, () => console.log('Server is listening on port 3000'))
+} 
 start()
