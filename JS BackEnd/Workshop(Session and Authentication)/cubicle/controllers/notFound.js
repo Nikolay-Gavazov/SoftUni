f445791b43852dff9427842d23582d175833d657
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-07 23:03:35 +0300

    Update notFound.js

--- JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/notFound.js
+++ JS BackEnd/Workshop(Session and Authentication)/cubicle/controllers/notFound.js
@@ -1,6 +1,5 @@
 module.exports = {
     async notFound(req, res){
-        const user = await req.user.checkUser(req);
-        res.render('404', {title: 'Not Found', user});
+        res.render('404', {title: 'Not Found', user: req.userData});
     }
 }
