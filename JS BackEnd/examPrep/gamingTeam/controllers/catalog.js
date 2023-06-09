dee4ecbdacc2c7c12350a783d88ae736db4ded9e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-10 01:09:37 +0300

    Update catalog.js

--- JS BackEnd/examPrep/gamingTeam/controllers/catalog.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/catalog.js
@@ -1,7 +1,7 @@
 module.exports = {
     async catalog(req, res){
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const games = await req.games.getAll(req.query);
+        const games = await req.game.getAll(req.query);
         res.render('catalog', {games, title: 'Catalog Page', user});
     }
 }
