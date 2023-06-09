aa6581faabd42b85d4af45688d78af9abc02dc54
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-10 00:05:30 +0300

    add validations

--- JS BackEnd/examPrep/gamingTeam/controllers/create.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/create.js
@@ -1,25 +1,42 @@
-module.exports = {
-    async get(req, res) {
+const { Router } = require('express');
+const { body, validationResult } = require('express-validator');
+
+const router = Router();
+
+    router.get('/create', async (req, res) => {
         const user = await req.storage.getUser(await req.storage.checkUser(req));
         res.render('create', { title: 'Create Game', user });
-    },
-    async post(req, res) {
+    })
+    router.post('/create',
+        body('name').trim(),
+        body('image').trim(),
+        body('price').trim(),
+        body('genre').trim(),
+        body('description').trim(),
+        body('platform')
+        .custom(value => value == 'PC' || 
+        value == 'Nintendo' || 
+        value == 'PS4' ||
+        value == 'PS5' ||
+        value == 'XBOX'),
+        async(req, res) => {
         const data = req.body;
         const game = {
+            platform: data.platform,
             name: data.name,
             image: data.image,
             price: Number(data.price),
-            description: data.description,
             genre: data.genre,
-            platform: data.platform,
+            description: data.description,
         };
 
         try {
-            await req.cube.createItem(cube);
+            await req.game.createItem(game);
             res.redirect('/');
         } catch (error) {
             console.log(error);
             res.redirect('/create');
         }
-    }
-}
+    });
+
+module.exports = router;
