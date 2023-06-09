00f74268a0db91d7265c2062bb6dab7e846a7585
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-09 23:47:12 +0300

    Update auth.js

--- JS BackEnd/examPrep/gamingTeam/controllers/auth.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/auth.js
@@ -12,26 +12,26 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register',
-body('username').trim(),
-body('email').trim(),
-body('password').trim(),
-body('rePass').trim(),
-body('username')
-    .isLength({min: 5})
-    .isAlphanumeric(),
-body('email')
-    .isLength({min: 10})
-    .isEmail(),
-body('password', 'Password must be at least 4 characters long')
-    .isLength({min: 4}),
-body('rePass', 'Password missmatch.').custom((value, {req}) => value == req.body.password),
-async (req, res) => {
-    const { username, email, password, rePass } = req.body;
-    const {errors} = validationResult(req);   
-    try {
-        if(errors.length > 0){
-            throw errors;
-        }
+    body('username').trim(),
+    body('email').trim(),
+    body('password').trim(),
+    body('rePass').trim(),
+    body('username', 'Username must be at least 5 characters long')
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+    body('email', 'Email must be at lest 10 characters long')
+        .isLength({ min: 10 })
+        .isEmail(),
+    body('password', 'Password must be at least 4 characters long')
+        .isLength({ min: 4 }),
+    body('rePass', 'Password missmatch.').custom((value, { req }) => value == req.body.password),
+    async (req, res) => {
+        const { username, email, password, rePass } = req.body;
+        const { errors } = validationResult(req);
+        try {
+            if (errors.length > 0) {
+                throw errors;
+            }
             const salt = await bcrypt.genSalt(3);
             const hash = await bcrypt.hash(password, salt);
             const payload = { email };
@@ -50,49 +50,49 @@ async (req, res) => {
             res.redirect('/');
         } catch (error) {
             console.log(error);
-            res.render('register', {title: 'Register Page', error})
+            res.render('register', { title: 'Register Page', error, username, email })
         }
-});
+    });
 
 router.get('/login', (req, res) => {
     res.render('login', { title: 'Login Page' });
 });
 
 router.post('/login',
-body('email')
-    .trim()
-    .isLength({min: 10}),
-body('password')
-    .trim()
-    .isLength({min: 4}),
-async (req, res) => {
-    const { email, password } = req.body;
-    
-    try {
-        const user = await req.storage.getUser(email);
-        const hash = user.password;
-        const isValid = await bcrypt.compare(password, hash);
+    body('email')
+        .trim()
+        .isLength({ min: 10 }),
+    body('password')
+        .trim()
+        .isLength({ min: 4 }),
+    async (req, res) => {
+        const { email, password } = req.body;
 
-        if(isValid){
-            const payload = { email };
-            const token = await jwt.sign(payload, secret, { expiresIn: '2d' });
-            res.cookie('token', token);
-            req.user = email;
-            res.redirect('/');
-        }else {
-            throw new Error('Expired Session. Please Login.')
+        try {
+            const user = await req.storage.getUser(email);
+            const hash = user.password;
+            const isValid = await bcrypt.compare(password, hash);
+
+            if (isValid) {
+                const payload = { email };
+                const token = await jwt.sign(payload, secret, { expiresIn: '2d' });
+                res.cookie('token', token);
+                req.user = email;
+                res.redirect('/');
+            } else {
+                throw new Error('Expired Session. Please Login.')
+            }
+        } catch (error) {
+            console.log(error);
+            //const errorMessages = Object.values(error.errors).map(e => e.message);
+            res.render('login', { title: 'Login Page', error: error.message });
         }
-    } catch (error) {
-        console.log(error);
-        //const errorMessages = Object.values(error.errors).map(e => e.message);
-        res.render('login', {title: 'Login Page', error: error.message});
-    }
-});
+    });
 
 router.get('/logout', (req, res) => {
     res.clearCookie('token');
-        req.user = null;
-        res.redirect('/');
+    req.user = null;
+    res.redirect('/');
 });
 
 module.exports = router;
