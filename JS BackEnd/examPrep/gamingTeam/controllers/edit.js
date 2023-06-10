a70df625f879bd56bf44372fac69e606a87ebf28
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-11 01:56:45 +0300

    make option set works

--- JS BackEnd/examPrep/gamingTeam/controllers/edit.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/edit.js
@@ -4,10 +4,23 @@ const { body, validationResult } = require('express-validator');
 const router = Router();
 
     router.get('/edit/:id', async(req, res) => {
+        const options = {
+            'PC': 'PC',
+            'Nintendo': 'Nintendo',
+            'PS4': 'PS4',
+            'PS5': 'PS5',
+            'XBOX': 'XBOX'
+        }
         const id = req.params.id;
         const game = await req.game.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        res.render('edit', { game, title: `Edit - ${game.name}`, user});
+        const platforms = Object.keys(options).map(value => ({
+            value,
+            label: options[value],
+            isSelected: game.platform == value
+
+        }));
+        res.render('edit', { game, title: `Edit - ${game.name}`, user, platforms});
     }),
     router.post('/edit/:id',
     body('name').trim(),
