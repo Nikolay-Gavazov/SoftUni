fe8d28a1c2ecd10f8bc407784cbfc513a4ae4ee5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-11 00:45:33 +0300

    add validations

--- JS BackEnd/examPrep/gamingTeam/controllers/edit.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/edit.js
@@ -5,27 +5,54 @@ const router = Router();
 
     router.get('/edit/:id', async(req, res) => {
         const id = req.params.id;
-        console.log(id);
         const game = await req.game.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
         res.render('edit', { game, title: `Edit - ${game.name}`, user});
     }),
-    router.post('/edit/:id', async(req, res) => {
+    router.post('/edit/:id',
+    body('name').trim(),
+    body('image').trim(),
+    body('price').trim(),
+    body('genre').trim(),
+    body('description').trim(),
+    body('platform')
+    .custom(value => value == 'PC' || 
+    value == 'Nintendo' || 
+    value == 'PS4' ||
+    value == 'PS5' ||
+    value == 'XBOX'),
+    body('name', 'Name should be at least four characters')
+    .isLength({min: 4}),
+    body('image', 'Game image should start with "http://" or "https://"')
+    .isURL(),
+    body('price', 'Price should be a positive number')
+    .isInt({min: 0}),
+    body('genre', 'Genre should be at least two characters long')
+    .isLength({min: 2}),
+    body('description', 'Description should be at least ten characters long')
+    .isLength({min: 10}),
+     async(req, res) => {
         const id = req.params.id;
         const data = req.body;
-        const cube = {
+        const user = await req.storage.getUser(await req.storage.checkUser(req));
+        const { errors } = validationResult(req);
+        const game = {
+            platform: data.platform,
             name: data.name,
-            description: data.description,
-            imageUrl: data.imageUrl,
-            difficultyLevel: Number(data.difficultyLevel)
+            image: data.image,
+            price: Number(data.price),
+            genre: data.genre,
+            description: data.description
         };
-
         try {
-            await req.cube.editItem(id, cube);
-            res.redirect('/');
+            if(errors.length > 0){
+                throw errors;
+            }
+            await req.game.editItem(id, game);
+            res.redirect(`/details/${id}`);
         } catch (error) {
             console.log(error);
-            res.redirect('/edit/:id');
+            res.render('edit', { title: 'Edit Game',error, user, game});
         }
     });
 module.exports = router;
