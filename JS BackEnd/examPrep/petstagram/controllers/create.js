b133356723744a00958788764f98e8d9e2716b25
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-12 01:09:46 +0300

    Update create.js

--- JS BackEnd/examPrep/petstagram/controllers/create.js
+++ JS BackEnd/examPrep/petstagram/controllers/create.js
@@ -8,27 +8,21 @@ const router = Router();
         if(!user){
             return res.render('notFound');
         }
-        res.render('create', { title: 'Create Game', user });
+        res.render('create', { user });
     })
     router.post('/create',
         body('name').trim(),
         body('image').trim(),
-        body('price').trim(),
-        body('genre').trim(),
+        body('age').trim(),
+        body('location').trim(),
         body('description').trim(),
-        body('platform')
-        .custom(value => value == 'PC' || 
-        value == 'Nintendo' || 
-        value == 'PS4' ||
-        value == 'PS5' ||
-        value == 'XBOX'),
         body('name', 'Name should be at least four characters')
         .isLength({min: 4}),
-        body('image', 'Game image should start with "http://" or "https://"')
+        body('image', 'Photo should start with "http://" or "https://"')
         .isURL(),
-        body('price', 'Price should be a positive number')
+        body('age', 'Age should be a positive number')
         .isInt({min: 0}),
-        body('genre', 'Genre should be at least two characters long')
+        body('location', 'Location should be at least two characters long')
         .isLength({min: 2}),
         body('description', 'Description should be at least ten characters long')
         .isLength({min: 10}),
@@ -36,12 +30,11 @@ const router = Router();
         const data = req.body;
         const user = await req.storage.getUser(await req.storage.checkUser(req));
         const { errors } = validationResult(req);
-        const game = {
-            platform: data.platform,
+        const photo = {
             name: data.name,
+            age: Number(data.age),
             image: data.image,
-            price: Number(data.price),
-            genre: data.genre,
+            location: data.location,
             description: data.description,
             ownerId: user._id
         };
@@ -50,11 +43,11 @@ const router = Router();
             if(errors.length > 0){
                 throw errors
             }
-            await req.game.createItem(game);
+            await req.photo.createItem(photo);
             res.redirect('/');
         } catch (error) {
             console.log(error);
-            res.render('create', { title: 'Create Game',error, user, game});
+            res.render('create', { error, user, photo});
         }
     });
 
