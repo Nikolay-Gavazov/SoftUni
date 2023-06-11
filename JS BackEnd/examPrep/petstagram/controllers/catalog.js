61aec8ddcc505caa20914b705af447e5fc99f6ae
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-12 01:09:34 +0300

    Update catalog.js

--- JS BackEnd/examPrep/petstagram/controllers/catalog.js
+++ JS BackEnd/examPrep/petstagram/controllers/catalog.js
@@ -1,7 +1,7 @@
 module.exports = {
     async catalog(req, res){
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const games = await req.game.getAll(req.query);
-        res.render('catalog', {games, title: 'Catalog Page', user});
+        const photos = await req.photo.getAll(req.query);
+        res.render('catalog', { user, photos});
     }
 }
