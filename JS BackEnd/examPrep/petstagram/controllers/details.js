36a0e140864c637df111b06981303eff06ed2a18
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-12 01:10:03 +0300

    Update details.js

--- JS BackEnd/examPrep/petstagram/controllers/details.js
+++ JS BackEnd/examPrep/petstagram/controllers/details.js
@@ -1,12 +1,12 @@
 module.exports = {
     async details(req, res){
         const id = req.params.id;
-        const game = await req.game.getById(id);
+        const photo = await req.photo.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const isOwner = user._id == game.ownerId;
-        let isBougth = game.bougthBy?.filter(el => el.username == user.username);
-        if(game){
-            res.render('details', {game, title: `Details - ${game.name}`, user, isOwner, isBougth});
+        const isOwner = user._id == photo._ownerId;
+        // let isBougth = game.bougthBy?.filter(el => el.username == user.username);
+        if(photo){
+            res.render('details', { user, photo});
         }else{
             res.redirect('/404');
         }
