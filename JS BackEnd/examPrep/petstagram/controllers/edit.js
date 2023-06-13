673eb42ce66b726dacab7c441342c208e6778f3b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-13 13:27:14 +0300

    Update edit.js

--- JS BackEnd/examPrep/petstagram/controllers/edit.js
+++ JS BackEnd/examPrep/petstagram/controllers/edit.js
@@ -4,28 +4,30 @@ const { body, validationResult } = require('express-validator');
 const router = Router();
 
     router.get('/edit/:id', async(req, res) => {
+        console.log(req);
         const id = req.params.id;
         const photo = await req.photo.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        res.render('edit', { photo, user});
+        res.render('edit', { id, photo, user});
     }),
     router.post('/edit/:id',
     body('name').trim(),
-    body('age').trim(),
-    body('description').trim(),
-    body('location').trim(),
-    body('image').trim(),
-    body('name', 'Name should be at least four characters')
-    .isLength({min: 4}),
-    body('image', 'Game image should start with "http://" or "https://"')
-    .isURL(),
-    body('age', 'Price should be a positive number')
-    .isInt({min: 0}),
-    body('location', 'Genre should be at least two characters long')
-    .isLength({min: 2}),
-    body('description', 'Description should be at least ten characters long')
-    .isLength({min: 10}),
+        body('image').trim(),
+        body('age').trim(),
+        body('location').trim(),
+        body('description').trim(),
+        body('name', 'Name should be at least 2 characters')
+        .isLength({min: 2}),
+        body('image', 'Photo should start with "http://" or "https://"')
+        .isURL(),
+        body('age', 'Age should be at least 1 and no longer than 100 characters.')
+        .isLength({min: 1, max: 100}),
+        body('location', 'Location  should be at least 5 and no longer than 50 characters.')
+        .isLength({min: 5, max: 50}),
+        body('description', 'Description should be at least 5 and no longer than 50 characters.')
+        .isLength({min: 5, max: 50}),
      async(req, res) => {
+        
         const id = req.params.id;
         const data = req.body;
         const user = await req.storage.getUser(await req.storage.checkUser(req));
@@ -41,11 +43,11 @@ const router = Router();
             if(errors.length > 0){
                 throw errors;
             }
-            await req.game.editItem(id, photo);
+            await req.photo.editItem(id, photo);
             res.redirect(`/details/${id}`);
         } catch (error) {
             console.log(error);
-            res.render('edit', { error, user, photo});
+            res.render('edit', { error, user, photo,id});
         }
     });
 module.exports = router;
