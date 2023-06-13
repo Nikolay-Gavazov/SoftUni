fc319ceb51698cbd5db90d77e7cabb2f28e1dc9e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 01:52:16 +0300

    change validation

--- JS BackEnd/examPrep/Wildlife Photography/controllers/create.js
+++ JS BackEnd/examPrep/Wildlife Photography/controllers/create.js
@@ -8,46 +8,54 @@ const router = Router();
         if(!user){
             return res.render('notFound');
         }
-        res.render('create', { user });
+        res.render('create', { _title: 'Create Page', user });
     })
     router.post('/create',
-        body('name').trim(),
-        body('image').trim(),
-        body('age').trim(),
+        body('title').trim(),
+        body('author').trim(),
+        body('keyword').trim(),
         body('location').trim(),
+        body('date').trim(),
+        body('image').trim(),
         body('description').trim(),
-        body('name', 'Name should be at least 2 characters')
-        .isLength({min: 2}),
+        body('title', 'Title should be at least 6 characters')
+        .isLength({min: 6}),
+        body('keyword', 'Keyword should be at least 6 characters')
+        .isLength({min: 6}),
         body('image', 'Photo should start with "http://" or "https://"')
         .isURL(),
-        body('age', 'Age should be at least 1 and no longer than 100 characters.')
-        .isLength({min: 1, max: 100}),
-        body('location', 'Location  should be at least 5 and no longer than 50 characters.')
-        .isLength({min: 5, max: 50}),
-        body('description', 'Description should be at least 5 and no longer than 50 characters.')
-        .isLength({min: 5, max: 50}),
+        body('date', 'Date should be exactly 10 characters.')
+        .isLength({min: 10, max: 10}),
+        body('location', 'Location  should be no longer than 15 characters.')
+        .isLength({ max: 15}),
+        body('description', 'Description should be at least 8 characters.')
+        .isLength({min: 8}),
         async(req, res) => {
         const data = req.body;
         const user = await req.storage.getUser(await req.storage.checkUser(req));
         const { errors } = validationResult(req);
-        const photo = {
-            name: data.name,
-            age: Number(data.age),
+        const post = {
+            title: data.title,
+            keyword: data.keyword,
+            date: data.date,
             image: data.image,
             location: data.location,
             description: data.description,
-            ownerId: user._id
+            author: {user: user._id, 
+                    fullname : `${user.firstName} ${user.lastName}`},
+            rating: 0
         };
 
         try {
             if(errors.length > 0){
                 throw errors
             }
-            await req.photo.createItem(photo);
+            const result = await req.post.createItem(post);
+            await req.storage.addPostToUser(user._id, result._id);
             res.redirect('/catalog');
         } catch (error) {
             console.log(error);
-            res.render('create', { error, user, photo});
+            res.render('create', { error, user, post});
         }
     });
 
