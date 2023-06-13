de196cf15b59ee4f974dae8a419301c9d87d9e20
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 01:52:24 +0300

    Update deleteController.js

--- JS BackEnd/examPrep/Wildlife Photography/controllers/deleteController.js
+++ JS BackEnd/examPrep/Wildlife Photography/controllers/deleteController.js
@@ -2,7 +2,7 @@ module.exports = {
     async deletePage(req, res) {
         const id = req.params.id;
 
-        await req.photo.deleteItem(id);
+        await req.post.deleteItem(id);
 
         res.redirect('/catalog')
     }
