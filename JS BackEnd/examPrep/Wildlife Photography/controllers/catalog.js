659bd7f261ec628a802e322f4b4cd1daa061b0c7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 01:51:57 +0300

    Update catalog.js

--- JS BackEnd/examPrep/Wildlife Photography/controllers/catalog.js
+++ JS BackEnd/examPrep/Wildlife Photography/controllers/catalog.js
@@ -1,7 +1,7 @@
 module.exports = {
     async catalog(req, res){
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const photos = await req.photo.getAll(req.query);
-        res.render('catalog', { user, photos});
+        const posts = await req.post.getAll(req.query);
+        res.render('all-posts', { _title: 'All Posts', user, posts});
     }
 }
