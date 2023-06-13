8319a7dc61ebb68f8ebc3991f5f645bce7151ebb
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-13 13:26:40 +0300

    Delete buy.js

--- JS BackEnd/examPrep/petstagram/controllers/buy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    async buy(req, res) {
-        const id = req.params.id;
-        const user = await req.storage.getUser(await req.storage.checkUser(req));
-        await req.game.buyGame(id, user._id);
-
-        res.redirect(`/details/${id}`);
-    }
-}
