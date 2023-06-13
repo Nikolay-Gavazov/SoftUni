6fc155b57290ca2825c5b17af68d01085c00d754
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-13 13:27:06 +0300

    add comments to details

--- JS BackEnd/examPrep/petstagram/controllers/details.js
+++ JS BackEnd/examPrep/petstagram/controllers/details.js
@@ -8,17 +8,40 @@ const router = Router();
         const id = req.params.id;
         const photo = await req.photo.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const isOwner = user._id.toString() == photo.ownerId._id.toString();
-        const isNotOwner = user._id.toString() != photo.ownerId._id.toString();
+        const isOwner = user?._id.toString() == photo.ownerId._id.toString();
+        const isNotOwner = user?._id.toString() != photo.ownerId._id.toString() && user;
         if(photo){
-            res.render('details', { user, photo, isOwner, isNotOwner});
+            res.render('details', {user,photo, isOwner, isNotOwner, id});
         }else{
             res.redirect('/404');
         }
     });
 
-    router.post('/details/:id/comment', async (req, res) => {
-
+    router.post('/details/:id/comment',
+    body('comment').trim(),
+    body('comment', 'Comment should be at least 1 and no longer than 100 characters.')
+        .isLength({min: 1, max: 100}),
+     async (req, res) => {
+        const id = req.params.id;
+        const photo = await req.photo.getById(id);
+        const user = await req.storage.getUser(await req.storage.checkUser(req));
+        const isOwner = user._id.toString() == photo.ownerId._id.toString();
+        const isNotOwner = user._id.toString() != photo.ownerId._id.toString();
+        const { errors } = validationResult(req);
+        const comment = {
+                userId: user._id,
+                username: user.username,
+                comment: req.body.comment
+            }
+        try {
+            if(errors.length > 0){
+                throw errors;
+            }
+            await req.photo.comment(id, comment);
+            res.redirect(`/details/${id}`)
+        } catch (error) {
+            res.render('details', { error: error.message, photo, isOwner, isNotOwner});
+        }
     });
 
     module.exports = router;
