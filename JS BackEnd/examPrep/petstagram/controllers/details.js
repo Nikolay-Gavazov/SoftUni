1175ecd85206d7ba56908a0ff79b895318444c54
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-13 10:58:20 +0300

    add router to details, and make details work

--- JS BackEnd/examPrep/petstagram/controllers/details.js
+++ JS BackEnd/examPrep/petstagram/controllers/details.js
@@ -1,14 +1,24 @@
-module.exports = {
-    async details(req, res){
+const { Router } = require('express');
+const { body, validationResult } = require('express-validator');
+
+const router = Router();
+
+
+    router.get('/details/:id', async(req, res) => {
         const id = req.params.id;
         const photo = await req.photo.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const isOwner = user._id == photo._ownerId;
-        // let isBougth = game.bougthBy?.filter(el => el.username == user.username);
+        const isOwner = user._id.toString() == photo.ownerId._id.toString();
+        const isNotOwner = user._id.toString() != photo.ownerId._id.toString();
         if(photo){
-            res.render('details', { user, photo});
+            res.render('details', { user, photo, isOwner, isNotOwner});
         }else{
             res.redirect('/404');
         }
-    }
-}
+    });
+
+    router.post('/details/:id/comment', async (req, res) => {
+
+    });
+
+    module.exports = router;
