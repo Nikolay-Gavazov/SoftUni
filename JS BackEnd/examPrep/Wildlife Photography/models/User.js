70f9a6c0fa927a1e6f38db61b147dde7200f4811
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 01:54:44 +0300

    update model

--- JS BackEnd/examPrep/Wildlife Photography/models/User.js
+++ JS BackEnd/examPrep/Wildlife Photography/models/User.js
@@ -1,15 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, default: mongoose } = require('mongoose');
 
 const userSchema = new Schema({
-    username: { type: String,
-                required: true,
-                minLength: 3 },
-    password: { type: String,
-                required: true,
-                minLength: 4},
-    email: {type: String,
-            required: true,
-            minLength: 10}
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 4
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 10
+    },
+    myPosts: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Post'
+    }]
 });
 
 const User = model('User', userSchema);
