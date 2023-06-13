8e80ad096983d95f797d7b77780878afb7c95be1
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 01:53:13 +0300

    Update notFound.js

--- JS BackEnd/examPrep/Wildlife Photography/controllers/notFound.js
+++ JS BackEnd/examPrep/Wildlife Photography/controllers/notFound.js
@@ -1,6 +1,6 @@
 module.exports = {
     async notFound(req, res){
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        res.render('404', {title: 'Not Found', user});
+        res.render('404', {_title: 'Not Found', user});
     }
 }
