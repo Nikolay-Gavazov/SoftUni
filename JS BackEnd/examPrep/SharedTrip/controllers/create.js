2556670fcf57b9b21725a223c2b4e126defce84b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 23:13:47 +0300

    update validations

--- JS BackEnd/examPrep/SharedTrip/controllers/create.js
+++ JS BackEnd/examPrep/SharedTrip/controllers/create.js
@@ -11,51 +11,52 @@ const router = Router();
         res.render('create', { _title: 'Create Page', user });
     })
     router.post('/create',
-        body('title').trim(),
-        body('author').trim(),
-        body('keyword').trim(),
-        body('location').trim(),
-        body('date').trim(),
-        body('image').trim(),
+        body('startPoint').trim(),
+        body('endPoint').trim(),
+        body('carImage').trim(),
+        body('carBrand').trim(),
         body('description').trim(),
-        body('title', 'Title should be at least 6 characters')
-        .isLength({min: 6}),
-        body('keyword', 'Keyword should be at least 6 characters')
-        .isLength({min: 6}),
-        body('image', 'Photo should start with "http://" or "https://"')
+        body('startPoint', 'Start Point should be at least 4 characters')
+        .isLength({min: 4}),
+        body('endPoint', 'End Point should be at least 4 characters')
+        .isLength({min: 4}),
+        body('carImage', 'Photo should start with "http://" or "https://"')
         .isURL(),
-        body('date', 'Date should be exactly 10 characters.')
-        .isLength({min: 10, max: 10}),
-        body('location', 'Location  should be no longer than 15 characters.')
-        .isLength({ max: 15}),
-        body('description', 'Description should be at least 8 characters.')
-        .isLength({min: 8}),
+        body('carBrand', 'Car Brand should be at least 3 characters')
+        .isLength({min: 3, max: 15}),
+        body('price', 'Price should be positive number (from 1 to 50 inclusive)')
+        .isInt({min: 1, max: 50}),
+        body('seats', 'Seats should be positive number (from 0 to 4 inclusive)')
+        .isInt({min: 0, max: 4}),
+        body('description', 'Description should be at least 10 characters.')
+        .isLength({min: 10}),
         async(req, res) => {
         const data = req.body;
         const user = await req.storage.getUser(await req.storage.checkUser(req));
         const { errors } = validationResult(req);
-        const post = {
-            title: data.title,
-            keyword: data.keyword,
+        const trip = {
+            startPoint: data.startPoint,
+            endPoint: data.endPoint,
             date: data.date,
-            image: data.image,
-            location: data.location,
+            time: data.time,
+            carImage: data.carImage,
+            carBrand: data.carBrand,
+            seats: Number(data.seats),
+            price: Number(data.price),
             description: data.description,
-            author: {user: user._id, 
-                    fullname : `${user.firstName} ${user.lastName}`},
-            rating: 0
+            creator: {user: user._id, email: user.email},
         };
 
         try {
             if(errors.length > 0){
-                throw errors
+                throw errors;
             }
-            const result = await req.post.createItem(post);
-            await req.storage.addPostToUser(user._id, result._id);
+            const result = await req.trip.createItem(trip);
+            await req.storage.addTrip(user._id, result._id);
             res.redirect('/catalog');
         } catch (error) {
             console.log(error);
-            res.render('create', { error, user, post});
+            res.render('create', { error, user, trip});
         }
     });
 
