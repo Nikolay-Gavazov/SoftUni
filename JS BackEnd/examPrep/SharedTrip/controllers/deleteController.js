b8104f13ed294799f6756aa5cc3c649fe38fddbf
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 23:13:53 +0300

    Update deleteController.js

--- JS BackEnd/examPrep/SharedTrip/controllers/deleteController.js
+++ JS BackEnd/examPrep/SharedTrip/controllers/deleteController.js
@@ -2,7 +2,7 @@ module.exports = {
     async deletePage(req, res) {
         const id = req.params.id;
 
-        await req.post.deleteItem(id);
+        await req.trip.deleteItem(id);
 
         res.redirect('/catalog')
     }
