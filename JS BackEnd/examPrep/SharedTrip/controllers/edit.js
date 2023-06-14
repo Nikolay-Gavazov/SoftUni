44a59994b7c58b2dd6c3cecc2739ffbccd1b8ad2
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-14 23:14:16 +0300

    update validations

--- JS BackEnd/examPrep/SharedTrip/controllers/edit.js
+++ JS BackEnd/examPrep/SharedTrip/controllers/edit.js
@@ -6,51 +6,56 @@ const router = Router();
     router.get('/edit/:id', async(req, res) => {
         console.log(req);
         const id = req.params.id;
-        const post = await req.post.getById(id);
+        const trip = await req.trip.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        res.render('edit', { _title: 'Edit Page', id, post, user});
+        res.render('edit', { _title: 'Edit Page', id, trip, user});
     }),
-    router.post('/edit/:id', body('title').trim(),
-    body('author').trim(),
-    body('keyword').trim(),
-    body('location').trim(),
-    body('date').trim(),
-    body('image').trim(),
+    router.post('/edit/:id',
+    
+    body('startPoint').trim(),
+    body('endPoint').trim(),
+    body('carImage').trim(),
+    body('carBrand').trim(),
     body('description').trim(),
-    body('title', 'Title should be at least 6 characters')
-    .isLength({min: 6}),
-    body('keyword', 'Keyword should be at least 6 characters')
-    .isLength({min: 6}),
-    body('image', 'Photo should start with "http://" or "https://"')
+    body('startPoint', 'Start Point should be at least 4 characters')
+    .isLength({min: 4}),
+    body('endPoint', 'End Point should be at least 4 characters')
+    .isLength({min: 4}),
+    body('carImage', 'Photo should start with "http://" or "https://"')
     .isURL(),
-    body('date', 'Date should be exactly 10 characters.')
-    .isLength({min: 10, max: 10}),
-    body('location', 'Location  should be no longer than 15 characters.')
-    .isLength({ max: 15}),
-    body('description', 'Description should be at least 8 characters.')
-    .isLength({min: 8}),
+    body('carBrand', 'Car Brand should be at least 3 characters')
+    .isLength({min: 3, max: 15}),
+    body('price', 'Price should be positive number (from 1 to 50 inclusive)')
+    .isInt({min: 1, max: 50}),
+    body('seats', 'Seats should be positive number (from 0 to 4 inclusive)')
+    .isInt({min: 0, max: 4}),
+    body('description', 'Description should be at least 10 characters.')
+    .isLength({min: 10}),
     async(req, res) => {
     const id = req.params.id;
     const data = req.body;
     const user = await req.storage.getUser(await req.storage.checkUser(req));
     const { errors } = validationResult(req);
-    const post = {
-        title: data.title,
-        keyword: data.keyword,
+    const trip = {
+        startPoint: data.startPoint,
+        endPoint: data.endPoint,
         date: data.date,
-        image: data.image,
-        location: data.location,
+        time: data.time,
+        carImage: data.carImage,
+        carBrand: data.carBrand,
+        seats: Number(data.seats),
+        price: Number(data.price),
         description: data.description,
     };
         try {
             if(errors.length > 0){
                 throw errors;
             }
-            await req.post.editItem(id, post);
+            await req.trip.editItem(id, trip);
             res.redirect(`/details/${id}`);
         } catch (error) {
             console.log(error);
-            res.render('edit', {_title: 'Edit Page', error, user, post,id});
+            res.render('edit', {_title: 'Edit Page', error, user, trip});
         }
     });
 module.exports = router;
