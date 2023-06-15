bfc1dcf16f56981222c0dc33b54e181ddc031d63
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 23:43:00 +0300

    Update details.js

--- JS BackEnd/examPrep/gamingTeam/controllers/details.js
+++ JS BackEnd/examPrep/gamingTeam/controllers/details.js
@@ -3,8 +3,8 @@ module.exports = {
         const id = req.params.id;
         const game = await req.game.getById(id);
         const user = await req.storage.getUser(await req.storage.checkUser(req));
-        const isOwner = user._id == game.ownerId;
-        let isBougth = game.bougthBy?.filter(el => el.username == user.username);
+        const isOwner = user?._id == game.ownerId;
+        let isBougth = game.bougthBy?.filter(el => el.username == user?.username);
         if(game){
             res.render('details', {game, title: `Details - ${game.name}`, user, isOwner, isBougth});
         }else{
