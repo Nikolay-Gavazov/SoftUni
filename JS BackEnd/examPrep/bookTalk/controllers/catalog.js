616928b4d5db1086ab3d7dddf6ce5fae6c517b58
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 11:11:49 +0300

    Update catalog.js

--- JS BackEnd/examPrep/bookTalk/controllers/catalog.js
+++ JS BackEnd/examPrep/bookTalk/controllers/catalog.js
@@ -1,7 +1,7 @@
 module.exports = {
     async catalog(req, res){
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const trips = await req.storage.getAll(req.query);
-        res.render('catalog', { _title: 'Shared Trips', user, trips});
+        const books = await req.storage.getAll(req.query);
+        res.render('catalog', { _title: 'Shared Trips', user, books});
     }
 }
