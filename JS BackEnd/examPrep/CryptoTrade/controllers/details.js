c07577ec71e9b7cc083074b5bfd3c6d28519c55a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 13:27:07 +0300

    make buy function

--- JS BackEnd/examPrep/CryptoTrade/controllers/details.js
+++ JS BackEnd/examPrep/CryptoTrade/controllers/details.js
@@ -5,27 +5,27 @@ const router = Router();
 
     router.get('/details/:id', async(req, res) => {
         const id = req.params.id;
-        const book = await req.storage.getById(id);
+        const coin = await req.storage.getById(id);
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const isOwner = user?._id.toString() == book.owner.user._id.toString();
-        const isWished = await req.userStorage.getWishes(user?._id, id)
-        if(book){
-            res.render('details', { _title: 'Details Page', user,book, isOwner,isWished, id});
+        const isOwner = user?._id.toString() == coin.owner.user._id.toString();
+        const isBought = await req.storage.getBuyers(id, user?._id)
+        if(coin){
+            res.render('details', { _title: 'Details Page', user,coin, isOwner,isBought, id});
         }else{
             res.redirect('/404');
         }
     });
 
-    router.get('/details/:id/wish', async (req, res) => {
+    router.get('/details/:id/buy', async (req, res) => {
         const id = req.params.id;
-        const book = await req.storage.getById(id);
+        const coin = await req.storage.getById(id);
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
         try {
-            await req.userStorage.wish(user._id, id);
+            await req.storage.buy(id, user._id);
             res.redirect(`/details/${id}`)
         } catch (error) {
             console.log(error);
-            res.render('details', { _title: 'Details Page', error: error.message, book});
+            res.render('details', { _title: 'Details Page', error: error.message, coin});
         }
     });
 
