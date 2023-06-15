3db39d247e9110d5160eae1d37055f2f34d54f02
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 11:16:13 +0300

    update validations

--- JS BackEnd/template/controllers/create.js
+++ JS BackEnd/template/controllers/create.js
@@ -11,52 +11,46 @@ const router = Router();
         res.render('create', { _title: 'Create Page', user });
     })
     router.post('/create',
-        body('startPoint').trim(),
-        body('endPoint').trim(),
-        body('carImage').trim(),
-        body('carBrand').trim(),
-        body('description').trim(),
-        body('startPoint', 'Start Point should be at least 4 characters')
-        .isLength({min: 4}),
-        body('endPoint', 'End Point should be at least 4 characters')
-        .isLength({min: 4}),
-        body('carImage', 'Photo should start with "http://" or "https://"')
+        body('title').trim(),
+        body('author').trim(),
+        body('genre').trim(),
+        body('image').trim(),
+        body('review').trim(),
+        body('title', 'Title should be at least 2 characters')
+        .isLength({min: 2}),
+        body('author', 'Author should be at least 5 characters')
+        .isLength({min: 5}),
+        body('image', 'Genre should start with "http://" or "https://"')
         .isURL(),
-        body('carBrand', 'Car Brand should be at least 3 characters')
-        .isLength({min: 3, max: 15}),
-        body('price', 'Price should be positive number (from 1 to 50 inclusive)')
-        .isInt({min: 1, max: 50}),
-        body('seats', 'Seats should be positive number (from 0 to 4 inclusive)')
-        .isInt({min: 0, max: 4}),
-        body('description', 'Description should be at least 10 characters.')
+        body('genre', 'Genre should be at least 3 characters')
+        .isLength({min: 3}),
+        body('stars', 'Stars should be positive number (from 1 to 5 inclusive)')
+        .isInt({min: 1, max: 5}),
+        body('review', 'Review should be at least 10 characters.')
         .isLength({min: 10}),
         async(req, res) => {
         const data = req.body;
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
         const { errors } = validationResult(req);
-        const trip = {
-            startPoint: data.startPoint,
-            endPoint: data.endPoint,
-            date: data.date,
-            time: data.time,
-            carImage: data.carImage,
-            carBrand: data.carBrand,
-            seats: Number(data.seats),
-            price: Number(data.price),
-            description: data.description,
-            creator: {user: user._id, email: user.email},
+        const book = {
+            title: data.title,
+            author: data.author,
+            genre: data.genre,
+            stars: Number(data.stars),
+            image: data.image,
+            review: data.review,
+            owner: {user: user._id, email: user.email},
         };
 
         try {
             if(errors.length > 0){
                 throw errors;
             }
-            const result = await req.storage.createItem(trip);
-            await req.storage.addTrip(user._id, result._id);
+            const result = await req.storage.createItem(book);
             res.redirect('/catalog');
         } catch (error) {
             console.log(error);
-            res.render('create', { error, user, trip});
+            res.render('create', { error, user, book});
         }
     });
 
