681ed11b875b371186ac3404b166503d170fcc3a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-16 02:08:22 +0300

    Update AdService.js

--- JS BackEnd/examPrep/JobAds/data/AdService.js
+++ JS BackEnd/examPrep/JobAds/data/AdService.js
@@ -3,7 +3,7 @@ const Ad = require('../models/Ad');
 async function getAll(query) {
     const options = {};
     if (query.name) {
-        options.name = new RegExp(query.name, 'i');
+        options.name = {email: new RegExp(query.name, 'i')};
     }
     if (query.platform) {
         if (!options.platform) {
@@ -11,26 +11,24 @@ async function getAll(query) {
         }
         options.platform = new RegExp(query.platform, 'i');
     }
-    if(query.isOpen){
+    if (query.isOpen) {
         options.isOpen = true;
     }
-    
-    const data = await Ad.find(options).lean();
-    if(data){
+    const data = await Ad.find(options).lean().populate('author');
+    if (data) {
         return data;
     }
     return null;
 };
 
 async function getById(id) {
-    const data = await Ad.findById(id).lean();
-    // const data = await Trip.findById(id).lean().populate('ownerId');
-    // const data2 = await Item.findById(id).lean().populate('author');
-    // data['author'] = data2.author;
+    const data = await Ad.findById(id).lean().populate('author');
+    const data2 = await Ad.findById(id).lean().populate('applied');
 
-    if (data) {
-        return data;
-    } else {
+    data['applied'] = data2.applied;
+if(data) {
+    return data;
+} else {
         return undefined;
     }
 };
@@ -47,27 +45,19 @@ async function editItem(id, data) {
     await Ad.findByIdAndUpdate(id, data);
 }
 
-// async function bid(id, user, amount) {
-//     const item = await Item.findById(id);
-//     item.winner = `${user.firstName} ${user.lastName}`
-//     item.bidder[0] = user._id;
-//     item.price = Number(amount);
-//     await item.save();
-// }
+async function apply(id, userId) {
+    const ad = await Ad.findById(id);
+    ad.applied.push(userId)
+    await ad.save();
+};
 
-// async function close(id) {
-//     const item = await Item.findById(id);
-//     item.isOpen = false;
-//     await item.save();
-// }
-
-// async function getBidder(id, userId){
-//     const item = await Item.findById(id).lean();
-//     if(item && item.bidder.length > 0){
-//         return item.bidder.some(el => el._id.toString() == userId?.toString());
-//     };
-//     return undefined;
-// }
+async function getApply(id, userId) {
+    const ad = await Ad.findById(id).lean();
+    if (ad && ad.applied.length > 0) {
+        return ad.applied.some(el => el._id.toString() == userId?.toString());
+    };
+    return undefined;
+};
 
 
 module.exports = () => (req, res, next) => {
@@ -77,9 +67,8 @@ module.exports = () => (req, res, next) => {
         createItem,
         deleteItem,
         editItem,
-        // bid,
-        // getBidder,
-        // close
-    };
-    next();
-}
+        apply,
+        getApply
+    }
+    next()
+};
