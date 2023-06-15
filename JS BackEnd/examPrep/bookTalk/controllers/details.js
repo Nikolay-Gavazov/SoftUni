198afa10f676941fbed1e1e0fc7d5e9fc069b17b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 11:12:07 +0300

    Update details.js

--- JS BackEnd/examPrep/bookTalk/controllers/details.js
+++ JS BackEnd/examPrep/bookTalk/controllers/details.js
@@ -5,28 +5,27 @@ const router = Router();
 
     router.get('/details/:id', async(req, res) => {
         const id = req.params.id;
-        const trip = await req.storage.getById(id);
+        const book = await req.storage.getById(id);
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const isOwner = user?._id.toString() == trip.creator.user._id.toString();
-        const isJoined = await req.storage.getJoinedUser(id, user?._id)
-        const noSeats = trip.seats == 0;
-        if(trip){
-            res.render('details', { _title: 'Details Page', user,trip, isOwner, isJoined, id, noSeats});
+        const isOwner = user?._id.toString() == book.owner.user._id.toString();
+        const isWished = await req.userStorage.getWishes(user?._id, id)
+        if(book){
+            res.render('details', { _title: 'Details Page', user,book, isOwner,isWished, id});
         }else{
             res.redirect('/404');
         }
     });
 
-    router.get('/details/:id/join', async (req, res) => {
+    router.get('/details/:id/wish', async (req, res) => {
         const id = req.params.id;
-        const trip = await req.storage.getById(id);
+        const book = await req.storage.getById(id);
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
         try {
-            await req.storage.join(id, user._id);
+            await req.userStorage.wish(user._id, id);
             res.redirect(`/details/${id}`)
         } catch (error) {
             console.log(error);
-            res.render('details', { _title: 'Details Page', error: error.message, trip});
+            res.render('details', { _title: 'Details Page', error: error.message, book});
         }
     });
 
