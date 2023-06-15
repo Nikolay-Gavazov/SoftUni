55f70535b13e51482f66961276187e991c5ac497
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 13:26:04 +0300

    change model and db connection

--- JS BackEnd/examPrep/CryptoTrade/data/database.js
+++ JS BackEnd/examPrep/CryptoTrade/data/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionStr = 'mongodb://127.0.0.1:27017/BookTalk';
+const connectionStr = 'mongodb://127.0.0.1:27017/CryptoTrade';
 
-const Book = require('../models/Book');
+const Coin = require('../models/Coin');
 const User = require('../models/User');
 
 async function init() {
