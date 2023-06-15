47e54dc20aba63fb74d970d48effb3095c0b9555
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-16 02:08:16 +0300

    Update home.js

--- JS BackEnd/examPrep/JobAds/controllers/home.js
+++ JS BackEnd/examPrep/JobAds/controllers/home.js
@@ -1,7 +1,7 @@
 module.exports = {
     async home(req, res){
         const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const jobs = await req.storage.getAll(req.query)
-        res.render('home', {_title: 'Home Page', user, jobs});
+        const ads = await req.storage.getAll(req.query)
+        res.render('home', {_title: 'Home Page', user, ads});
     }
 }
