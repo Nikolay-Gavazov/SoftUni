69456b78840f739c5be1a65c6b4e8167b9ff5329
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 18:29:19 +0300

    Create itemService.js

---
+++ JS BackEnd/examPrep/AuctionHouse/data/itemService.js
@@ -0,0 +1,85 @@
+const Item = require('../models/Item');
+
+async function getAll(query) {
+    const options = {};
+    if (query.name) {
+        options.name = new RegExp(query.name, 'i');
+    }
+    if (query.platform) {
+        if (!options.platform) {
+            options.platform = {};
+        }
+        options.platform = new RegExp(query.platform, 'i');
+    }
+    if(query.isOpen){
+        options.isOpen = true;
+    }
+    
+    const data = await Item.find(options).lean();
+    if(data){
+        return data;
+    }
+    return null;
+};
+
+async function getById(id) {
+    const data = await Item.findById(id).lean().populate('bidder');
+    // const data = await Trip.findById(id).lean().populate('ownerId');
+    const data2 = await Item.findById(id).lean().populate('author');
+    data['author'] = data2.author;
+
+    if (data) {
+        return data;
+    } else {
+        return undefined;
+    }
+};
+
+async function createItem(data) {
+    return await Item.create(data);
+};
+
+async function deleteItem(id) {
+    await Item.findByIdAndDelete(id);
+};
+
+async function editItem(id, data) {
+    await Item.findByIdAndUpdate(id, data);
+}
+
+async function bid(id, user, amount) {
+    const item = await Item.findById(id);
+    item.winner = `${user.firstName} ${user.lastName}`
+    item.bidder[0] = user._id;
+    item.price = Number(amount);
+    await item.save();
+}
+
+async function close(id) {
+    const item = await Item.findById(id);
+    item.isOpen = false;
+    await item.save();
+}
+
+async function getBidder(id, userId){
+    const item = await Item.findById(id).lean();
+    if(item && item.bidder.length > 0){
+        return item.bidder.some(el => el._id.toString() == userId?.toString());
+    };
+    return undefined;
+}
+
+
+module.exports = () => (req, res, next) => {
+    req.storage = {
+        getAll,
+        getById,
+        createItem,
+        deleteItem,
+        editItem,
+        bid,
+        getBidder,
+        close
+    };
+    next();
+}
