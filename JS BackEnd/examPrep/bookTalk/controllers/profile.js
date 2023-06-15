9985b08b232eba7e789e593d357e7983ea7864c6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-15 11:12:20 +0300

    Update profile.js

--- JS BackEnd/examPrep/bookTalk/controllers/profile.js
+++ JS BackEnd/examPrep/bookTalk/controllers/profile.js
@@ -4,9 +4,9 @@ const router = Router();
 
 router.get('/profile', async (req, res) => {
     const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-    const myTrips = user.tripsHistory;
+    const myWish = user.wishingList;
 
-    res.render('profile', { _title: 'Profile Page', user, myTrips})
+    res.render('profile', { _title: 'Profile Page', user, myWish})
 });
 
 module.exports = router;
