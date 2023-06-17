ca51286a2f1058844d22d29c7560b4cc738bbb28
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-17 13:07:47 +0300

    update connection

--- JS BackEnd/examPrep/ArtGallery/data/database.js
+++ JS BackEnd/examPrep/ArtGallery/data/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionStr = 'mongodb://127.0.0.1:27017/RealEstateAgency';
+const connectionStr = 'mongodb://127.0.0.1:27017/ArtGallery';
 
-const Home = require('../models/Home');
+const Publication = require('../models/Publication');
 const User = require('../models/User');
 
 async function init() {
