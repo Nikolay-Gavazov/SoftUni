b01fe30dd16d434cb5d3828091a43ece7e5ed181
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-17 13:06:29 +0300

    Update details.js

--- JS BackEnd/examPrep/ArtGallery/controllers/details.js
+++ JS BackEnd/examPrep/ArtGallery/controllers/details.js
@@ -1,34 +1,34 @@
 const { Router } = require('express');
+const { isAuth, autMidd } = require('../middlewares/authMidd');
 
 const router = Router();
 
+router.get('/details/:id', async (req, res) => {
+    const id = req.params.id;
+    const publication = await req.storage.getById(id);
+    const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
+    const isOwner = user?._id.toString() == publication.author._id.toString();
+    const isShared = await req.storage.getShare(id, user?._id);
+    // const tenants = home.rented.map(el => el.fullName);
+    if (publication) {
+        res.render('details', { _title: 'Details Page', user, publication, isOwner, isShared });
+    } else {
+        res.redirect('/404');
+    }
+});
 
-    router.get('/details/:id', async(req, res) => {
-        const id = req.params.id;
-        const home = await req.storage.getById(id);
-        const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const isOwner = user?._id.toString() == home.owner._id.toString();
-        const isApplied = await req.storage.getApply(id, user?._id);
-        const tenants = home.rented.map(el => el.fullName);
-        console.log(tenants);
-        if(home){  
-         res.render('details', { _title: 'Details Page', user,home,isOwner, isApplied, tenants: tenants.join(', ')});   
-        }else{
-            res.redirect('/404');
-        }
-    });
+router.get('/details/:id/share', autMidd, isAuth, async (req, res) => {
+    const id = req.params.id;
+    const publication = await req.storage.getById(id);
+    const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
+    try {
+        await req.storage.share(id, user?._id);
+        await req.userStorage.share(user?._id, id);
+        res.redirect(`/`)
+    } catch (error) {
+        console.log(error);
+        res.render('details', { _title: 'Details Page', error, publication });
+    }
+});
 
-    router.get('/details/:id/rent', async (req, res) => {
-        const id = req.params.id;
-        const home = await req.storage.getById(id);
-        const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        try {
-            await req.storage.rent(id,user._id);
-            res.redirect(`/details/${id}`)
-        } catch (error) {
-            console.log(error);
-            res.render('details', { _title: 'Details Page', error, home});
-        }
-    });
-
-    module.exports = router;
+module.exports = router;
