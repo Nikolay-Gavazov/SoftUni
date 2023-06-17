85333f3df87473212007533f3800059e6debf1c4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-17 13:06:51 +0300

    add try-catch

--- JS BackEnd/examPrep/ArtGallery/controllers/home.js
+++ JS BackEnd/examPrep/ArtGallery/controllers/home.js
@@ -1,7 +1,12 @@
 module.exports = {
     async home(req, res){
-        const user = await req.userStorage.getUser(await req.userStorage.checkUser(req));
-        const data = await req.storage.getAll(req.query)
-        res.render('home', {_title: 'Home Page', user, data});
+        try {
+            const data = await req.storage.getAll(req.query)
+        res.render('home', {_title: 'Home Page', user:req.user, data});
+        } catch (error) {
+            console.log(error);
+            res.render('home', {_title: 'Home Page', user:req.user, data, error});
+        }
+    
     }
 }
