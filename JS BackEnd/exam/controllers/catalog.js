d49f6823b16267ead53024d19c8b197e38edcb92
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-19 10:44:38 +0300

    Update catalog.js

--- JS BackEnd/exam/controllers/catalog.js
+++ JS BackEnd/exam/controllers/catalog.js
@@ -2,10 +2,10 @@ module.exports = {
     async catalog(req, res) {
         try {
             const data = await req.storage.getAll(req.query);
-            res.render('gallery', { _title: 'Gallery Page', user: req.user, data });
+            res.render('dashboard', { _title: 'Dashboard Page', user: req.user, data });
         } catch (error) {
             console.log(error);
-            res.render('gallery', { _title: 'Gallery Page', user: req.user, data, error });
+            res.render('dashboard', { _title: 'Dashboard Page', user: req.user, data, error });
         }
     }
 }
