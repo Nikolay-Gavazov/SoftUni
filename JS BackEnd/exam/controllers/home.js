f23153eca0c43f729a437bc6e1d6ace9a5098b7d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-19 10:44:51 +0300

    Update home.js

--- JS BackEnd/exam/controllers/home.js
+++ JS BackEnd/exam/controllers/home.js
@@ -1,7 +1,8 @@
 module.exports = {
     async home(req, res){
         try {
-            const data = await req.storage.getAll(req.query)
+            const animals = await req.storage.getAll(req.query)
+            const data = animals.slice(-3).reverse();
         res.render('home', {_title: 'Home Page', user:req.user, data});
         } catch (error) {
             console.log(error);
