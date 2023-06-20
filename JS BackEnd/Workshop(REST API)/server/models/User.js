94dda982d82ef33ca659ec78baa690be583291d8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-20 22:49:58 +0300

    Create User.js

---
+++ JS BackEnd/Workshop(REST API)/server/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save',async function() {
+    const hash = await bcrypt.hash(this.password, 5);
+    this.password = hash;
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
