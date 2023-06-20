843284ac9854df0fcc65c63ccac89ebd81843eb6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-21 01:14:30 +0300

    Update furniture.js

--- JS BackEnd/Workshop(REST API)/server/controllers/furniture.js
+++ JS BackEnd/Workshop(REST API)/server/controllers/furniture.js
@@ -1,7 +1,68 @@
 const router = require('express').Router();
 
-router.post('/', (req, res) => {
-    
+const furnitureService = require('../Services/furnitureService');
+const { auth } = require('../middlewares/authMiddleware');
+
+router.post('/', auth, async (req, res) => {
+    try {
+        await furnitureService.create({
+            ...req.body,
+            _ownerId: req.user._id
+        });
+        res.status(201).end();
+    } catch (error) {
+        res.status(400).json({
+            message: 'Connot create furniture'
+        })
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const furnitures = await furnitureService.getAll(req.query);
+        console.log(furnitures);
+        res.status(200).json(furnitures);
+    } catch (error) {
+        res.status(400).json({
+            message: 'Connot find furniture'
+        })
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const furniture = await furnitureService.findById(req.params.id);
+
+        res.status(200).json(furniture);
+    } catch (error) {
+        res.status(400).json({
+            message: 'Connot find furniture'
+        })
+    }
+});
+
+router.put('/:id', auth, async (req, res) => {
+    try {
+        const furniture = await furnitureService.edit(req.params.id, req.body);
+
+        res.status(200).json(furniture);
+    } catch (error) {
+        res.status(400).json({
+            message: 'Connot edit furniture'
+        })
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        await furnitureService.del(req.params.id);
+
+        res.status(200).end();
+    } catch (error) {
+        res.status(400).json({
+            message: 'Connot delete furniture'
+        })
+    }
 })
 
 module.exports = router;
