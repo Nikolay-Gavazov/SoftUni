334dfa0a84567978c053c189841b388be4fd0420
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-20 22:50:24 +0300

    create furniture model

---
+++ JS BackEnd/Workshop(REST API)/server/models/Furniture.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const furnitureSchema = new mongoose.Schema({
+    make: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    material: {
+        type: String,
+    }
+});
+
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+module.exports = Furniture;
