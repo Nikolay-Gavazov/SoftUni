544b98570a0eb56035f7361bf9755ee0d7305ada
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-22 00:26:47 +0300

    Update test.js

--- Angular/1.Lecture/test.js
+++ Angular/1.Lecture/test.js
@@ -1,1 +1,14 @@
 var isOpen = true;
+var age = 21;
+var firstName = 'Ivan';
+var users = { id: 12 };
+var ivanUser = {
+    name: 'Ivan',
+    age: 30,
+};
+var userList = [
+    { lastName: 'Ivanov', id: 1 },
+    { lastName: 'Petrov', id: 2 },
+    { lastName: 'Dimitrov', id: 3 },
+];
+ivanUser.age = 22;
