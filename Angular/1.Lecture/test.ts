8a84caf3a47332f7295b7995c6f79dec353e776c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-22 00:26:51 +0300

    Update test.ts

--- Angular/1.Lecture/test.ts
+++ Angular/1.Lecture/test.ts
@@ -1,1 +1,40 @@
 const isOpen: boolean = true;
+const age: number = 21;
+const firstName: string = 'Ivan';
+const users: object = {id: 12};
+const numbers: number[] = [1, 2, 3];
+
+type User = {
+    name: string;
+    age: number
+}
+
+const ivanUser = {
+    name: 'Ivan',
+    age: 30,
+} as User
+
+interface AnotherUser{
+    lastName: string,
+    id: number
+}
+
+const userList = [
+    {lastName: 'Ivanov', id: 1},
+    {lastName: 'Petrov', id: 2},
+    {lastName: 'Dimitrov', id: 3},
+] as AnotherUser[];
+
+
+class Person {
+    private name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const personMitko = new Person('Mitko', 20);
+
