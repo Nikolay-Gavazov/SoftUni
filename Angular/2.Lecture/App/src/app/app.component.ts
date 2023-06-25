0b5cb45e55b0d0492c1007a7f78d0a52619accfc
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-25 17:08:56 +0300

    Update app.component.ts

--- Angular/2.Lecture/App/src/app/app.component.ts
+++ Angular/2.Lecture/App/src/app/app.component.ts
@@ -3,29 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'User Create'
-  buttonOnClick(inputName: HTMLInputElement, inputAge: HTMLInputElement) :void {
+  title = 'User Create';
+  buttonOnClick(inputName: HTMLInputElement, inputAge: HTMLInputElement): void {
     console.log(inputName, inputAge);
-    
+
     this.users.push({
       name: inputName.value as string,
-      age: Number(inputAge.value) as number
+      age: Number(inputAge.value) as number,
     });
   }
- 
+
   users = [
     {
       name: 'Goshko' as string,
-      age: 22 as number
-    }
-  ]
+      age: 22 as number,
+    },
+  ];
   showText = true;
-  toggleText(event: MouseEvent) :void {
+  toggleText(event: MouseEvent): void {
     console.log(event);
-    
+
     this.showText = !this.showText;
   }
 }
