f0dd7ff9e9c884b87888c17006089e9719492a8d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-26 13:23:16 +0300

    Create lab.component.ts

---
+++ Angular/2.Lecture/App/src/lab/lab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lab-root',
+  templateUrl: './lab.component.html',
+  styleUrls: ['./lab.component.css']
+})
+export class LabComponent {
+  title = 'Lab-Component'
+
+}
