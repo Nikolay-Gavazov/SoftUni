4bc5a7032dcb611dffa1b16a1c4c66c5847c92c5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-26 13:22:23 +0300

    Update app.module.ts

--- Angular/2.Lecture/App/src/app/app.module.ts
+++ Angular/2.Lecture/App/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LabComponent } from '../lab/lab.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LabComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
