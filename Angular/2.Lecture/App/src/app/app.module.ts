02b1a2ab624f51c5344201eb3550065d374e8d4b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-27 23:39:22 +0300

    Update app.module.ts

--- Angular/2.Lecture/App/src/app/app.module.ts
+++ Angular/2.Lecture/App/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LabComponent } from '../lab/lab.component';
 
 @NgModule({
-  declarations: [AppComponent, LabComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class Article {
+  constructor(
+   public title: string,
+   public description: string,
+   public author: string,
+   public imageUrl: string
+  )
+  {}
+}
