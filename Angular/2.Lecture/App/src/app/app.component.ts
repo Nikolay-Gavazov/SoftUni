0802e498ea1967473aab346800aee5ada941f71f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-06-27 23:39:10 +0300

    Update app.component.ts

--- Angular/2.Lecture/App/src/app/app.component.ts
+++ Angular/2.Lecture/App/src/app/app.component.ts
@@ -1,31 +1,46 @@
 import { Component } from '@angular/core';
-
+import { data } from './data';
+import { Article } from './app.module';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'User Create';
-  buttonOnClick(inputName: HTMLInputElement, inputAge: HTMLInputElement): void {
-    console.log(inputName, inputAge);
-
-    this.users.push({
-      name: inputName.value as string,
-      age: Number(inputAge.value) as number,
-    });
+    private symbols : number = 250;
+    @Input() article: Article;
+    @Input() articleDesc: string;
+    descToShow: string;
+    articleDescLen: number;
+    showReadMoreBtn: boolean = true;
+    showHideBtn: boolean = false;
+    imageIsShown: boolean = false;
+    imageButtonTitle: string = 'Show Image';
+    constructor(){
+      this.articleDescLen = 0;
+      this.descToShow = '';
+    }
   }
+  // title = 'User Create';
+  // buttonOnClick(inputName: HTMLInputElement, inputAge: HTMLInputElement): void {
+  //   console.log(inputName, inputAge);
 
-  users = [
-    {
-      name: 'Goshko' as string,
-      age: 22 as number,
-    },
-  ];
-  showText = true;
-  toggleText(event: MouseEvent): void {
-    console.log(event);
+  //   this.users.push({
+  //     name: inputName.value as string,
+  //     age: Number(inputAge.value) as number,
+  //   });
+  // }
 
-    this.showText = !this.showText;
-  }
+  // users = [
+  //   {
+  //     name: 'Goshko' as string,
+  //     age: 22 as number,
+  //   },
+  // ];
+  // showText = true;
+  // toggleText(event: MouseEvent): void {
+  //   console.log(event);
+
+  //   this.showText = !this.showText;
+  // }
 }
