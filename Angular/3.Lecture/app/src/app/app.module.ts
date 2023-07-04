9fde1188789b8bc81a554782eebcd1e665714224
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-04 10:52:15 +0300

    Update app.module.ts

--- Angular/3.Lecture/app/src/app/app.module.ts
+++ Angular/3.Lecture/app/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
