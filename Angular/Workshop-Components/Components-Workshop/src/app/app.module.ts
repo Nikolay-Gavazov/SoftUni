6c615d699d53b6411d9681e853b59466907fb8aa
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-05 13:31:02 +0300

    add Core, Http, Shared modules

--- Angular/Workshop-Components/Components-Workshop/src/app/app.module.ts
+++ Angular/Workshop-Components/Components-Workshop/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import {HttpClientModule} from '@angular/common/http'
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
