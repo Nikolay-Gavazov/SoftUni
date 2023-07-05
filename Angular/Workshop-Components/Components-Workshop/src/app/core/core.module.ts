649bb1a92daf2f9d5e38465943e35769e6f596df
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-05 13:31:08 +0300

    Update core.module.ts

--- Angular/Workshop-Components/Components-Workshop/src/app/core/core.module.ts
+++ Angular/Workshop-Components/Components-Workshop/src/app/core/core.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
 
@@ -9,7 +8,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     HeaderComponent,
-    MainComponent,
     FooterComponent
   ],
   imports: [
@@ -17,7 +15,6 @@ import { FooterComponent } from './footer/footer.component';
   ],
   exports:[
     HeaderComponent,
-    MainComponent,
     FooterComponent
   ]
 })
