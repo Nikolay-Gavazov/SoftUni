15aa66ca2f443d62be16cc05873f4474ed7de83d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-09 22:29:33 +0300

    Update app-routing.module.ts

--- Angular/Workshop2/WorkShop-Routing/src/app/app-routing.module.ts
+++ Angular/Workshop2/WorkShop-Routing/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './theme/create/create.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent,
+    component: HomeComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: '**',
