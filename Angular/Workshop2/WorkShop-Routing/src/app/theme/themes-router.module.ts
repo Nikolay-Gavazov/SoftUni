64563f831b595b75f6545f86d54435e5e917b2aa
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-10 19:23:33 +0300

    Update themes-router.module.ts

--- Angular/Workshop2/WorkShop-Routing/src/app/theme/themes-router.module.ts
+++ Angular/Workshop2/WorkShop-Routing/src/app/theme/themes-router.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './details/details.component';
 import { MainComponent } from '../main/main.component';
+import { AuthActivate } from '../core/guards/auth.activate';
+import { DetailsComponent } from './details/details.component';
 import { CreateComponent } from './create/create.component';
 
 
@@ -9,15 +10,18 @@ const routes: Routes = [
     {
         path: 'themes',
         component: MainComponent,
-      },
-      {
-        path: 'themes/create',
-        component: CreateComponent,
-      },
+    },
     {
-        path: 'themes/details/:id',
-        component: DetailsComponent
-    }
+        path: 'themes/:id',
+        component: DetailsComponent,
+        /* canActivate:[AuthActivate] */
+    },
+    {
+        path: 'create',
+        component: CreateComponent,
+        /* canActivate:[AuthActivate] */
+    },
+    
 ]
 
 export const ThemeRoutingModule = RouterModule.forChild(routes);
