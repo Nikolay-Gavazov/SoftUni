e7f328b99740d11dec90b30518f6990c793b65e3
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-10 19:19:38 +0300

    Update main.component.ts

--- Angular/Workshop2/WorkShop-Routing/src/app/main/main.component.ts
+++ Angular/Workshop2/WorkShop-Routing/src/app/main/main.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements DoCheck{
 
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngDoCheck(): void {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
 }
