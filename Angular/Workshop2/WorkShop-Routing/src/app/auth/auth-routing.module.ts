d868dc2f436855f50cfaa96304b74172a22942d8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-10 19:11:21 +0300

    add canActivate

--- Angular/Workshop2/WorkShop-Routing/src/app/auth/auth-routing.module.ts
+++ Angular/Workshop2/WorkShop-Routing/src/app/auth/auth-routing.module.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { AuthActivate } from "../core/guards/auth.activate";
 
 const routes: Routes = [
-    { path: 'auth/login', component: LoginComponent},
-    { path: 'auth/register', component: RegisterComponent},
+    { path: 'auth/login', component: LoginComponent, /* canActivate:[AuthActivate] */},
+    { path: 'auth/register', component: RegisterComponent, /* canActivate:[AuthActivate] */},
     { path: 'auth/logout', component: LogoutComponent},
-    { path: 'auth/profile', component: ProfileComponent},
+    { path: 'auth/profile', component: ProfileComponent, /* canActivate:[AuthActivate] */},
     
 ]
 
