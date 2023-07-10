eafcb1779095559cdb23c3400b760e5b27e6f2ee
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-10 19:12:54 +0300

    make static login

--- Angular/Workshop2/WorkShop-Routing/src/app/auth/auth.service.ts
+++ Angular/Workshop2/WorkShop-Routing/src/app/auth/auth.service.ts
@@ -1,13 +1,37 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   user: User | null = null;
-  get isLoggedIn(){
-    return this.user !=null;
+  USER_KEY = '[user]'
+ 
+constructor() { 
+  try {
+    const user = localStorage.getItem(this.USER_KEY) || '';
+    this.user = JSON.parse(user);
+    } catch (error) {
+      this.user = null;
+    }
   }
-  constructor() { }
+
+  login(): void{
+    this.user = {
+      "email": "peshoKioravia@gmail",
+      "firstName": "Pesho",
+      "phoneNumber": "+123456789",
+      "passWord": "<PASSWORD>"
+    };
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user)); 
+  }
+  logout(): void{
+    this.user = null;
+    localStorage.removeItem(this.USER_KEY);
+  }
+  get isLoggedIn(){
+    return !!this.user
+  }
 }
