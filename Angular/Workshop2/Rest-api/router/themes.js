2a32114345f24943a31de35c6e46ef3fda65eb9b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-11 23:57:08 +0300

    Update themes.js

--- Angular/Workshop2/Rest-api/router/themes.js
+++ Angular/Workshop2/Rest-api/router/themes.js
@@ -6,10 +6,10 @@ const { themeController, postController } = require('../controllers');
 // middleware that is specific to this router
 
 router.get('/', themeController.getThemes);
-router.post('/', auth(), themeController.createTheme);
+router.post('/', /* auth(), */ themeController.createTheme);
 
 router.get('/:themeId', themeController.getTheme);
-router.post('/:themeId', auth(), postController.createPost);
+router.post('/:themeId', /* auth(), */ postController.createPost);
 router.put('/:themeId', auth(), themeController.subscribe);
 router.put('/:themeId/posts/:postId', auth(), postController.editPost);
 router.delete('/:themeId/posts/:postId', auth(), postController.deletePost);
