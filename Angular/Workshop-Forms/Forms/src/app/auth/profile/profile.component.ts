4c6f447a207b5c2e20c4e97ef6842d21ea73f72d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-16 17:46:00 +0300

    add edit function and validations

--- Angular/Workshop-Forms/Forms/src/app/auth/profile/profile.component.ts
+++ Angular/Workshop-Forms/Forms/src/app/auth/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  @ViewChild('profileForm') profileForm!: NgForm;
+  showEdit = false;
+
+  constructor(){}
+  toggle():void {
+    this.showEdit =!this.showEdit;
+  }
+  onSubmit(profileForm: NgForm):void {
+    if(profileForm.invalid){
+      return;
+    }
+    profileForm.reset();
+    this.toggle()
+  }
 }
