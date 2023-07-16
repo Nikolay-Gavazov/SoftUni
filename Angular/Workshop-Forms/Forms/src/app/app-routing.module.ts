9f2d99a014e4650e30eac999e8d64812345375af
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-16 17:43:59 +0300

    clean from imports

--- Angular/Workshop-Forms/Forms/src/app/app-routing.module.ts
+++ Angular/Workshop-Forms/Forms/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
-import { CreateComponent } from './theme/create/create.component';
 
 const routes: Routes = [
   {
