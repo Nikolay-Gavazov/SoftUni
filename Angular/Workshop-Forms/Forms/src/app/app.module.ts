a52fd8cc92a6d0fcbb9d39c3336371cd91b6ea77
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-16 17:44:33 +0300

    add and remove imports

--- Angular/Workshop-Forms/Forms/src/app/app.module.ts
+++ Angular/Workshop-Forms/Forms/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
-import { PostsListComponent } from './posts-list/posts-list.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { HomeComponent } from './home/home.component';
@@ -14,14 +14,14 @@ import { CreateComponent } from './theme/create/create.component';
 import { ThemeModule } from './theme/theme.module';
 import { AppRoutingModule } from './app-routing.module';
 import { DetailsComponent } from './theme/details/details.component';
-import { FormsModule } from '@angular/forms';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    PostsListComponent,
     HomeComponent,
     CreateComponent,
     DetailsComponent,
@@ -35,7 +35,7 @@ import { FormsModule } from '@angular/forms';
     SharedModule,
     ThemeModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
