52e3b30d73f8291382962d327e1a79c29d95b591
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-17 15:36:19 +0300

    make edit func

--- Angular/Workshop-Forms/Forms/src/app/auth/profile/profile.component.ts
+++ Angular/Workshop-Forms/Forms/src/app/auth/profile/profile.component.ts
@@ -1,12 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  userData: any = {
+    username: 'Pesho',
+    email: 'pesho1@gmail.com',
+    phone: '887 215 454'
+  }
+  emailDomains = DEFAULT_EMAIL_DOMAINS;
   @ViewChild('profileForm') profileForm!: NgForm;
   showEdit = false;
 
@@ -18,6 +24,10 @@ export class ProfileComponent {
     if(profileForm.invalid){
       return;
     }
+    this.userData.username = profileForm.value.username;
+    this.userData.email = profileForm.value.email;
+    this.userData.phone = profileForm.value.tel;
+    
     profileForm.reset();
     this.toggle()
   }
