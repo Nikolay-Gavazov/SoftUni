3aa577c62c7f0de9e6d548c63da31d5c392e8304
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-17 15:37:06 +0300

    Update register.component.ts

--- Angular/Workshop-Forms/Forms/src/app/auth/register/register.component.ts
+++ Angular/Workshop-Forms/Forms/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  emailDomains = DEFAULT_EMAIL_DOMAINS;
+  
 
+  register(form: NgForm){
+    
+  }
 }
