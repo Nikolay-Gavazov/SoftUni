68741f243ef6b651511ae92926281d50963ad2fc
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-19 15:26:35 +0300

    Update app.component.ts

--- Angular/6.Lecture(Pipes,Interceptors and Subjects)/Lecture/src/app/app.component.ts
+++ Angular/6.Lecture(Pipes,Interceptors and Subjects)/Lecture/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- user = {name: 'John', email: 'peshom@gmail.com'};
+ title(title: any) {
+   throw new Error('Method not implemented.');
+ }
+ user = {name: 'John', email: 'peshom@gmail.com', list:[1,2,3,4,5,6, 7]};
+
+ sum(a: number, b:number){
+  return a + b;
+ }
+
+ addProperty(): void {
+  (this.user as any)['test123'] = 'test123';
+  console.log(this.user);
+  
+ }
 }
