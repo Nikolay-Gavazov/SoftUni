9c67d5777cea0e2edb84889ac983f7b01160ed7a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-20 13:48:10 +0300

    Update app.module.ts

--- Angular/6.Lecture(Pipes,Interceptors and Subjects)/Lecture/src/app/app.module.ts
+++ Angular/6.Lecture(Pipes,Interceptors and Subjects)/Lecture/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReducePipe } from './reduce.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { AppInterceptorProvider } from './app.interceptor';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { ReducePipe } from './reduce.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AppInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
