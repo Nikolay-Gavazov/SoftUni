f54b7ba3252a6cdd0ba04cf26ca78e53b27bf878
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-22 19:03:16 +0300

    create exam project

---
+++ Angular/exam/SozopoliStudios/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
