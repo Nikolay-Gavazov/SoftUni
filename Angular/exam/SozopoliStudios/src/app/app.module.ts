a938967d0f11867b937b0d3cf9e61b295e5568c7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-22 19:02:40 +0300

    Update app.module.ts

--- Angular/exam/SozopoliStudios/src/app/app.module.ts
+++ Angular/exam/SozopoliStudios/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainComponent,
+    
   ],
   imports: [
     BrowserModule,
