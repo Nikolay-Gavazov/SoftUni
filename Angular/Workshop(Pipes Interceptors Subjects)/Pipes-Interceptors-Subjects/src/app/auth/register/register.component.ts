1b4c1f8f52ca1d34374e851d401f77b3727d129b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-24 18:09:53 +0300

    Update register.component.ts

--- Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/auth/register/register.component.ts
+++ Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/auth/register/register.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-register[',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   emailDomains = DEFAULT_EMAIL_DOMAINS;
-  
+  constructor(private authService: AuthService, private router: Router){}
 
   register(form: NgForm){
-    
+    if(form.invalid){
+      return;
+    }
+    const {username, email, password, rePassword, tel} = form.value;
+
+    this.authService.register(username, email, password, rePassword, tel).subscribe(() =>{
+      form.reset();
+      this.router.navigate(['/']);
+    })
   }
 }
