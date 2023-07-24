4a3c2114ae1f11d296e2d9129d4a68edf1c1df44
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-07-24 18:09:23 +0300

    Update app-routing.module.ts

--- Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/app-routing.module.ts
+++ Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'themes',
+    loadChildren: () => import("./theme/theme.module").then((m)=>m.ThemeModule),
+  },
+  {
     path: '**',
     component: NotFoundComponent,
   },
