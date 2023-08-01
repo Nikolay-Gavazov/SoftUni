227ab1046ffd230215a36f9a13ba76e4ff60e679
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-08-02 00:21:15 +0300

    Update app.module.ts

--- Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/app.module.ts
+++ Angular/Workshop(Pipes Interceptors Subjects)/Pipes-Interceptors-Subjects/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
