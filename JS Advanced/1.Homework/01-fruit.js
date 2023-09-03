2ef2a08a91d04e83d2027d3995d478eb26173381
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-09-03 23:00:55 +0300

    Update 01-fruit.js

--- JS Advanced/1.Homework/01-fruit.js
+++ JS Advanced/1.Homework/01-fruit.js
@@ -1,4 +1,4 @@
-function fruit(fruit, grams, price){
+function fruit(fruit, grams, price) {
     let kg = grams / 1000;
     let totalPrice = kg * price;
     console.log(`I need $${totalPrice.toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${fruit}.`);
