44822abf59b92167cc4471c6aa78ce536d8c0977
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-03 12:10:53 +0200

    Create Header.jsx

---
+++ React/2.Lecture/Demo/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Catalog</a></li>
+            </ul>
+        </header>
+    )
+}
