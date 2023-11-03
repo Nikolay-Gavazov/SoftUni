16d95af11d3073f6e0c63770075d0cec623afc9c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-03 12:10:50 +0200

    Create Data.jsx

---
+++ React/2.Lecture/Demo/src/components/Data.jsx
@@ -0,0 +1,17 @@
+export default function Data(props){
+    const data = props.data;
+    const div = data.map((el) =>
+        <div>
+        <h2><strong>Title:</strong> {el.Title}</h2>
+        <p><strong>Year:</strong> {el.Year}</p>
+        <p><strong>Director:</strong> {el.Director}</p>
+        <p><strong>Actors:</strong> {el.Actors}</p>
+        </div>
+    )
+    console.log(data);
+    return (
+        <section>
+            {div}
+        </section>
+    )
+}
