7c4de32d0162f7cabd1cb636cf9e1b26d5ec4054
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-03 12:10:39 +0200

    Create movies.js

---
+++ React/2.Lecture/Demo/src/assets/movies.js
@@ -0,0 +1,27 @@
+export default [
+        {
+            "Title": "I Am Legend",
+            "Year": "2007",
+            "Director": "Francis Lawrence",
+            "Actors": "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
+        },
+    
+        {
+            "Title": "Avatar",
+            "Year": "2009",
+            "Director": "James Cameron",
+            "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        },
+        {
+            "Title": "300",
+            "Year": "2006",
+            "Director": "Zack Snyder",
+            "Actors": "Gerard Butler, Lena Headey, Dominic West, David Wenham",
+        },
+        {
+            "Title": "The Wolf of Wall Street",
+            "Year": "2013",
+            "Director": "Martin Scorsese",
+            "Actors": "Leonardo DiCaprio, Jonah Hill, Margot Robbie, Matthew McConaughey",
+        }
+    ]
