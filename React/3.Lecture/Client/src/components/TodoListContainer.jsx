26837380c9ee8f7fb7188ee1ac6e90642c6c2d22
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-05 11:20:45 +0200

    Create TodoListContainer.jsx

---
+++ React/3.Lecture/Client/src/components/TodoListContainer.jsx
@@ -0,0 +1,17 @@
+import TodoListTable from "./TodoListTable";
+
+const TodoListContainer = () => {
+    return(
+        <section className="todo-list-container">
+      <h1>Todo List</h1>
+      <div className="add-btn-container">
+        <button className="btn">+ Add new Todo</button>
+      </div>
+      <div className="table-wrapper">
+        <TodoListTable/>
+      </div>
+    </section>
+    )
+};
+
+export default TodoListContainer;
