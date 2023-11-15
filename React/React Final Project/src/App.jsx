17c9f20bfb707a3f67cb70687e80577c3e57a078
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-15 11:46:32 +0200

    Add routes

--- React/React Final Project/src/App.jsx
+++ React/React Final Project/src/App.jsx
@@ -4,17 +4,38 @@ import Home from './components/pages/home/Home'
 import Topbar from './components/pages/layout/Topbar'
 import Navbar from './components/pages/layout/Navbar'
 import Footer from './components/pages/layout/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/pages/user/Login'
+import Register from './components/pages/user/Register'
+import About from './components/pages/About'
+import Booking from './components/pages/Booking'
+import Contacts from './components/pages/Contacts'
+import NotFound from './components/pages/NotFound'
+import Logout from './components/pages/user/Logout'
+import Service from './components/pages/home/Service'
 
 function App() {
 
-  return (
-    <>
-      <Topbar/>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </>
-  )
+    return (
+        <>
+            <Topbar />
+            <Navbar />
+
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/register' element={<Register/>}/>
+                <Route path='/logout' element={<Logout/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/booking' element={<Booking/>}/>
+                <Route path='/contact' element={<Contacts/>}/>
+                <Route path='/services' element={<Service/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Routes>
+
+            <Footer />
+        </>
+    )
 }
 
 export default App
