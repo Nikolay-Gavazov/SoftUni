3e59c8579553a1cf175855e112f536364cd38096
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-17 10:57:26 +0200

    Update App.jsx

--- React/React Final Project/src/App.jsx
+++ React/React Final Project/src/App.jsx
@@ -1,41 +1,36 @@
-import { useState } from 'react'
-import './styles/style.css'
-import Home from './components/pages/home/Home'
-import Topbar from './components/pages/layout/Topbar'
-import Navbar from './components/pages/layout/Navbar'
-import Footer from './components/pages/layout/Footer'
 import { Route, Routes } from 'react-router-dom'
-import Login from './components/pages/user/Login'
-import Register from './components/pages/user/Register'
-import About from './components/pages/About'
-import Booking from './components/pages/Booking'
-import Contacts from './components/pages/Contacts'
-import NotFound from './components/pages/NotFound'
-import Logout from './components/pages/user/Logout'
-import Service from './components/pages/home/Service'
+import About from "./components/pages/About"
+import Contacts from "./components/pages/Contacts"
+import Create from "./components/pages/Create"
+import NotFound from "./components/pages/NotFound"
+import Home from "./components/pages/home/Home"
+import Footer from "./components/pages/layout/Footer"
+import Navbar from "./components/pages/layout/Navbar"
+import Topbar from "./components/pages/layout/Topbar"
+import Login from "./components/pages/user/Login"
+import Logout from "./components/pages/user/Logout"
+import Register from "./components/pages/user/Register"
+import PageLoader from './components/shared/PageLoader'
 
 function App() {
-
-    return (
-        <>
-            <Topbar />
-            <Navbar />
-
-            <Routes>
+  return (
+    <>
+        <PageLoader/>
+      <Navbar/>
+      <Topbar/>
+      <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/logout' element={<Logout/>}/>
                 <Route path='/about' element={<About/>}/>
-                <Route path='/booking' element={<Booking/>}/>
+                <Route path='/create' element={<Create/>}/>
                 <Route path='/contact' element={<Contacts/>}/>
-                <Route path='/services' element={<Service/>}/>
                 <Route path='*' element={<NotFound/>}/>
             </Routes>
-
-            <Footer />
-        </>
-    )
+      <Footer/>
+    </>
+  )
 }
 
 export default App
