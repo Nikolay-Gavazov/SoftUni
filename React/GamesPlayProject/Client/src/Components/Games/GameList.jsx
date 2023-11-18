15ea814a13aa281e887c0b52f56d47e55b15df7a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-19 00:00:22 +0200

    Create GameList.jsx

---
+++ React/GamesPlayProject/Client/src/Components/Games/GameList.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import Game from "./Game";
+import { getAll } from "../../services/gameService";
+
+const GameList = () => {
+    const [games, setGames] = useState([]);
+    useEffect(() =>{
+      getAll()
+        .then(setGames)
+    }, []);
+    return(
+      <section id="catalog-page">
+      <h1>All Games</h1>
+      {/* Display div: with information about every game (if any) */}
+      {games.map(game => <Game key={game._id} {...game}/>)}
+      {games.length === 0 &&(
+        <h3 className="no-articles">No articles yet</h3>
+      )}
+    </section>
+    );
+};
+
+export default GameList;
