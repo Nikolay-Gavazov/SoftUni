f4ddd06df66eb2c4206c927259e97b07144be518
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-21 11:14:04 +0200

    Create Gallery.jsx

---
+++ React/React Final Project/src/components/pages/home/Gallery.jsx
@@ -0,0 +1,11 @@
+import Photo from "./Photo";
+
+const Gallery = () => {
+    return(
+      <div className="row tm-mb-90 tm-gallery">
+        <Photo/>
+      </div>
+    )
+}
+
+export default Gallery;
