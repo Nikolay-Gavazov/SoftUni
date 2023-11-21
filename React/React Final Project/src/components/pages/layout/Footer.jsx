eeedc3f80aa5a97285ed923b8810d670e5f5770d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-21 11:14:19 +0200

    Create Footer.jsx

---
+++ React/React Final Project/src/components/pages/layout/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer">
+  <div className="container-fluid tm-container-small">
+    <div className="row">
+      <div className="col-lg-8 col-md-7 col-12 px-5 mb-3">
+        <p>Copyright 2023 Free Photos Company. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+  )
+}
+
+export default Footer
