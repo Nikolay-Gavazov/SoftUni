1a20268e1a0987ee93854252a7ec5ea2f0db7c72
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-22 15:47:32 +0200

    Made logout works

--- React/GamesPlayProject/Client/src/Components/user/Logout.jsx
+++ React/GamesPlayProject/Client/src/Components/user/Logout.jsx
@@ -1,7 +1,18 @@
+import { useContext, useEffect } from "react";
+import { logout } from "../../services/userService";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../context/authContext";
+
 const Logout = () => {
-    return(
-        <h1>Logout</h1>
-    );
+    const navigate = useNavigate();
+    const {logoutHandler } = useContext(AuthContext);
+    useEffect(() => {
+        logout()
+        .then(() => {
+        logoutHandler()
+        navigate('/')})
+        .catch(() => navigate('/'));
+    },[]);
 };
 
 export default Logout;
