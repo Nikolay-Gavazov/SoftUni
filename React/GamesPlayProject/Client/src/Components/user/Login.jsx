7c221c80535fa2d962d41913d932b45ab8f0d1f0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-22 15:47:22 +0200

    Made login works

--- React/GamesPlayProject/Client/src/Components/user/Login.jsx
+++ React/GamesPlayProject/Client/src/Components/user/Login.jsx
@@ -1,29 +1,48 @@
+import { useContext } from "react";
+import useForm from "../../hooks/useForm";
+import AuthContext from "../../context/authContext";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return(
-        <section id="login-page" className="auth">
-  <form id="login">
-    <div className="container">
-      <div className="brand-logo" />
-      <h1>Login</h1>
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Sokka@gmail.com"
-      />
-      <label htmlFor="login-pass">Password:</label>
-      <input type="password" id="login-password" name="password" />
-      <input type="submit" className="btn submit" defaultValue="Login" />
-      <p className="field">
-        <span>
-          If you don't have profile click <a href="#">here</a>
-        </span>
-      </p>
-    </div>
-  </form>
-</section>
-    );
+  const { loginSubmitHandler } = useContext(AuthContext);
+  const { formValue, onSubmit, onChange } = useForm(
+    { email: "", password: "" },
+    loginSubmitHandler
+  );
+
+  return (
+    <section id="login-page" className="auth">
+      <form id="login" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo" />
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={formValue.email}
+          />
+          <label htmlFor="login-pass">Password:</label>
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={formValue.password}
+          />
+          <input type="submit" className="btn submit" defaultValue="Login" />
+          <p className="field">
+            <span>
+              If you don't have profile click <Link to="/register">here</Link>
+            </span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
