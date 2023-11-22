300f714335ccdd74f20a8a99063bd61f3ce5cead
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-22 15:47:38 +0200

    Made register works

--- React/GamesPlayProject/Client/src/Components/user/Register.jsx
+++ React/GamesPlayProject/Client/src/Components/user/Register.jsx
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import useForm from "../../hooks/useForm";
+import AuthContext from "../../context/authContext";
+import { Link } from "react-router-dom";
+
 const Register = () => {
+    const { registerSubmitHandler } = useContext(AuthContext);
+
+    const { formValue, onSubmit, onChange } = useForm(
+      { email: "", username: "", password: "", rePass: ""},
+      registerSubmitHandler
+    );
+
     return(
       <section id="register-page" className="content auth">
-      <form id="register">
+      <form id="register" onSubmit={onSubmit}>
         <div className="container">
           <div className="brand-logo" />
           <h1>Register</h1>
@@ -11,15 +23,28 @@ const Register = () => {
             id="email"
             name="email"
             placeholder="maria@email.com"
+            value={formValue.email}
+            onChange={onChange}
+          />
+          <label htmlFor="email">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Ivan1"
+            value={formValue.username}
+            onChange={onChange}
           />
           <label htmlFor="pass">Password:</label>
-          <input type="password" name="password" id="register-password" />
+          <input type="password" name="password" id="register-password" value={formValue.password}
+            onChange={onChange} />
           <label htmlFor="con-pass">Confirm Password:</label>
-          <input type="password" name="confirm-password" id="confirm-password" />
+          <input type="password" name="rePass" id="confirm-password" value={formValue.rePass}
+            onChange={onChange} />
           <input className="btn submit" type="submit" defaultValue="Register" />
           <p className="field">
             <span>
-              If you already have profile click <a href="#">here</a>
+              If you already have profile click <Link to="/login">here</Link>
             </span>
           </p>
         </div>
