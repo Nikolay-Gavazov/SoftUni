d7af6c18c995eb58f29174eae0bbde2df70f9739
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-24 17:22:58 +0200

    Create authContext.jsx

---
+++ React/GamesPlayProject/Client/src/context/authContext.jsx
@@ -0,0 +1,45 @@
+import { createContext } from "react";
+import { login, register } from "../services/userService";
+import usePersistedState from "../hooks/usePersistedState";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children
+}) => {
+
+    const [auth, setAuth] = usePersistedState("auth",{});
+     
+      const loginSubmitHandler = async (data) => {
+        const result = await login( data.email, data.password );
+        setAuth(result);
+        localStorage.setItem('accessToken', result.accessToken);
+      };
+    
+      const registerSubmitHandler = async (data) => {
+        const result = await register( data.email, data.username, data.password );
+        setAuth(result);
+        localStorage.setItem('accessToken', result.accessToken);
+      };
+      const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
+      };
+    
+      const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username,
+        userId: auth._id,
+        isAuthenticated: !!auth.accessToken
+      }
+
+    return(
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
