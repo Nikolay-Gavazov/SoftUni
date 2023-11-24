a830b6ec8e6190a6893e1325fbf645aa9aea570f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-25 00:37:16 +0200

    Update useForm.jsx

--- React/GamesPlayProject/Client/src/hooks/useForm.jsx
+++ React/GamesPlayProject/Client/src/hooks/useForm.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-const useForm = (initValue, service, id = null, getById) => {
-const navigate = useNavigate();
+const useForm = (initValue, service) => {
 const [formValue, setFormValue] = useState(initValue);
 
-    if(id){
-        useEffect(() => {
-        getById(id).then(result => setFormValue(result));  
-        },[])
-    }
+    useEffect(() => {
+        setFormValue(initValue);
+    },[initValue])
+
     const onChange = (e) =>{
         setFormValue(state => ({...state, [e.target.name]: e.target.value}));
     }
@@ -18,17 +15,10 @@ const [formValue, setFormValue] = useState(initValue);
     
         if(service){
             try {
-                if(id){
-                    await service(formValue, id);
-                    console.log(formValue);
-                    navigate(`/gameList/${id}`);
-                    return;
-                }
                 await service(formValue);
                 setFormValue(initValue);
-                navigate('/gameList');
               } catch (error) {
-                console.log(error);
+                return error;
               }
         }
     }
