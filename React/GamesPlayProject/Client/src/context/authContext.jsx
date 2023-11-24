112e6a4930b52111f6267d3a8530c662d445f51e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-25 00:37:11 +0200

    Update authContext.jsx

--- React/GamesPlayProject/Client/src/context/authContext.jsx
+++ React/GamesPlayProject/Client/src/context/authContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 import { login, register } from "../services/userService";
 import usePersistedState from "../hooks/usePersistedState";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
@@ -9,21 +10,25 @@ export const AuthProvider = ({
 }) => {
 
     const [auth, setAuth] = usePersistedState("auth",{});
+    const navigate = useNavigate();
      
       const loginSubmitHandler = async (data) => {
         const result = await login( data.email, data.password );
         setAuth(result);
         localStorage.setItem('accessToken', result.accessToken);
+        navigate('/');
       };
     
       const registerSubmitHandler = async (data) => {
         const result = await register( data.email, data.username, data.password );
         setAuth(result);
         localStorage.setItem('accessToken', result.accessToken);
+        navigate('/');
       };
       const logoutHandler = () => {
         setAuth({});
         localStorage.removeItem('accessToken');
+        navigate('/');
       };
     
       const values = {
