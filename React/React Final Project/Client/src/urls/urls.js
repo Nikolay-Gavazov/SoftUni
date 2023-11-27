c186e68f963794ebd1accafe73149f136006f0bd
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-27 11:19:05 +0200

    Create urls.js

---
+++ React/React Final Project/Client/src/urls/urls.js
@@ -0,0 +1,3 @@
+export const userUrl = 'http://localhost:3030/users';
+export const photosUrl = 'http://localhost:3030/data/photos';
+export const commentsUrl = 'http://localhost:3030/data/comments';
