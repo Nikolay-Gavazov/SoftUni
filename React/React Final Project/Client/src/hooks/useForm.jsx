f5ec232107e1d772e08c56259c9ba56f133870d9
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-27 11:18:49 +0200

    Create useForm.jsx

---
+++ React/React Final Project/Client/src/hooks/useForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useForm = (initValue, service) => {
+    const [formValue, setFormValue] = useState(initValue);
+
+    useEffect(() => {
+        setFormValue(initValue);
+    }, [initValue])
+
+    const onChange = (e) =>{
+        setFormValue(state => ({...state, [e.target.name]: e.target.value}));
+    };
+
+    const onSubmit = async (e) =>{
+        e.preventDefault();
+
+        if(service){
+            try {
+                await service(formValue);
+                setFormValue(initValue);
+            } catch (error) {
+                return error;
+            }
+        }
+
+    };
+
+    return{
+        formValue,
+        onChange,
+        onSubmit
+    }
+};
+
+export default useForm;
