67c4f729617bbe9a6532fde459a57a0b6f67daad
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-28 19:35:27 +0200

    Update App.jsx

--- React/React Final Project/Client/src/App.jsx
+++ React/React Final Project/Client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+
 import About from "./components/pages/About"
 import Contacts from "./components/pages/Contacts"
 import Create from "./components/pages/photos/Create"
@@ -11,15 +12,18 @@ import Login from "./components/pages/user/Login"
 import Logout from "./components/pages/user/Logout"
 import Register from "./components/pages/user/Register"
 import PageLoader from './components/shared/PageLoader'
+import { AuthProvider } from './context/authContext'
+import Gallery from './components/pages/photos/Gallery'
 
 function App() {
   return (
-    <>
-        <PageLoader/>
-      <Navbar/>
-      <Topbar/>
-      <Routes>
+    <AuthProvider>
+      <PageLoader/>
+        <Navbar/>
+        <Topbar/>
+            <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/gallery' element={<Gallery/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/logout' element={<Logout/>}/>
@@ -28,8 +32,8 @@ function App() {
                 <Route path='/contact' element={<Contacts/>}/>
                 <Route path='*' element={<NotFound/>}/>
             </Routes>
-      <Footer/>
-    </>
+        <Footer/>
+    </AuthProvider>
   )
 }
 
