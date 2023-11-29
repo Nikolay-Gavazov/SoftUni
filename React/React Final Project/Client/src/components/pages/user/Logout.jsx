251c622ebb7942f6c1a8e2a4a23d11723e82002b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-29 13:29:43 +0200

    Update Logout.jsx

--- React/React Final Project/Client/src/components/pages/user/Logout.jsx
+++ React/React Final Project/Client/src/components/pages/user/Logout.jsx
@@ -1,9 +1,18 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../../context/authContext";
+import { logout } from "../../../services/userService";
 
 const Logout = () =>{
-    return(
-        <>
-        <h1>Logout</h1>
-        </>
-    )
+    const navigate = useNavigate();
+    const {logoutHandler} = useContext(AuthContext);
+
+    useEffect(() => {
+        logout()
+        .then(() =>{
+            logoutHandler()
+            navigate('/')})
+        .catch(() => navigate('/'));
+        },[]);
 };
 export default Logout;
