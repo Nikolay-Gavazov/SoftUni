25dd0217d156e1e0b3b66672e049abe40e98b00a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-29 13:29:36 +0200

    Update Navbar.jsx

--- React/React Final Project/Client/src/components/pages/layout/Navbar.jsx
+++ React/React Final Project/Client/src/components/pages/layout/Navbar.jsx
@@ -1,66 +1,85 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import AuthContext from "../../../context/authContext";
 
 const Navbar = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <nav className="navbar navbar-expand-lg">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">
-        <i className="fas fa-film mr-2" />
-        Free Photos
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i className="fas fa-bars" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link nav-link-1 active" aria-current="page"to="/">
-              Photos
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link nav-link-2" to="/create">
-              Create
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link nav-link-3" to="/about">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link nav-link-4" to="/contact">
-              Contact
-            </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/login" className="nav-link nav-link-1">
-            Login
-          </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/register" className="nav-link nav-link-2">
-            Register
-          </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/logout" className="nav-link nav-link-3">
-            Logout
-          </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  )
-}
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <i className="fas fa-film mr-2" />
+          Free Photos
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link nav-link-1"
+                activeClassName = "active"
+                aria-current="page"
+                to="/gallery"
+              >
+                Photos
+              </NavLink>
+            </li>
 
-export default Navbar
+            <li className="nav-item">
+              <NavLink className="nav-link nav-link-3" to="/about" activeClassName = "active">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link nav-link-4" to="/contact" activeClassName = "active">
+                Contact
+              </NavLink>
+            </li>
+            {!isAuthenticated && (
+              <>
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link nav-link-1" activeClassName = "active">
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link nav-link-2" activeClassName = "active">
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            )}
+
+            {isAuthenticated && (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link nav-link-2" to="/create" activeClassName = "active">
+                    Create
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/logout" className="nav-link nav-link-3" activeClassName = "active">
+                    Logout
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
