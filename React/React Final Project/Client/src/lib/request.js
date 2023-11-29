9319ce413e2c7cb72a00c726ff17af7d0c6bea80
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-29 13:29:53 +0200

    Update request.js

--- React/React Final Project/Client/src/lib/request.js
+++ React/React Final Project/Client/src/lib/request.js
@@ -1,25 +1,43 @@
-function buildOptions(data){
-    const options = {};
+function buildOptions(data) {
+  const options = {};
 
-    if(data){
-        options.body = JSON.stringify(data);
-        options.headers = {"Content-Type": "application/json"};
-    }
+  if (data) {
+    options.body = JSON.stringify(data);
+    options.headers = { "Content-Type": "application/json" };
+  }
 
-    return options;
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      "X-Authorization": token,
+    };
+  }
+
+  return options;
 }
 
-const request = async(method, url, data) =>{
-    const responce = await fetch(url, {
-        ...buildOptions(data),
-        method
-    });
-    const result = await responce.json();
-    return result;
-};
+const request = async (method, url, data) => {
+  const responce = await fetch(url, {
+    ...buildOptions(data),
+    method,
+  });
 
+  if (responce.status === 204) {
+    return {};
+  }
+
+  const result = await responce.json();
+
+  if (!responce.ok) {
+    throw result;
+  }
+
+  return result;
+};
 
 export const get = request.bind(null, "GET");
 export const post = request.bind(null, "POST");
 export const put = request.bind(null, "PUT");
-export const remove = request.bind(null, "DELETE");
+export const del = request.bind(null, "DELETE");
