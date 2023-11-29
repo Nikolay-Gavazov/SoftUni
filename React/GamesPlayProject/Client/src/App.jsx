1004c9937174aad954740ea9b03a82f13a83b240
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-29 19:17:45 +0200

    Update App.jsx

--- React/GamesPlayProject/Client/src/App.jsx
+++ React/GamesPlayProject/Client/src/App.jsx
@@ -10,6 +10,7 @@ import Logout from "./Components/user/Logout";
 import Details from "./Components/Games/Details";
 import Edit from "./Components/Games/Edit";
 import {AuthProvider} from "./context/authContext";
+import AuthGuard from "./guards/AuthGuard";
 
 
 function App() {
@@ -20,13 +21,16 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
         <Route path="/gameList" element={<GameList />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/logout" element={<Logout />} />
         <Route path="/gameList/:id" element={<Details />} />
-        <Route path="/gameList/:id/edit" element={<Edit />} />
+      
+        <Route element={<AuthGuard/> }>
+          <Route path="/create" element={<Create />} />
+          <Route path="/gameList/:id/edit" element={<Edit />} />
+          <Route path="/logout" element={<Logout />} />
+        </Route>
       </Routes>
     </AuthProvider>
   );
