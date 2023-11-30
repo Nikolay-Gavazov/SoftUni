96b792f74acb6ba537d60606b2c454ec37896d06
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-30 02:13:23 +0200

    Create About.jsx

---
+++ React/React Final Project/Client/src/components/About/About.jsx
@@ -0,0 +1,52 @@
+const About = () => {
+    return(
+      <div className="container-fluid tm-mt-60">
+      <div className="row" style={{margin:"0.5em auto", justifyContent:"center", width: "58%"}}>
+          <h2 className="col-12 tm-text-primary" style={{textAlign:"center"}}>About us</h2>
+          <p style={{padding: "6px", marginBottom: 0}}>Welcome to Free Photos, the ultimate platform for free picture sharing! Our mission is to create a vibrant community where individuals can freely upload, discover, and share stunning visuals with the world.</p>
+      </div>
+      <div className="row tm-mb-74 tm-row-1640" style={{justifyContent:"center"}}>
+      <div className="col-lg-7 col-md-6 col-12">
+      <h3 className="col-12 tm-text-primary" style={{textAlign:"center"}}>Out Vision</h3>
+        <div className="tm-about-img-text">
+          <p className="mb-4">
+          At Free Photos, we believe in the power of imagery to connect people, inspire creativity, and foster a sense of community. Our vision is to provide a space where photographers, artists, and enthusiasts alike can showcase their work without any barriers.
+          </p>
+        </div>
+      </div>
+
+      <div className="col-lg-7 col-md-6 col-12">
+      <h3 className="col-12 tm-text-primary" style={{textAlign:"center"}}>Free and Open</h3>
+        <div className="tm-about-img-text">
+          <p className="mb-4">
+          We are passionate about making high-quality pictures accessible to everyone. That's why Free Photos is committed to being a free and open platform. Anyone can contribute their unique perspective and talents to our growing collection, and every user has the freedom to explore and download captivating images without any cost.
+          </p>
+        </div>
+      </div>
+
+      <div className="col-lg-7 col-md-6 col-12">
+      <h3 className="col-12 tm-text-primary" style={{textAlign:"center"}}>Community-Driven</h3>
+        <div className="tm-about-img-text">
+          <p className="mb-4">
+          We thrive on the energy of our community. Free Photos is more than just a repository of images; it's a hub for individuals who appreciate the beauty and diversity of visual art. Join our community to connect with like-minded individuals, exchange ideas, and celebrate the incredible talent within our network.
+          </p>
+        </div>
+      </div>
+
+      <div className="col-lg-7 col-md-6 col-12">
+      <h3 className="col-12 tm-text-primary" style={{textAlign:"center"}}>Join Us Today!</h3>
+        <div className="tm-about-img-text">
+          <p className="mb-4">
+          Whether you're an avid photographer, a digital artist, or someone who just loves browsing beautiful images, Free Photos welcomes you. Unleash your creativity, discover incredible visuals, and become part of a supportive community that values the power of freely sharing art.
+          </p>
+          <p className="mb-4">
+          Thank you for being a part of Free Photos!
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+    )
+}
+
+export default About;
