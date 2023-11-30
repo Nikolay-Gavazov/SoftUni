93bb3170097da11ec8d5e568cc6798b9f08081c1
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-30 02:13:37 +0200

    Create Topbar.jsx

---
+++ React/React Final Project/Client/src/components/Layout/Topbar.jsx
@@ -0,0 +1,8 @@
+const Topbar = () =>{
+    return(
+      <div className="topbar"style={{height:"300px", width:"100%"}}>
+        <img src="img/296099.jpg" alt="topbar" style={{display:"block", width:"100%", height:"300px"}} />
+      </div>
+    )
+}
+export default Topbar;
