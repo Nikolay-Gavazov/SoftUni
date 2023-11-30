c0f390befd6a5ec3f94ef77f806a05c73d05ebef
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-30 02:13:29 +0200

    Create Home.jsx

---
+++ React/React Final Project/Client/src/components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import Photo from "../photos/Photo";
+import { getLatest } from "../../services/PhotoService";
+
+const Home = () => {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() =>{
+        getLatest()
+        .then(setPhotos);
+    },[]);
+    return(
+        <div className="container-fluid tm-container-content tm-mt-60">
+        <div>
+        <h1 style={{color:"#009999", margin:"2em 0", textAlign:"center"}}>Unlock Creativity with Endless Visual Inspiration – Where Every Image Tells a Story, and It's All Yours for Free!</h1>
+      </div>
+      <div className="row mb-4">
+      <h2 className="col-6 tm-text-primary" >Latest Photos</h2>
+      <div className="row tm-mb-90 tm-gallery">
+        {photos.map(photo => <Photo key={photo._id} {...photo}/>)}
+      </div>
+    </div>
+        </div>
+    );
+};
+export default Home;
