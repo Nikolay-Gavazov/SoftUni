181bb66d2a40fac2b10d3b0f0df0f2ad0a1a6f4f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-03 14:27:33 +0200

    Update Home.jsx

--- React/React Final Project/Client/src/components/Home/Home.jsx
+++ React/React Final Project/Client/src/components/Home/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
         <div>
         <h1 style={{color:"#009999", margin:"2em 0", textAlign:"center"}}>Unlock Creativity with Endless Visual Inspiration – Where Every Image Tells a Story, and It's All Yours for Free!</h1>
       </div>
-      <div className="row mb-4">
-      <h2 className="col-6 tm-text-primary" >Latest Photos</h2>
+      <h2 className="col-6 tm-text-primary" style={{margin:"1em 0"}}>Latest Photos</h2>
+      <div className="row mb-4" style={{justifyContent:"center"}}>
       <div className="row tm-mb-90 tm-gallery">
         {photos.map(photo => <Photo key={photo._id} {...photo}/>)}
       </div>
