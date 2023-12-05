c01dbe985edf3f04f40c5d58a357c9a74b41447d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-05 03:09:09 +0200

    Update Home.jsx

--- React/React Final Project/Client/src/components/Home/Home.jsx
+++ React/React Final Project/Client/src/components/Home/Home.jsx
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react"
-import Photo from "../photos/Photo";
+import Photo from "../Photos/PhotoItem";
 import { getLatest } from "../../services/PhotoService";
+import PageLoader from "../Shared/PageLoader";
 
 const Home = () => {
     const [photos, setPhotos] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
 
     useEffect(() =>{
+        setIsloading(true);
         getLatest()
-        .then(setPhotos);
+        .then(setPhotos)
+        .catch(error => {
+          if(error.code === 404){
+            navigate('*')
+          }else{
+            setError(error.message);
+          }
+        })
+        .finally(() => setIsloading(false));
     },[]);
     return(
         <div className="container-fluid tm-container-content tm-mt-60">
+          {isLoading && (<PageLoader/>)}
         <div>
         <h1 style={{color:"#009999", margin:"2em 0", textAlign:"center"}}>Unlock Creativity with Endless Visual Inspiration – Where Every Image Tells a Story, and It's All Yours for Free!</h1>
       </div>
