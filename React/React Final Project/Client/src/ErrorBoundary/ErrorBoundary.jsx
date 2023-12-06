69e80d8ce7425b5fdc865216a640c8f07109e952
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-06 11:06:36 +0200

    Create ErrorBoundary.jsx

---
+++ React/React Final Project/Client/src/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import NotFound from "../components/NotFound/NotFound";
+
+
+export default class ErrorBoundary extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            hasError: false
+        };
+    }
+
+    static getDerivedStateFromError(err){
+        return{
+            hasError: true
+        }
+    }
+
+    render(){
+        if(this.state.hasError){
+            return <NotFound/>
+        }
+        return this.props.children;
+    }
+}
