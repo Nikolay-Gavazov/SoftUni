3af5e0d10e6efd9de223fc1df06661c03586b255
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-06 11:04:43 +0200

    Update Gallery.jsx

--- React/React Final Project/Client/src/components/Photos/Gallery.jsx
+++ React/React Final Project/Client/src/components/Photos/Gallery.jsx
@@ -1,17 +1,29 @@
 import { useEffect, useState } from "react";
-import Photo from "./Photo";
+import Photo from "./PhotoItem";
 import { getAll } from "../../services/PhotoService";
+import PageLoader from "../Shared/PageLoader";
 
 const Gallery = () => {
     const [photos, setPhotos] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
 
     useEffect(() =>{
+      setIsloading(true);
       getAll()
-      .then(setPhotos);
+      .then(setPhotos)
+      .catch(error => {
+        if(error.code === 404){
+          navigate('*')
+        }else{
+          setError(error.message);
+        }
+      })
+      .finally(() => setIsloading(false));
     },[]);
 
     return(
       <div className="container-fluid tm-container-content tm-mt-60">
+        {isLoading && (<PageLoader/>)}
       <h2 className="col-6 tm-text-primary" style={{margin:"1em 0"}}>Gallery</h2>
       <div className="row mb-4" style={{justifyContent:"center"}}>
       <div className="row tm-mb-90 tm-gallery">
