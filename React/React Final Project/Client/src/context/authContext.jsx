1bee231e43c630046e6626d33f8b9ae834ba5334
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-07 11:44:55 +0200

    Update authContext.jsx

--- React/React Final Project/Client/src/context/authContext.jsx
+++ React/React Final Project/Client/src/context/authContext.jsx
@@ -1,31 +1,53 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import usePersistedState from "../hooks/usePersistedState";
 import { useNavigate } from "react-router-dom";
+import { login, register } from "../services/userService";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [isLoading, setIsloading] = useState(false);
   const [auth, setAuth] = usePersistedState("auth", {});
+  const [serverError, setServerError] = useState('');
   const navigate = useNavigate();
 
   const loginSubmitHandler = async (data) => {
-    const result = await login(data.email, data.password);
+    setIsloading(true);
+    try {
+      const result = await login(data.email, data.password);
     setAuth(result);
-    localStorage.setItem("accessToken", result.accsessToken);
+    localStorage.setItem("accessToken", result.token);
     navigate("/");
+    setIsloading(false);
+    setServerError('');
+    } catch (error) {
+      setServerError(error.message);
+      setIsloading(false);
+    }
   };
 
   const registerSubmitHandler = async (data) => {
-    const result = await register(data.email, data.password);
+    setIsloading(true);
+    try {
+      const result = await register(data.email, data.password);
     setAuth(result);
-    localStorage.setItem("accessToken", result.accsessToken);
+    localStorage.setItem("accessToken", result.token);
     navigate("/");
+    setIsloading(false);
+    setServerError('');
+    } catch (error) {
+      setServerError(error.message);
+      setIsloading(false);
+    }
+    
   };
 
   const logoutHandler = () => {
+    setIsloading(true);
     setAuth({});
     localStorage.removeItem("accessToken");
     navigate("/");
+    setIsloading(false);
   };
 
   const values = {
@@ -34,7 +56,9 @@ export const AuthProvider = ({ children }) => {
     logoutHandler,
     email: auth.email,
     userId: auth._id,
-    isAuthenticated: !!auth.accsessToken,
+    isAuthenticated: !!auth.token,
+    serverError,
+    isLoading
   };
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
