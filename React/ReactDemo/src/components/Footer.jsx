eb2b371409a106ec5c418482268980a8d7edb6b5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-11 09:37:01 +0200

    Create Footer.jsx

---
+++ React/ReactDemo/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <section className="footer_section">
+          <div className="container">
+            <p>
+              © <span id="displayYear" /> All Rights Reserved By
+              <a href="https://html.design/">Free Html Templates</a>
+            </p>
+          </div>
+        </section>
+    )
+}
