f41c15191cb37eb93418466185431ec504697df0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-15 00:40:47 +0200

    Update script.js

--- React/1.Lecture/src/script.js
+++ React/1.Lecture/src/script.js
@@ -11,33 +11,28 @@ const reactSecondHeading = React.createElement('h2', {}, "New World");
 
 const header = React.createElement('header',{className: "header-element"}, reactHeading, reactSecondHeading); */
 
-const Footer = () =>
-{
-    return React.createElement(
-        'footer',
-        {className: "site-footer"},
-        React.createElement(
-            "p",
-            {}, 
-            "All rights reserved")
-            );
-}
-
+const Footer = () => {
+  return React.createElement(
+    "footer",
+    { className: "site-footer" },
+    React.createElement("p", {}, "All rights reserved")
+  );
+};
 
 const body = (
-    <div>
-        <header className="site-header">
-        <h1>Hello from JSX</h1>
-        <h2>Tasdasfasf</h2>
-        <p>asdasfasd</p>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+  <div>
+    <header className="site-header">
+      <h1>Hello from JSX</h1>
+      <h2>Tasdasfasf</h2>
+      <p>asdasfasd</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
     </header>
-    <Footer/>
-    </div>
-)
+    <Footer />
+  </div>
+);
 
 root.render(body);
