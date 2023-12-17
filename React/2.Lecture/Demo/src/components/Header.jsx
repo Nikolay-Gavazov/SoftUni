19e36c5fc9f513444017cec9c5398e8a7f634972
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-17 11:48:06 +0200

    Update Header.jsx

--- React/2.Lecture/Demo/src/components/Header.jsx
+++ React/2.Lecture/Demo/src/components/Header.jsx
@@ -1,12 +1,20 @@
-export default function Header(){
-    return(
-        <header>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contacts</a></li>
-                <li><a href="#">Catalog</a></li>
-            </ul>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contacts</a>
+        </li>
+        <li>
+          <a href="#">Catalog</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
