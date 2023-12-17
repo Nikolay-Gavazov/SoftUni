e70c989572fa04b56533924c21547f06b9b7b09b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-17 11:48:08 +0200

    Update Timer.jsx

--- React/2.Lecture/Demo/src/components/Timer.jsx
+++ React/2.Lecture/Demo/src/components/Timer.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-export default function Timer(){
-    const [timer, setTimer] = useState(0);
-    function resetTimer(){
-        setTimer(0);
-    }
+export default function Timer() {
+  const [timer, setTimer] = useState(0);
+  function resetTimer() {
+    setTimer(0);
+  }
 
-    setTimeout(() =>{
-        setTimer(timer => timer + 1);
-    }, "1000");
+  setTimeout(() => {
+    setTimer((timer) => timer + 1);
+  }, "1000");
 
-    return(
-        <div>
-            <p>{timer}</p>
-            <button onClick={resetTimer}>Reset Timer</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>{timer}</p>
+      <button onClick={resetTimer}>Reset Timer</button>
+    </div>
+  );
 }
