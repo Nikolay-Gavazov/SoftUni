34820777727f26c41e3feac788459e3ed728246e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-20 01:11:25 +0200

    Update TodoItem.jsx

--- React/3.Lecture/Client/src/components/TodoItem.jsx
+++ React/3.Lecture/Client/src/components/TodoItem.jsx
@@ -1,16 +1,18 @@
-const TodoItem = ({text, isCompleted, _id, changeStatusHandler}) => {
-    const onChangeStatus = () =>{
-        changeStatusHandler(_id);
-    }
-    return (
-        <tr className= {`todo ${isCompleted && "is-completed"} `}>
-            <td>{text}</td>
-            <td>{isCompleted ? "Completed" : "Not Completed"}</td>
-            <td className="todo-action">
-                <button onClick={onChangeStatus} className="btn todo-btn" >Change status</button>
-            </td>
-        </tr>
-    )
+const TodoItem = ({ text, isCompleted, _id, changeStatusHandler }) => {
+  const onChangeStatus = () => {
+    changeStatusHandler(_id);
+  };
+  return (
+    <tr className={`todo ${isCompleted && "is-completed"} `}>
+      <td>{text}</td>
+      <td>{isCompleted ? "Completed" : "Not Completed"}</td>
+      <td className="todo-action">
+        <button onClick={onChangeStatus} className="btn todo-btn">
+          Change status
+        </button>
+      </td>
+    </tr>
+  );
 };
 
 export default TodoItem;
