19333583f836bf43cf8947aca7cd6076c17faa53
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-12-20 01:11:23 +0200

    Update Spinner.jsx

--- React/3.Lecture/Client/src/components/Spinner.jsx
+++ React/3.Lecture/Client/src/components/Spinner.jsx
@@ -1,10 +1,10 @@
 const Spinner = () => {
-    return (
-        <div className="loading-container">
-            <div className="loading-spinner">
-                <span className="loading-spinner-text">Loading</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner">
+        <span className="loading-spinner-text">Loading</span>
+      </div>
+    </div>
+  );
 };
 export default Spinner;
