f834508ef4fb2b331291e52ae2358be9739cac6f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-03 11:59:49 +0200

    Update Login.jsx

--- React/React Final Project/Client/src/components/User/Login/Login.jsx
+++ React/React Final Project/Client/src/components/User/Login/Login.jsx
@@ -8,44 +8,53 @@ import PageLoader from "../../Shared/PageLoader";
 
 const Login = () => {
   const [errors, setErrors] = useState({});
-  const {loginSubmitHandler, serverError, isLoading} = useContext(AuthContext);
-  const initValue = useMemo(() => ({email: "", password:""}), []);
-  const {formValue, onSubmit, onChange} = useForm(initValue, loginSubmitHandler);
+  const { loginSubmitHandler, serverError, isLoading } =
+    useContext(AuthContext);
+  const initValue = useMemo(() => ({ email: "", password: "" }), []);
+  const { formValue, onSubmit, onChange } = useForm(
+    initValue,
+    loginSubmitHandler
+  );
 
   const emailValidator = () => {
     const emailRegex = /^[a-zA-Z\.-]{2,}[0-9]*@[a-z]+\.[a-z]{2,}$/gm;
     if (!emailRegex.test(formValue.email)) {
-      setErrors(state => ({
+      setErrors((state) => ({
         ...state,
-        email: 'Email is not valid. Please enter a valid email address',
+        email: "Email is not valid. Please enter a valid email address",
       }));
     } else {
       if (errors.email) {
-        setErrors(state => ({ ...state, email: '' }));
+        setErrors((state) => ({ ...state, email: "" }));
       }
     }
   };
 
   const passwordValidator = () => {
     if (formValue.password.length < 6) {
-      setErrors(state => ({
+      setErrors((state) => ({
         ...state,
-        password: 'Password must be at least 6 characters',
+        password: "Password must be at least 6 characters",
       }));
     } else {
       if (errors.password) {
-        setErrors(state => ({ ...state, password: '' }));
+        setErrors((state) => ({ ...state, password: "" }));
       }
     }
   };
 
-
   return (
     <div className={styles.form}>
       <div className="col-lg-4 col-12 mb-5">
-      {isLoading && (<PageLoader/>)}
-        <h2 className="tm-text-primary mb-5" style={{textAlign: "center"}}>Login Page</h2>
-        <form id="contact-form" className="tm-contact-form mx-auto" onSubmit={onSubmit}>
+        {isLoading && <PageLoader />}
+        <h2 className="tm-text-primary mb-5" style={{ textAlign: "center" }}>
+          Login Page
+        </h2>
+        <form
+          id="contact-form"
+          className="tm-contact-form mx-auto"
+          onSubmit={onSubmit}
+        >
           <div className="form-group">
             <input
               type="text"
@@ -58,8 +67,8 @@ const Login = () => {
             />
           </div>
           {errors.email && (
-                    <p className={styles.errorMessage}>{errors.email}</p>
-                  )}
+            <p className={styles.errorMessage}>{errors.email}</p>
+          )}
           <div className="form-group">
             <input
               type="password"
@@ -72,20 +81,30 @@ const Login = () => {
             />
           </div>
           {errors.password && (
-                    <p className={styles.errorMessage}>{errors.password}</p>
-                  )}
-          <div className="form-group tm-text-right" style={{display: "flex", justifyContent: "center"}}>
-            <button type="submit" className="btn btn-primary"disabled={(Object.values(errors).some(x => x)
-                      || (Object.values(formValue).some(x => x == '')))} >
+            <p className={styles.errorMessage}>{errors.password}</p>
+          )}
+          <div
+            className="form-group tm-text-right"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={
+                Object.values(errors).some((x) => x) ||
+                Object.values(formValue).some((x) => x == "")
+              }
+            >
               Login
             </button>
           </div>
-          {serverError && (
-                    <p className={styles.errorMessage}>{serverError}</p>
-                  )}
+          {serverError && <p className={styles.errorMessage}>{serverError}</p>}
           <p className={styles.paragraph}>
             <span className={styles.field}>
-              You don't have profile? Click <Link to="/register" className={styles.link}>Register</Link>
+              You don't have profile? Click{" "}
+              <Link to="/register" className={styles.link}>
+                Register
+              </Link>
             </span>
           </p>
         </form>
