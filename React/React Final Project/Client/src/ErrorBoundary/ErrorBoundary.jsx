201bd90edbf5b5cfe18a7114e20046ea8ec65342
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-09 00:21:01 +0200

    Update ErrorBoundary.jsx

--- React/React Final Project/Client/src/ErrorBoundary/ErrorBoundary.jsx
+++ React/React Final Project/Client/src/ErrorBoundary/ErrorBoundary.jsx
@@ -1,26 +1,25 @@
 import { Component } from "react";
 import NotFound from "../components/NotFound/NotFound";
 
+export default class ErrorBoundary extends Component {
+  constructor() {
+    super();
 
-export default class ErrorBoundary extends Component{
-    constructor(){
-        super()
+    this.state = {
+      hasError: false,
+    };
+  }
 
-        this.state = {
-            hasError: false
-        };
+  static getDerivedStateFromError(err) {
+    return {
+      hasError: true,
+    };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <NotFound />;
     }
-
-    static getDerivedStateFromError(err){
-        return{
-            hasError: true
-        }
-    }
-
-    render(){
-        if(this.state.hasError){
-            return <NotFound/>
-        }
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
