e769a4ed3b910326a3f97707f697c7bad70eeb20
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-18 17:41:40 +0200

    Update commentService.js

--- React/React Final Project/Client/src/services/commentService.js
+++ React/React Final Project/Client/src/services/commentService.js
@@ -1,18 +1,18 @@
 import * as request from "../lib/request";
 import { commentsUrl, photosUrl } from "../urls/urls";
 
-export const getAll = async(id) => {
-    const result = await request.get(`${commentsUrl}/${id}`);
+export const getAll = async (id) => {
+  const result = await request.get(`${commentsUrl}/${id}`);
 
-    return result;
+  return result;
 };
 
-export const create = async(photoId,email, text, userId) => {
-    const newComment = await request.post(`${photosUrl}/comments/${photoId}`, {
-        photoId,
-        email,
-        text,
-        userId
-    });
-    return newComment;
+export const create = async (photoId, email, text, userId) => {
+  const newComment = await request.post(`${photosUrl}/comments/${photoId}`, {
+    photoId,
+    email,
+    text,
+    userId,
+  });
+  return newComment;
 };
