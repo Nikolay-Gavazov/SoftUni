68356a325d27568f8f89f99c43bfbb39f3f6a3b8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-09-09 14:36:10 +0300

    Update ExtractParenthesis.js

--- JS Advanced/4.Lecture(DOM)/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/4.Lecture(DOM)/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,5 +1,5 @@
 function extract(content) {
-    const text = document.getElementById('content').textContent;
+    const text = document.getElementById(content).textContent;
     const pattern = /\(([^)]+)\)/gm;
     let result = [];
     let match = text.matchAll(pattern);
