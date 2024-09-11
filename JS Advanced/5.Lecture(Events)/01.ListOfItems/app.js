241d50c3f6f039cf67841aec854353ad584cd68d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-09-11 17:14:01 +0300

    Update app.js

--- JS Advanced/5.Lecture(Events)/01.ListOfItems/app.js
+++ JS Advanced/5.Lecture(Events)/01.ListOfItems/app.js
@@ -1,8 +1,8 @@
 function addItem() {
-    const input = document.getElementById('newItemText');
-    const ul = document.getElementById('items');
-    const element = document.createElement('li');
-    element.textContent = input.value;
-    ul.appendChild(element);
-
+  const input = document.getElementById("newItemText");
+  const ul = document.getElementById("items");
+  const element = document.createElement("li");
+  element.textContent = input.value;
+  input.value = "";
+  ul.appendChild(element);
 }
