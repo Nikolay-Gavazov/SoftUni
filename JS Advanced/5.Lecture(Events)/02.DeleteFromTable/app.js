9a647cbdfcd031c294ee398b5998c85d607ff94a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-09-17 11:01:49 +0300

    Update app.js

--- JS Advanced/5.Lecture(Events)/02.DeleteFromTable/app.js
+++ JS Advanced/5.Lecture(Events)/02.DeleteFromTable/app.js
@@ -1,14 +1,14 @@
 function deleteByEmail() {
-    const input = document.querySelector('input');
-    const result = document.getElementById('result');
-    const tr = Array.from(document.querySelectorAll('tr'));
+  const input = document.querySelector("input");
+  const result = document.getElementById("result");
+  const tr = Array.from(document.querySelectorAll("tr"));
 
-    tr.forEach(el => {
-        if(el.textContent.includes(input.value)){
-            el.remove();
-            result.textContent = 'Deleted.';
-        }else{
-            result.textContent = 'Not found.';
-        }
-    });
+  tr.forEach((el) => {
+    if (el.textContent.includes(input.value)) {
+      el.remove();
+      result.textContent = "Deleted.";
+    } else {
+      result.textContent = "Not found.";
+    }
+  });
 }
