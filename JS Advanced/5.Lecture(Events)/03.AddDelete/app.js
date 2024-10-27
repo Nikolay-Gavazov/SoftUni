3b21ed3b07c33920b9f87c95fa45b3cfa8263445
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-10-28 00:02:43 +0200

    Update app.js

--- JS Advanced/5.Lecture(Events)/03.AddDelete/app.js
+++ JS Advanced/5.Lecture(Events)/03.AddDelete/app.js
@@ -1,22 +1,21 @@
 function addItem() {
-    const input = document.getElementById('newItemText');
-    const ul = document.getElementById('items');
+  const input = document.getElementById("newItemText");
+  const ul = document.getElementById("items");
 
-    const element = document.createElement('li');
+  const element = document.createElement("li");
 
-    element.textContent = input.value;
-    input.value = '';
+  element.textContent = input.value;
+  input.value = "";
 
-    const removeBtn = document.createElement('a');
-    const linkText = document.createTextNode('[Delete]');
-    removeBtn.appendChild(linkText);
-    removeBtn.href = '#';
-    removeBtn.addEventListener('click', deleteItem);
-    element.appendChild(removeBtn);
-    ul.appendChild(element);
+  const removeBtn = document.createElement("a");
+  const linkText = document.createTextNode("[Delete]");
+  removeBtn.appendChild(linkText);
+  removeBtn.href = "#";
+  removeBtn.addEventListener("click", deleteItem);
+  element.appendChild(removeBtn);
+  ul.appendChild(element);
 
-
-    function deleteItem() {
-        element.remove()
-    }
+  function deleteItem() {
+    element.remove();
+  }
 }
