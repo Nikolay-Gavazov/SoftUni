070283a82225170244c2123e3cef2d567a3727a3
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-10-29 22:27:49 +0200

    Update app.js

--- JS Advanced/5.Lecture(Events)/04.MouseGradient/app.js
+++ JS Advanced/5.Lecture(Events)/04.MouseGradient/app.js
@@ -1,10 +1,17 @@
 function attachGradientEvents() {
-    const result = document.getElementById('result');
-    const element = document.getElementById('gradient');
-    element.addEventListener('mousemove', func = (event) => {
-        const res = Math.floor(event.offsetX / 3);
-        result.textContent = `${res}%`
-    });
-    element.addEventListener('mouseout', out = () => { result.textContent = '' });
-
+  const result = document.getElementById("result");
+  const element = document.getElementById("gradient");
+  element.addEventListener(
+    "mousemove",
+    (func = (event) => {
+      const res = Math.floor(event.offsetX / 3);
+      result.textContent = `${res}%`;
+    })
+  );
+  element.addEventListener(
+    "mouseout",
+    (out = () => {
+      result.textContent = "";
+    })
+  );
 }
