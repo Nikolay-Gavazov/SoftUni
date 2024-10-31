3fb4662fee760a77e0f31a45295e0cce2c20a7a6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-01 00:09:34 +0200

    Update app.js

--- JS Advanced/5.Lecture(Events)/05.HighlightActive/app.js
+++ JS Advanced/5.Lecture(Events)/05.HighlightActive/app.js
@@ -1,8 +1,17 @@
 function focused() {
-    const element = Array.from(document.querySelectorAll('div>div input'));
-    element.forEach(el => {
-        el.addEventListener('focus', focus = (event) =>{event.target.parentElement.className = 'focused'});
-        el.addEventListener('blur', focusOut = (event) =>{event.target.parentElement.className = ''});
-    });
-   
+  const element = Array.from(document.querySelectorAll("div>div input"));
+  element.forEach((el) => {
+    el.addEventListener(
+      "focus",
+      (focus = (event) => {
+        event.target.parentElement.className = "focused";
+      })
+    );
+    el.addEventListener(
+      "blur",
+      (focusOut = (event) => {
+        event.target.parentElement.className = "";
+      })
+    );
+  });
 }
