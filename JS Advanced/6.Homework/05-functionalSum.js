c555cefe2c727bd00ca31d6c23efb7ec32ad3e4d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-07 10:03:21 +0200

    Update 05-functionalSum.js

--- JS Advanced/6.Homework/05-functionalSum.js
+++ JS Advanced/6.Homework/05-functionalSum.js
@@ -1,10 +1,10 @@
 function add(a) {
   let curry = (b) => {
-    a += b
-    return curry
-  }
-  curry.toString = () => a
-  return curry
+    a += b;
+    return curry;
+  };
+  curry.toString = () => a;
+  return curry;
 }
 
-console.log(add(1))
+console.log(add(1));
