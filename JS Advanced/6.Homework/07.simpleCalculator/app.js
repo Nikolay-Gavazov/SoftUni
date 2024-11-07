aeadf085edc8bfa4648013b328d9767f163c18de
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-07 10:03:26 +0200

    Update app.js

--- JS Advanced/6.Homework/07.simpleCalculator/app.js
+++ JS Advanced/6.Homework/07.simpleCalculator/app.js
@@ -1,22 +1,18 @@
 function calculator() {
-    let sumEl1;
-    let sumEl2;
-    let resultEl;
+  let sumEl1;
+  let sumEl2;
+  let resultEl;
 
-    return{
-        init: (selector1, selector2, resultSelector) => {
-            sumEl1 = document.querySelector(selector1);
-            sumEl2 = document.querySelector(selector2);
-            resultEl = document.querySelector(resultSelector);
-        },
-        add: () => (resultEl.value = Number(sumEl1.value) + Number(sumEl2.value)),
-        subtract: () => (resultEl.value = sumEl1.value - sumEl2.value)
-    };
+  return {
+    init: (selector1, selector2, resultSelector) => {
+      sumEl1 = document.querySelector(selector1);
+      sumEl2 = document.querySelector(selector2);
+      resultEl = document.querySelector(resultSelector);
+    },
+    add: () => (resultEl.value = Number(sumEl1.value) + Number(sumEl2.value)),
+    subtract: () => (resultEl.value = sumEl1.value - sumEl2.value),
+  };
 }
-const calculate = calculator ();
+const calculate = calculator();
 
-calculate.init ('#num1', '#num2', '#result');
-
-
-
-
+calculate.init("#num1", "#num2", "#result");
