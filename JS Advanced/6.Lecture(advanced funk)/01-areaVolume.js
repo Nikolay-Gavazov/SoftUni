ca630acf0fc93f180b8c450e2617ce2c11e1cb59
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-14 19:03:43 +0200

    Update 01-areaVolume.js

--- JS Advanced/6.Lecture(advanced funk)/01-areaVolume.js
+++ JS Advanced/6.Lecture(advanced funk)/01-areaVolume.js
@@ -1,15 +1,19 @@
-function solve(area, vol, input){
-    let inputObj = JSON.parse(input);
+function solve(area, vol, input) {
+  let inputObj = JSON.parse(input);
 
-    function calc(obj){
-        const areaObj = Math.abs(area.call(obj));
-        const volumeObj = Math.abs(vol.call(obj));
+  function calc(obj) {
+    const areaObj = Math.abs(area.call(obj));
+    const volumeObj = Math.abs(vol.call(obj));
 
-        return {area: areaObj, volume: volumeObj};
-    }
-    return inputObj.map(calc)
+    return { area: areaObj, volume: volumeObj };
+  }
+  return inputObj.map(calc);
 }
-console.log(solve(area, vol, `[
+console.log(
+  solve(
+    area,
+    vol,
+    `[
 
     {"x":"1","y":"2","z":"10"},
     
@@ -17,11 +21,13 @@ console.log(solve(area, vol, `[
     
     {"x":"5","y":"2","z":"10"}
     
-    ]`))
+    ]`
+  )
+);
 
-    function area(){
-        return Math.abs(this.x * this.y);
-    }
-    function vol(){
-        return Math.abs(this.x * this.y * this.z);
-    }
+function area() {
+  return Math.abs(this.x * this.y);
+}
+function vol() {
+  return Math.abs(this.x * this.y * this.z);
+}
