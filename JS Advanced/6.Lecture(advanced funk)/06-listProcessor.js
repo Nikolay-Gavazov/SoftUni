a5f0a12ca922ae53e13fe23b8eb31eff70a57955
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-16 00:23:12 +0200

    Update 06-listProcessor.js

--- JS Advanced/6.Lecture(advanced funk)/06-listProcessor.js
+++ JS Advanced/6.Lecture(advanced funk)/06-listProcessor.js
@@ -1,13 +1,21 @@
 function listProcessor(input) {
-    let result = [];
-    const comands = {
-        add: (str) => result.push(str),
-        remove: (str) => result = result.filter(function(item){return item !=str}),
-        print: () => console.log(result.join(','))
-    }
-    input.forEach(el => {
-        const [command, str] = el.split(' ');
-        
-    });
+  let result = [];
+  const comands = {
+    add: (str) => result.push(str),
+    remove: (str) =>
+      (result = result.filter(function (item) {
+        return item != str;
+      })),
+    print: () => console.log(result.join(",")),
+  };
+  input.forEach((el) => {
+    const [command, str] = el.split(" ");
+  });
 }
-listProcessor(['add pesho', 'add george', 'add peter', 'remove peter','print'])
+listProcessor([
+  "add pesho",
+  "add george",
+  "add peter",
+  "remove peter",
+  "print",
+]);
