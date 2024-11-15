3cd5f40fee5f20af1cad4552e1308cbdb53788a0
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-15 23:22:07 +0200

    Update 03-currencyFormat.js

--- JS Advanced/6.Lecture(advanced funk)/03-currencyFormat.js
+++ JS Advanced/6.Lecture(advanced funk)/03-currencyFormat.js
@@ -1,20 +1,17 @@
-function createFormatter(separator, symbol, symbolFirst, formatter){
-    return (value) => formatter (separator, symbol, symbolFirst, value);
+function createFormatter(separator, symbol, symbolFirst, formatter) {
+  return (value) => formatter(separator, symbol, symbolFirst, value);
 }
 
 function currencyFormatter(separator, symbol, symbolFirst, value) {
+  let result = Math.trunc(value) + separator;
 
-    let result = Math.trunc(value) + separator;
-    
-    result += value.toFixed(2).substr(-2,2);
-    
-    if (symbolFirst) return symbol + ' ' + result;
-    
-    else return result + ' ' + symbol;
-    
-    }
+  result += value.toFixed(2).substr(-2, 2);
 
-    let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+  if (symbolFirst) return symbol + " " + result;
+  else return result + " " + symbol;
+}
+
+let dollarFormatter = createFormatter(",", "$", true, currencyFormatter);
 
 console.log(dollarFormatter(5345)); // $ 5345,00
 
