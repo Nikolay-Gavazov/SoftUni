182270c3a29e4e8b2e7ff8159256d4abb874ed7b
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-16 00:23:09 +0200

    Update 05-commandProcessor.js

--- JS Advanced/6.Lecture(advanced funk)/05-commandProcessor.js
+++ JS Advanced/6.Lecture(advanced funk)/05-commandProcessor.js
@@ -1,15 +1,15 @@
 function solution() {
-    let result = '';
-    return {
-        append: (string) => result += string,
-        removeStart: (n) => result = result.slice(n),
-        removeEnd: (n) => result = result.slice(0, -n),
-        print: () => console.log(result)
-    }
+  let result = "";
+  return {
+    append: (string) => (result += string),
+    removeStart: (n) => (result = result.slice(n)),
+    removeEnd: (n) => (result = result.slice(0, -n)),
+    print: () => console.log(result),
+  };
 }
 let firstZeroTest = solution();
-firstZeroTest.append('hello'); 
-firstZeroTest.append('again');
-firstZeroTest.removeStart(3); 
-firstZeroTest.removeEnd(4); 
+firstZeroTest.append("hello");
+firstZeroTest.append("again");
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
 firstZeroTest.print();
