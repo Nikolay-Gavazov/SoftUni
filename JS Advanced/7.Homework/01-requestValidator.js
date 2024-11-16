79976ca6ae3c680031e7603d5f4e73b3b271a8f4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-17 00:31:54 +0200

    Update 01-requestValidator.js

--- JS Advanced/7.Homework/01-requestValidator.js
+++ JS Advanced/7.Homework/01-requestValidator.js
@@ -1,34 +1,31 @@
-function requestValidator(obj){
+function requestValidator(obj) {
+  const methods = ["GET", "POST", "DELETE", "CONNECT"];
+  const version = ["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2.0"];
+  const uriPattern = /^[\d\w\.\*]+$/gm;
+  const messagePattern = /[\s\S]*^[^<>0\\&'"]+$/gm;
 
-    const methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
-    const version = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
-    const uriPattern = /^[\d\w\.\*]+$/gm;
-    const messagePattern = /[\s\S]*^[^<>0\\&'"]+$/gm
+  if (!methods.includes(obj.method)) {
+    throw Error("Invalid request header: Invalid Method");
+  }
+  if (!version.includes(obj.version)) {
+    throw Error("Invalid request header: Invalid Version");
+  }
+  if (!uriPattern.exec(obj.uri) || obj.uri == undefined) {
+    throw Error("Invalid request header: Invalid URI");
+  }
+  if (obj.message == undefined) {
+    throw Error("Invalid request header: Invalid Message");
+  } else if (!messagePattern.exec(obj.message) && obj.message.length > 0) {
+    throw Error("Invalid request header: Invalid Message");
+  }
 
-    if(!methods.includes(obj.method)){
-        throw Error("Invalid request header: Invalid Method");
-    }
-    if(!version.includes(obj.version)){
-        throw Error("Invalid request header: Invalid Version");
-    }
-    if(!uriPattern.exec(obj.uri) || obj.uri == undefined){
-        throw Error("Invalid request header: Invalid URI");
-    }
-    if(obj.message == undefined){
-        throw Error("Invalid request header: Invalid Message");
-    }else if(!messagePattern.exec(obj.message) && obj.message.length > 0){
-        throw Error("Invalid request header: Invalid Message");
-    }
-
-    return obj;
+  return obj;
 }
 requestValidator({
+  method: "GET",
 
-    method: 'GET',
-    
-    uri: 'svn.public.catalog',
-    
-    version: 'HTTP/1.1',
-    message: ''
-    })
+  uri: "svn.public.catalog",
 
+  version: "HTTP/1.1",
+  message: "",
+});
