0cc6fda207fb07d48e8b8d34acec2838b5044718
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-26 22:05:28 +0200

    Update 02-dataClass.js

--- JS Advanced/8.Homework/02-dataClass.js
+++ JS Advanced/8.Homework/02-dataClass.js
@@ -1,17 +1,20 @@
-class Request{
-    constructor(method, uri, version, message){
-        this.method = method;
-        this.uri = uri;
-        this.version = version;
-        this.message = message;
-        this.response = undefined;
-        this.fulfilled = false;
-
-    }
+class Request {
+  constructor(method, uri, version, message) {
+    this.method = method;
+    this.uri = uri;
+    this.version = version;
+    this.message = message;
+    this.response = undefined;
+    this.fulfilled = false;
+  }
 }
 
-let myData = new Request('GET',
+let myData = new Request(
+  "GET",
 
-'http://google.com', 'HTTP/1.1', '')
+  "http://google.com",
+  "HTTP/1.1",
+  ""
+);
 
 console.log(myData);
