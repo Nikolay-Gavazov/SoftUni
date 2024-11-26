818cc34e5d8d3bd56eb0beadfd4e9074fa0c2c8a
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-26 22:05:35 +0200

    Update 03-tickets.js

--- JS Advanced/8.Homework/03-tickets.js
+++ JS Advanced/8.Homework/03-tickets.js
@@ -1,33 +1,39 @@
-function tickets(arr, sorting){
-class Ticket{
-    constructor(destination, price, status){
-        this.destination = destination;
-        this.price = price;
-        this.status = status;
+function tickets(arr, sorting) {
+  class Ticket {
+    constructor(destination, price, status) {
+      this.destination = destination;
+      this.price = price;
+      this.status = status;
     }
+  }
+  const ticketsArr = [];
+  arr.forEach((el) => {
+    let [destination, price, status] = el.split("|");
+    price = Number(price);
+    const ticket = new Ticket(destination, price, status);
+    ticketsArr.push(ticket);
+  });
+  let sorted = [];
+  if (sorting == "destination") {
+    sorted = ticketsArr.sort((a, b) =>
+      a.destination.localeCompare(b.destination)
+    );
+  } else if (sorting == "status") {
+    sorted = ticketsArr.sort((a, b) => a.status.localeCompare(b.status));
+  }
+
+  return sorted;
 }
-const ticketsArr = [];
-    arr.forEach(el => {
-        let [destination, price, status] = el.split('|');
-        price = Number(price);
-        const ticket = new Ticket(destination, price, status)
-        ticketsArr.push(ticket);
-    });
-    let sorted = [];
-    if(sorting == 'destination'){
-        sorted = ticketsArr.sort((a, b) => a.destination.localeCompare(b.destination))
-    }else if(sorting == 'status'){
-        sorted = ticketsArr.sort((a, b) => a.status.localeCompare(b.status))
-    }
+tickets(
+  [
+    "Philadelphia|94.20|available",
 
-    return sorted;
-}
-tickets(['Philadelphia|94.20|available',
+    "New York City|95.99|available",
 
-'New York City|95.99|available',
+    "New York City|95.99|sold",
 
-'New York City|95.99|sold',
+    "Boston|126.20|departed",
+  ],
 
-'Boston|126.20|departed'],
-
-'status')
+  "status"
+);
