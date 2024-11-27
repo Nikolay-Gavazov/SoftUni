08ef8222f7252e79819b47c1f344c2ecdab678da
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-27 22:20:58 +0200

    Update 04.sortedList.js

--- JS Advanced/8.Homework/04.sortedList.js
+++ JS Advanced/8.Homework/04.sortedList.js
@@ -1,31 +1,30 @@
-class List{
-    constructor(){
-        this.arr = [];
-        this.size = this.arr.length;
-    }
+class List {
+  constructor() {
+    this.arr = [];
+    this.size = this.arr.length;
+  }
 
-    add(element){
-        this.arr.push(element);
-        this.arr = this.arr.sort((a,b) => a-b);
-        this.size = this.arr.length;
+  add(element) {
+    this.arr.push(element);
+    this.arr = this.arr.sort((a, b) => a - b);
+    this.size = this.arr.length;
+  }
+  remove(index) {
+    if (index >= 0 && index < this.arr.length) {
+      this.arr.splice(index, 1);
+      this.arr = this.arr.sort((a, b) => a - b);
+      this.size = this.arr.length;
+    } else {
+      throw new Error("Invalid Index!");
     }
-    remove(index){
-        if(index >= 0 && index < this.arr.length){
-            this.arr.splice(index, 1);
-            this.arr = this.arr.sort((a,b) => a-b);
-            this.size = this.arr.length;
-        }else{
-            throw new Error('Invalid Index!')
-        }
-       
+  }
+  get(index) {
+    if (index >= 0 && index < this.arr.length) {
+      return this.arr[index];
+    } else {
+      throw new Error("Invalid Index!");
     }
-    get(index){
-        if(index >= 0 && index < this.arr.length){
-            return this.arr[index]
-        }else{
-            throw new Error('Invalid Index!')
-        }
-    }
+  }
 }
 
 let list = new List();
@@ -37,6 +36,6 @@ list.add(6);
 list.add(7);
 
 console.log(list.get(1));
-list.remove(1); 
+list.remove(1);
 console.log(list.arr[1]);
 console.log(list.get(1));
