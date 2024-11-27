7906755eec259b83a6556872afc84ef8086ca942
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-27 22:21:05 +0200

    Update 05-lengthLimit.js

--- JS Advanced/8.Homework/05-lengthLimit.js
+++ JS Advanced/8.Homework/05-lengthLimit.js
@@ -1,29 +1,28 @@
-class Stringer{
-    constructor(innerString, innerLength){
-        this.innerString = innerString;
-        this.innerLength = innerLength;
+class Stringer {
+  constructor(innerString, innerLength) {
+    this.innerString = innerString;
+    this.innerLength = innerLength;
+  }
+  decrease(length) {
+    this.innerLength -= length;
+    if (this.innerLength < 0) {
+      this.innerLength = 0;
     }
-    decrease(length){
-        this.innerLength -= length;
-        if(this.innerLength < 0){
-            this.innerLength = 0;
-        }
-    }
-    increase(length){
-        this.innerLength += length;
-    }
+  }
+  increase(length) {
+    this.innerLength += length;
+  }
 
-    toString(){
-        if(this.innerLength == 0){
-            return '...';
-        }else if(this.innerLength < this.innerString.length){
-            return `${this.innerString.substring(0, this.innerLength)}...`;
-        }else{
-            return this.innerString;
-        }
+  toString() {
+    if (this.innerLength == 0) {
+      return "...";
+    } else if (this.innerLength < this.innerString.length) {
+      return `${this.innerString.substring(0, this.innerLength)}...`;
+    } else {
+      return this.innerString;
     }
+  }
 }
-
 
 let test = new Stringer("Test", 5);
 
