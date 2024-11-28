65aa82cfcb468085c02da50a055d49703c2778ad
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-28 23:14:29 +0200

    Update 06.company.js

--- JS Advanced/8.Homework/06.company.js
+++ JS Advanced/8.Homework/06.company.js
@@ -1,40 +1,39 @@
-class Company{
-    constructor(){
-        this.departments = [];
-    }
-    addEmployee(name, salary, position, department){
-        if(!name || !salary || !position || !department){
-            throw new Error('Invalid input!');
-        }else{
-            if(!this.departments[department]){
-                this.departments[department] = [];
-            }
-            
-            this.departments[department].push({name,salary,position});
-            
-        }
-        return `New employee is hired. Name: ${name}. Position: ${position}`
-    }
-    bestDepartment(){
-        const avgDep = {};
-        Object.keys(this.departments).forEach((key) => {
-            const avg = 
-            this.departments[key].reduce((acc, curr) => acc + curr.salary, 0) /
-            this.departments[key].length;
-            avgDep[key] = avg;
-        });
-        const sorted = Object.entries(avgDep).sort((a,b) => b[1]-a[1]);
-        let result = `Best Department is: ${sorted[0][0]}\nAverage salary: ${sorted[0][1].toFixed(2)}`
-        this.departments[sorted[0][0]].sort((a,b)=> b.salary - a.salary || a.name.localeCompare(b.name))
-        .forEach((el)=>{
-            result += `\n${el.name} ${el.salary} ${el.position}`
-        });
-        return result
-    }
+class Company {
+  constructor() {
+    this.departments = [];
+  }
+  addEmployee(name, salary, position, department) {
+    if (!name || !salary || !position || !department) {
+      throw new Error("Invalid input!");
+    } else {
+      if (!this.departments[department]) {
+        this.departments[department] = [];
+      }
 
+      this.departments[department].push({ name, salary, position });
+    }
+    return `New employee is hired. Name: ${name}. Position: ${position}`;
+  }
+  bestDepartment() {
+    const avgDep = {};
+    Object.keys(this.departments).forEach((key) => {
+      const avg =
+        this.departments[key].reduce((acc, curr) => acc + curr.salary, 0) /
+        this.departments[key].length;
+      avgDep[key] = avg;
+    });
+    const sorted = Object.entries(avgDep).sort((a, b) => b[1] - a[1]);
+    let result = `Best Department is: ${
+      sorted[0][0]
+    }\nAverage salary: ${sorted[0][1].toFixed(2)}`;
+    this.departments[sorted[0][0]]
+      .sort((a, b) => b.salary - a.salary || a.name.localeCompare(b.name))
+      .forEach((el) => {
+        result += `\n${el.name} ${el.salary} ${el.position}`;
+      });
+    return result;
+  }
 }
-
-
 
 let c = new Company();
 
