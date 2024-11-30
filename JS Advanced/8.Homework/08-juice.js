465ed17ef6bb93ca4925d62d97c5b7deeaf3ecef
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-30 13:12:16 +0200

    Update 08-juice.js

--- JS Advanced/8.Homework/08-juice.js
+++ JS Advanced/8.Homework/08-juice.js
@@ -1,35 +1,36 @@
 function solve(arr) {
-    const juices = {};
-    const bottles = {};
-    arr.forEach(el => {
-        let [juice, quantity] = el.split(' => ');
-        quantity = Number(quantity);
-        if (!juices.hasOwnProperty(juice)) {
-            juices[juice] = {
-                quantity: 0,
-                bottles: 0,
-            }
-        }
-        juices[juice].quantity += quantity;
-        while (juices[juice].quantity >= 1000) {
-            juices[juice].quantity -= 1000;
-            juices[juice].bottles++;
-            bottles[juice] = juices[juice].bottles;
-        }
-    });
-    for (let el in bottles) {
-        console.log(`${el} => ${bottles[el]}`);
+  const juices = {};
+  const bottles = {};
+  arr.forEach((el) => {
+    let [juice, quantity] = el.split(" => ");
+    quantity = Number(quantity);
+    if (!juices.hasOwnProperty(juice)) {
+      juices[juice] = {
+        quantity: 0,
+        bottles: 0,
+      };
     }
-
+    juices[juice].quantity += quantity;
+    while (juices[juice].quantity >= 1000) {
+      juices[juice].quantity -= 1000;
+      juices[juice].bottles++;
+      bottles[juice] = juices[juice].bottles;
+    }
+  });
+  for (let el in bottles) {
+    console.log(`${el} => ${bottles[el]}`);
+  }
 }
-solve(['Kiwi => 234',
+solve([
+  "Kiwi => 234",
 
-'Pear => 2345',
+  "Pear => 2345",
 
-'Watermelon => 3456',
+  "Watermelon => 3456",
 
-'Kiwi => 4567',
+  "Kiwi => 4567",
 
-'Pear => 5678',
+  "Pear => 5678",
 
-'Watermelon => 6789'])
+  "Watermelon => 6789",
+]);
