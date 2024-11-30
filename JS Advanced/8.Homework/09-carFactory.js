d3f6fb20d1070cdb762a19d92f449bbb50deb62f
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-11-30 13:12:22 +0200

    Update 09-carFactory.js

--- JS Advanced/8.Homework/09-carFactory.js
+++ JS Advanced/8.Homework/09-carFactory.js
@@ -1,42 +1,43 @@
 function solve(arr) {
-    const cars = {}
+  const cars = {};
 
-    arr.forEach(el => {
-        let [brand, model, quantity] = el.split(' | ');
-        quantity = Number(quantity);
-        if (!cars.hasOwnProperty(brand)) {
-            cars[brand] = {};
-        }
-        if (!cars[brand].hasOwnProperty(model)) {
-            cars[brand][model] = { quantity }
-        } else {
-            cars[brand][model].quantity += quantity;
-        }
-    });
-    for (let el in cars) {
-        console.log(el)
-        for (let car in cars[el]) {
-            console.log(`###${car} -> ${cars[el][car].quantity}`);
-
-        }
+  arr.forEach((el) => {
+    let [brand, model, quantity] = el.split(" | ");
+    quantity = Number(quantity);
+    if (!cars.hasOwnProperty(brand)) {
+      cars[brand] = {};
     }
+    if (!cars[brand].hasOwnProperty(model)) {
+      cars[brand][model] = { quantity };
+    } else {
+      cars[brand][model].quantity += quantity;
+    }
+  });
+  for (let el in cars) {
+    console.log(el);
+    for (let car in cars[el]) {
+      console.log(`###${car} -> ${cars[el][car].quantity}`);
+    }
+  }
 }
-solve(['Audi | Q7 | 1000',
+solve([
+  "Audi | Q7 | 1000",
 
-'Audi | Q6 | 100',
+  "Audi | Q6 | 100",
 
-'BMW | X5 | 1000',
+  "BMW | X5 | 1000",
 
-'BMW | X6 | 100',
+  "BMW | X6 | 100",
 
-'Citroen | C4 | 123',
+  "Citroen | C4 | 123",
 
-'Volga | GAZ-24 | 1000000',
+  "Volga | GAZ-24 | 1000000",
 
-'Lada | Niva | 1000000',
+  "Lada | Niva | 1000000",
 
-'Lada | Jigula | 1000000',
+  "Lada | Jigula | 1000000",
 
-'Citroen | C4 | 22',
+  "Citroen | C4 | 22",
 
-'Citroen | C5 | 10'])
+  "Citroen | C5 | 10",
+]);
