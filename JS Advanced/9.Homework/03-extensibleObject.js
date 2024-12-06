fbe35bcd1e77a164be4d465b63298c46be27dbe9
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-06 23:50:22 +0200

    Update 03-extensibleObject.js

--- JS Advanced/9.Homework/03-extensibleObject.js
+++ JS Advanced/9.Homework/03-extensibleObject.js
@@ -1,25 +1,23 @@
-function extensibleObject(){
-     function ExtensibleObject(){}
-     ExtensibleObject.prototype.extend = function(temp){
-        Object.entries(temp).forEach(([key, value])=> {
-            if(value instanceof Function){
-                Object.getPrototypeOf(this)[key] = value;
-            }else{
-                this[key] = value;
-            }
-        });
-    }
-    return new ExtensibleObject();
+function extensibleObject() {
+  function ExtensibleObject() {}
+  ExtensibleObject.prototype.extend = function (temp) {
+    Object.entries(temp).forEach(([key, value]) => {
+      if (value instanceof Function) {
+        Object.getPrototypeOf(this)[key] = value;
+      } else {
+        this[key] = value;
+      }
+    });
+  };
+  return new ExtensibleObject();
 }
 
 const myObj = extensibleObject();
 console.log(myObj);
 
 const temp = {
-    extensionMethod : function(){
-
-    },
-    extensionProperty: 'asd'
-}
+  extensionMethod: function () {},
+  extensionProperty: "asd",
+};
 myObj.extend(temp);
 console.log(myObj);
