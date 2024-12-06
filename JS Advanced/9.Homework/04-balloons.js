50ca2e653540882d1e8c687384546f8814e86e74
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-06 23:50:26 +0200

    Update 04-balloons.js

--- JS Advanced/9.Homework/04-balloons.js
+++ JS Advanced/9.Homework/04-balloons.js
@@ -1,54 +1,52 @@
-function solution(){
-    class Balloon{
-        constructor(color, gasWeight){
-            this.color = color;
-            this.weight = gasWeight;
-        }
+function solution() {
+  class Balloon {
+    constructor(color, gasWeight) {
+      this.color = color;
+      this.weight = gasWeight;
     }
+  }
 
-    class PartyBalloon extends Balloon{
-        constructor(color, gasWeight, ribbonColor, ribbonLength){
-            super(color, gasWeight)
-            this._ribbon = {
-                color: ribbonColor,
-                length: ribbonLength
-            }
-            
-        }
-        get ribbon(){
-            return this._ribbon;   
-        }
+  class PartyBalloon extends Balloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength) {
+      super(color, gasWeight);
+      this._ribbon = {
+        color: ribbonColor,
+        length: ribbonLength,
+      };
     }
-
-    class BirthdayBalloon extends PartyBalloon{
-        constructor(color, gasWeight, ribbonColor, ribbonLength, text){
-            super(color, gasWeight, ribbonColor, ribbonLength)
-            this._text = text;
-        }
-        get text(){
-            return this._text;
-        }
+    get ribbon() {
+      return this._ribbon;
     }
+  }
 
-    return{
-        Balloon,
-        PartyBalloon,
-        BirthdayBalloon
+  class BirthdayBalloon extends PartyBalloon {
+    constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+      super(color, gasWeight, ribbonColor, ribbonLength);
+      this._text = text;
     }
+    get text() {
+      return this._text;
+    }
+  }
 
+  return {
+    Balloon,
+    PartyBalloon,
+    BirthdayBalloon,
+  };
 }
 
 let classes = solution();
 
-let testBalloon = new classes.Balloon(
+let testBalloon = new classes.Balloon("yellow", 20.5);
 
-"yellow", 20.5);
+let testPartyBalloon = new classes.PartyBalloon(
+  "yellow",
+  20.5,
 
-let testPartyBalloon = new
-
-classes.PartyBalloon("yellow", 20.5,
-
-"red", 10.25);
+  "red",
+  10.25
+);
 
 let ribbon = testPartyBalloon.ribbon;
 
