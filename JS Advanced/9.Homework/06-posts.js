5487aa66da021b186741f0531355f8773ad8f073
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-08 14:45:34 +0200

    Update 06-posts.js

--- JS Advanced/9.Homework/06-posts.js
+++ JS Advanced/9.Homework/06-posts.js
@@ -1,50 +1,55 @@
-function solution(){
-    class Post{
-        constructor(title, content){
-            this.title = title;
-            this.content = content
-        }
-        toString(){
-            return `Post: ${this.title}\n` + `Content: ${this.content}`
-        }
+function solution() {
+  class Post {
+    constructor(title, content) {
+      this.title = title;
+      this.content = content;
     }
+    toString() {
+      return `Post: ${this.title}\n` + `Content: ${this.content}`;
+    }
+  }
 
-    class SocialMediaPost extends Post{
-        constructor(title, content, likes, dislikes){
-            super(title, content)
-            this.likes = likes;
-            this.dislikes = dislikes;
-            this._comments = [];
-        }
-        addComment(comment){
-            this._comments.push(comment);
-        }
-        toString(){
-            return super.toString() + `\n` + 
-            `Rating ${this.likes - this.dislikes}` + 
-            (this._comments.length ? `\nComents` + `\n` + (this._comments.map(c => ` * ${c}`).join(`\n`)): '')
-        }
+  class SocialMediaPost extends Post {
+    constructor(title, content, likes, dislikes) {
+      super(title, content);
+      this.likes = likes;
+      this.dislikes = dislikes;
+      this._comments = [];
     }
+    addComment(comment) {
+      this._comments.push(comment);
+    }
+    toString() {
+      return (
+        super.toString() +
+        `\n` +
+        `Rating ${this.likes - this.dislikes}` +
+        (this._comments.length
+          ? `\nComents` + `\n` + this._comments.map((c) => ` * ${c}`).join(`\n`)
+          : "")
+      );
+    }
+  }
 
-    class BlogPost extends Post{
-        constructor(title, content, views){
-            super(title,content)
-            this.views = views;
-        }
-        views(){
-            this.views++;
-            return this;
-        }
-        toString(){
-            return super.toString() + `\n` + `Views: ${this.views}`
-        }
+  class BlogPost extends Post {
+    constructor(title, content, views) {
+      super(title, content);
+      this.views = views;
     }
+    views() {
+      this.views++;
+      return this;
+    }
+    toString() {
+      return super.toString() + `\n` + `Views: ${this.views}`;
+    }
+  }
 
-    return{
-        Post,
-        SocialMediaPost,
-        BlogPost
-    }
+  return {
+    Post,
+    SocialMediaPost,
+    BlogPost,
+  };
 }
 
 const classes = solution();
