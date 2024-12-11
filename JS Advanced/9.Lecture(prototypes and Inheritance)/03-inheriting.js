3c7ca77c1289bb4c55c9958e93de7b35315be9e4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-12 00:04:57 +0200

    Update 03-inheriting.js

--- JS Advanced/9.Lecture(prototypes and Inheritance)/03-inheriting.js
+++ JS Advanced/9.Lecture(prototypes and Inheritance)/03-inheriting.js
@@ -1,31 +1,30 @@
-function personAndTeacher(){
-    class Person{
-        constructor(name, email){
-            this.name = name;
-            this.email = email;
-        }
-        toString(){`Name: ${this.name}, Email: ${this.email}`};
+function personAndTeacher() {
+  class Person {
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
     }
+    toString() {
+      `Name: ${this.name}, Email: ${this.email}`;
+    }
+  }
 
+  class Teacher extends Person {
+    constructor(name, email, subject) {
+      super(name, email);
+      this.subject = subject;
+    }
+  }
+  class Student extends Person {
+    constructor(name, email, course) {
+      super(name, email);
+      this.course = course;
+    }
+  }
 
-    class Teacher extends Person{
-        constructor(name, email, subject){
-            super(name, email)
-            this.subject = subject;
-        }
-        
-    }
-    class Student extends Person{
-        constructor(name, email,course){
-            super(name, email)
-            this.course = course;
-        }
-    }
-
-    return{
-        Person,
-        Teacher,
-        Student
-    }
-    
+  return {
+    Person,
+    Teacher,
+    Student,
+  };
 }
