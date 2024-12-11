9c4ed7d9f8a437027d948109c730e675f84ba825
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-11 23:13:52 +0200

    Update 02-personTeacher.js

--- JS Advanced/9.Lecture(prototypes and Inheritance)/02-personTeacher.js
+++ JS Advanced/9.Lecture(prototypes and Inheritance)/02-personTeacher.js
@@ -1,21 +1,20 @@
-function personAndTeacher(){
-    class Person{
-        constructor(name, email){
-            this.name = name;
-            this.email = email;
-        }
+function personAndTeacher() {
+  class Person {
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
     }
+  }
 
-    class Teacher extends Person{
-        constructor(name, email, subject){
-            super(name, email)
-            this.subject = subject;
-        }
-        
+  class Teacher extends Person {
+    constructor(name, email, subject) {
+      super(name, email);
+      this.subject = subject;
     }
+  }
 
-    return{
-        Person,
-        Teacher
-    }
+  return {
+    Person,
+    Teacher,
+  };
 }
