faeaedf77aae6dfe680899ed47a77647c1394ea8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-19 09:39:35 +0200

    Update app.js

--- JS Advanced/ExamPrep/Exam-07Dec2022/01-hotelReservation/app.js
+++ JS Advanced/ExamPrep/Exam-07Dec2022/01-hotelReservation/app.js
@@ -1,31 +1,38 @@
-window.addEventListener('load', solve);
+window.addEventListener("load", solve);
 
 function solve() {
-    const firstName = document.getElementById('first-name');
-    const lastName = document.getElementById('last-name');
-    const dateIn = document.getElementById('date-in')
-    const dateOut = document.getElementById('date-out')
-    const numOfGuests = document.getElementById('people-count');
-    const nextBtn = document.getElementById('next-btn')
-    nextBtn.addEventListener('click', next);
+  const firstName = document.getElementById("first-name");
+  const lastName = document.getElementById("last-name");
+  const dateIn = document.getElementById("date-in");
+  const dateOut = document.getElementById("date-out");
+  const numOfGuests = document.getElementById("people-count");
+  const nextBtn = document.getElementById("next-btn");
+  nextBtn.addEventListener("click", next);
 
-    const infoSection = document.querySelector('.info-list');
-    const confirmSection = document.querySelector('.confirm-list');
-    const completeSection = document.getElementById('verification');
+  const infoSection = document.querySelector(".info-list");
+  const confirmSection = document.querySelector(".confirm-list");
+  const completeSection = document.getElementById("verification");
 
-    function next(e) {
-        e.preventDefault();
-        const fName = firstName.value;
-        const lName = lastName.value;
-        const checkIn = dateIn.value;
-        const checkOut = dateOut.value;
-        const guests = numOfGuests.value;
-        if (!fName || !lName || !checkIn || !checkOut || !guests || checkIn > checkOut) {
-            return
-        }
-        const li = document.createElement('li');
-        li.className = 'reservation-content';
-        li.innerHTML = `
+  function next(e) {
+    e.preventDefault();
+    const fName = firstName.value;
+    const lName = lastName.value;
+    const checkIn = dateIn.value;
+    const checkOut = dateOut.value;
+    const guests = numOfGuests.value;
+    if (
+      !fName ||
+      !lName ||
+      !checkIn ||
+      !checkOut ||
+      !guests ||
+      checkIn > checkOut
+    ) {
+      return;
+    }
+    const li = document.createElement("li");
+    li.className = "reservation-content";
+    li.innerHTML = `
             <article>
             <h3>Name: ${fName} ${lName}</h3>
             <p>From date: ${checkIn}</p>
@@ -35,28 +42,28 @@ function solve() {
             <button class = 'edit-btn'>Edit</button>
             <button class = 'continue-btn'>Continue</button>
             `;
-        li.querySelector('.edit-btn').addEventListener('click', edit);
-        li.querySelector('.continue-btn').addEventListener('click', conti);
-        infoSection.appendChild(li);
-        nextBtn.disabled = true;
-        firstName.value = ''
-        lastName.value = ''
-        dateIn.value = ''
-        dateOut.value = ''
-        numOfGuests.value = ''
-        function edit() {
-            nextBtn.disabled = false;
-            firstName.value = fName;
-            lastName.value = lName;
-            dateIn.value = checkIn;
-            dateOut.value = checkOut;
-            numOfGuests.value = guests;
-            li.remove();
-        }
+    li.querySelector(".edit-btn").addEventListener("click", edit);
+    li.querySelector(".continue-btn").addEventListener("click", conti);
+    infoSection.appendChild(li);
+    nextBtn.disabled = true;
+    firstName.value = "";
+    lastName.value = "";
+    dateIn.value = "";
+    dateOut.value = "";
+    numOfGuests.value = "";
+    function edit() {
+      nextBtn.disabled = false;
+      firstName.value = fName;
+      lastName.value = lName;
+      dateIn.value = checkIn;
+      dateOut.value = checkOut;
+      numOfGuests.value = guests;
+      li.remove();
+    }
 
-        function conti() {
-            li.remove();
-            li.innerHTML = `
+    function conti() {
+      li.remove();
+      li.innerHTML = `
             <article>
             <h3>Name: ${fName} ${lName}</h3>
             <p>From date: ${checkIn}</p>
@@ -66,29 +73,22 @@ function solve() {
             <button class = 'confirm-btn'>Confirm</button>
             <button class = 'cancel-btn'>Cancel</button>
             `;
-            li.querySelector('.confirm-btn').addEventListener('click', confirm);
-            li.querySelector('.cancel-btn').addEventListener('click', cancel);
-            confirmSection.appendChild(li);
-        }
-
-        function confirm() {
-            li.remove();
-            nextBtn.disabled = false;
-            completeSection.className = 'reservation-confirmed';
-            completeSection.textContent = 'Confirmed.';
-
-        }
-        function cancel() {
-            li.remove();
-            nextBtn.disabled = false;
-            completeSection.className = 'reservation-cancelled';
-            completeSection.textContent = 'Cancelled.';
-        }
+      li.querySelector(".confirm-btn").addEventListener("click", confirm);
+      li.querySelector(".cancel-btn").addEventListener("click", cancel);
+      confirmSection.appendChild(li);
     }
 
+    function confirm() {
+      li.remove();
+      nextBtn.disabled = false;
+      completeSection.className = "reservation-confirmed";
+      completeSection.textContent = "Confirmed.";
     }
-
-
-
-    
-    
+    function cancel() {
+      li.remove();
+      nextBtn.disabled = false;
+      completeSection.className = "reservation-cancelled";
+      completeSection.textContent = "Cancelled.";
+    }
+  }
+}
