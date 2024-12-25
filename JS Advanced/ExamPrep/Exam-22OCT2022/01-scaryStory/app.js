4df1c110e218709ba6e8aa3888fde27c9d535cdf
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-25 23:34:32 +0200

    Update app.js

--- JS Advanced/ExamPrep/Exam-22OCT2022/01-scaryStory/app.js
+++ JS Advanced/ExamPrep/Exam-22OCT2022/01-scaryStory/app.js
@@ -1,21 +1,20 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  const firstName = document.getElementById('first-name');
-  const lastName = document.getElementById('last-name');
-  const age = document.getElementById('age');
-  const storyTitle = document.getElementById('story-title');
-  const genre = document.getElementById('genre');
-  const story = document.getElementById('story');
-  const publishBtn = document.getElementById('form-btn');
-  publishBtn.addEventListener('click', publish);
+  const firstName = document.getElementById("first-name");
+  const lastName = document.getElementById("last-name");
+  const age = document.getElementById("age");
+  const storyTitle = document.getElementById("story-title");
+  const genre = document.getElementById("genre");
+  const story = document.getElementById("story");
+  const publishBtn = document.getElementById("form-btn");
+  publishBtn.addEventListener("click", publish);
 
-  const preview = document.getElementById('preview-list');
-  const main = document.getElementById('main');
+  const preview = document.getElementById("preview-list");
+  const main = document.getElementById("main");
 
-  const inputDiv = document.querySelector('.form-wrapper');
-  const previewDiv = document.getElementById('side-wrapper');
-
+  const inputDiv = document.querySelector(".form-wrapper");
+  const previewDiv = document.getElementById("side-wrapper");
 
   function publish(e) {
     e.preventDefault();
@@ -26,10 +25,17 @@ function solve() {
     const genreInp = genre.value;
     const storyInp = story.value;
 
-    if (!fName || !lName || !ageInp || !storyTitleInp || !genreInp || !storyInp) {
-      return
+    if (
+      !fName ||
+      !lName ||
+      !ageInp ||
+      !storyTitleInp ||
+      !genreInp ||
+      !storyInp
+    ) {
+      return;
     }
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerHTML = `
     <article>
     <h4>Name: ${fName} ${lName}</h4>
@@ -41,27 +47,25 @@ function solve() {
     <button class = 'save-btn'>Save Story</button>
     <button class = 'edit-btn'>Edit Story</button>
     <button class = 'delete-btn'>Delete Story</button>
-    `
-    li.querySelector('.save-btn').addEventListener('click', save)
-    li.querySelector('.edit-btn').addEventListener('click', edit)
-    li.querySelector('.delete-btn').addEventListener('click', deleting)
+    `;
+    li.querySelector(".save-btn").addEventListener("click", save);
+    li.querySelector(".edit-btn").addEventListener("click", edit);
+    li.querySelector(".delete-btn").addEventListener("click", deleting);
 
     preview.appendChild(li);
-    firstName.value = '';
-    lastName.value = '';
-    age.value = '';
-    storyTitle.value = '';
-    genre.value = '';
-    story.value = '';
+    firstName.value = "";
+    lastName.value = "";
+    age.value = "";
+    storyTitle.value = "";
+    genre.value = "";
+    story.value = "";
     publishBtn.disabled = true;
-
-
 
     function save() {
       inputDiv.remove();
       previewDiv.remove();
-      const h1 = document.createElement('h1');
-      h1.textContent = 'Yout scary story is saved!';
+      const h1 = document.createElement("h1");
+      h1.textContent = "Yout scary story is saved!";
       main.appendChild(h1);
     }
 
