5b65175b0cb276b3115f9d54f0537104962f1ed6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-12-29 23:58:16 +0200

    Update 02-oddOrEven.js

--- JS Advanced/tests/02-oddOrEven.js
+++ JS Advanced/tests/02-oddOrEven.js
@@ -1,15 +1,14 @@
-const {expect} = require('chai');
-const isOddOrEven = require('../7.Homework/02-oddOrEven');
+const { expect } = require("chai");
+const isOddOrEven = require("../7.Homework/02-oddOrEven");
 
-describe('isOdd or isEven', function(){
-    it('return undefined if input is not string', () =>{
-        expect(isOddOrEven(123)).to.be.undefined;
-    })
-    it('return even', ()=>{
-        expect(isOddOrEven('asdf')).to.equal('even');
-    })
-    it('return odd', ()=>{
-        expect(isOddOrEven('asd')).to.equal('odd');
-    })
-
-})
+describe("isOdd or isEven", function () {
+  it("return undefined if input is not string", () => {
+    expect(isOddOrEven(123)).to.be.undefined;
+  });
+  it("return even", () => {
+    expect(isOddOrEven("asdf")).to.equal("even");
+  });
+  it("return odd", () => {
+    expect(isOddOrEven("asd")).to.equal("odd");
+  });
+});
