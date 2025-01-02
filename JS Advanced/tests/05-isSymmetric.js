b525680976c32820757aed1c7d582869d14dd3bf
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-02 23:31:17 +0200

    Update 05-isSymmetric.js

--- JS Advanced/tests/05-isSymmetric.js
+++ JS Advanced/tests/05-isSymmetric.js
@@ -1,33 +1,33 @@
-const {expect} = require('chai');
-const isSymmetric = require('../7.Lecture(tests)/05-checkSymmetry');
+const { expect } = require("chai");
+const isSymmetric = require("../7.Lecture(tests)/05-checkSymmetry");
 
-describe('Symmetric check', function(){
-    it('return true for symmetric', () =>{
-        const arr = [2,3,3,2];
-        expect(isSymmetric(arr)).to.be.true;
-    })
-    it('return true when length is odd', () =>{
-        const arr = [1,2,1];
-        expect(isSymmetric(arr)).to.be.true;
-    })
-    it('work with array with numbers as strings', ()=>{
-        const arr = ['1','2','2','1'];
-        expect(isSymmetric(arr)).to.be.true;
-    })
-    it('return false for non-symmetric', () =>{
-        const arr = [1,2,3];
-        expect(isSymmetric(arr)).to.be.false;
-    })
-    it('false with non-array', () =>{
-        const arr = 44332;
-        expect(isSymmetric(arr)).to.be.false;
-    })
-    it('return false for mix array', ()=>{
-        const arr = ['1',2,2,1];
-        expect(isSymmetric(arr)).to.be.false;
-    })
-    it('work with array of strings', ()=>{
-        const arr = ['a','b','b','a'];
-        expect(isSymmetric(arr)).to.be.true;
-    })
-    })
+describe("Symmetric check", function () {
+  it("return true for symmetric", () => {
+    const arr = [2, 3, 3, 2];
+    expect(isSymmetric(arr)).to.be.true;
+  });
+  it("return true when length is odd", () => {
+    const arr = [1, 2, 1];
+    expect(isSymmetric(arr)).to.be.true;
+  });
+  it("work with array with numbers as strings", () => {
+    const arr = ["1", "2", "2", "1"];
+    expect(isSymmetric(arr)).to.be.true;
+  });
+  it("return false for non-symmetric", () => {
+    const arr = [1, 2, 3];
+    expect(isSymmetric(arr)).to.be.false;
+  });
+  it("false with non-array", () => {
+    const arr = 44332;
+    expect(isSymmetric(arr)).to.be.false;
+  });
+  it("return false for mix array", () => {
+    const arr = ["1", 2, 2, 1];
+    expect(isSymmetric(arr)).to.be.false;
+  });
+  it("work with array of strings", () => {
+    const arr = ["a", "b", "b", "a"];
+    expect(isSymmetric(arr)).to.be.true;
+  });
+});
