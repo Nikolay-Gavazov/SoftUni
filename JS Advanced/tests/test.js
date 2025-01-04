595f4f1ae9c92cbed34c6dbee694da92a1410974
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-04 17:07:46 +0200

    Update test.js

--- JS Advanced/tests/test.js
+++ JS Advanced/tests/test.js
@@ -1,8 +1,8 @@
-const {expect} = require('chai');
-const argumentInfo = require('../6.Homework/02-argument');
+const { expect } = require("chai");
+const argumentInfo = require("../6.Homework/02-argument");
 
-describe('argumentInfo', ()=>{
-    it('isString', ()=>{
-        expect(argumentInfo('a')).to.be.undefined
-    })
-})
+describe("argumentInfo", () => {
+  it("isString", () => {
+    expect(argumentInfo("a")).to.be.undefined;
+  });
+});
