0c4d8dfa79fcd93ca8646368ce864464e7242803
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-04 17:07:41 +0200

    Update sumTest.js

--- JS Advanced/tests/sumTest.js
+++ JS Advanced/tests/sumTest.js
@@ -1,19 +1,17 @@
-const {expect} = require('chai');
-const sum = require('../7.Lecture(tests)/04-sumNumbers');
+const { expect } = require("chai");
+const sum = require("../7.Lecture(tests)/04-sumNumbers");
 
-describe('sum', function (){
-    it('can sum numbers', ()=>{
-        expect(sum([2,5,10])).to.be.equal(17);
-    })
-    it('isNaN', ()=>{
-        expect(sum(['a', 1])).to.be.NaN
-    })
-    it('can sum tho nums came as String', () =>{
-        expect(sum(['1', '3'])).to.be.equal(4)
-    })
-    it('works with floating-point numbers', () =>{
-        expect(sum([1.5, 0.5])).to.be.equal(2)
-    })
-
-
-})
+describe("sum", function () {
+  it("can sum numbers", () => {
+    expect(sum([2, 5, 10])).to.be.equal(17);
+  });
+  it("isNaN", () => {
+    expect(sum(["a", 1])).to.be.NaN;
+  });
+  it("can sum tho nums came as String", () => {
+    expect(sum(["1", "3"])).to.be.equal(4);
+  });
+  it("works with floating-point numbers", () => {
+    expect(sum([1.5, 0.5])).to.be.equal(2);
+  });
+});
