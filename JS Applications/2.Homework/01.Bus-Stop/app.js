cf683a4785a8027f34139e59a4cd1668e7d53056
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-10 18:34:41 +0200

    Update app.js

--- JS Applications/2.Homework/01.Bus-Stop/app.js
+++ JS Applications/2.Homework/01.Bus-Stop/app.js
@@ -1,23 +1,24 @@
 async function getInfo() {
-    const stopId = document.getElementById('stopId').value;
-    const buses = document.getElementById('buses');
-    const stopName = document.getElementById('stopName');
-    buses.innerHTML = ''
+  const stopId = document.getElementById("stopId").value;
+  const buses = document.getElementById("buses");
+  const stopName = document.getElementById("stopName");
+  buses.innerHTML = "";
 
-    try {
-        const responce = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
-        if(responce.ok == false) throw Error;
-        
-        const data = await responce.json();
-        console.log(data.buses);
-        stopName.textContent = data.name;
-        Object.entries(data.buses).forEach(([busId, time]) => {
-            const li = document.createElement('li');
-            li.innerHTML = `Bus ${busId} arrives in ${time} minutes`;
-            buses.appendChild(li);           
-        });
+  try {
+    const responce = await fetch(
+      `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+    );
+    if (responce.ok == false) throw Error;
 
-    } catch (error) {
-        stopName.textContent = 'Error';
-    }
+    const data = await responce.json();
+    console.log(data.buses);
+    stopName.textContent = data.name;
+    Object.entries(data.buses).forEach(([busId, time]) => {
+      const li = document.createElement("li");
+      li.innerHTML = `Bus ${busId} arrives in ${time} minutes`;
+      buses.appendChild(li);
+    });
+  } catch (error) {
+    stopName.textContent = "Error";
+  }
 }
