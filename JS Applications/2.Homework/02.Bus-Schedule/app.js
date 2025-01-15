f43371625f3c2a98f726efef7bacb85b81f52245
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-15 22:07:12 +0200

    Update app.js

--- JS Applications/2.Homework/02.Bus-Schedule/app.js
+++ JS Applications/2.Homework/02.Bus-Schedule/app.js
@@ -1,46 +1,43 @@
 function solve() {
+  const departBtn = document.getElementById("depart");
+  const arriveBtn = document.getElementById("arrive");
+  const info = document.querySelector(".info");
 
-    const departBtn = document.getElementById('depart');
-    const arriveBtn = document.getElementById('arrive');
-    const info = document.querySelector('.info');
+  let stopId = "depot";
+  let stopName;
 
-    let stopId = 'depot';
-    let stopName;
-
-   async function depart() {
+  async function depart() {
     try {
-        const responce = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`);
-        if(responce.ok == false) throw Error;
-        
-        const data = await responce.json();
-        stopName = data.name;
-        info.textContent = `Next stop ${stopName}`;
-        stopId = data.next
-        
+      const responce = await fetch(
+        `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
+      );
+      if (responce.ok == false) throw Error;
+
+      const data = await responce.json();
+      stopName = data.name;
+      info.textContent = `Next stop ${stopName}`;
+      stopId = data.next;
     } catch (error) {
-        info.textContent = 'Error';
-        departBtn.disabled = true;
-        arriveBtn.disabled = true;
+      info.textContent = "Error";
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
     }
 
+    departBtn.disabled = true;
+    arriveBtn.disabled = false;
+  }
 
-        departBtn.disabled = true;
-        arriveBtn.disabled = false;
-    }
+  function arrive() {
+    info.textContent = `Arriving at ${stopName}`;
 
-    function arrive() {
-        info.textContent = `Arriving at ${stopName}`;
+    departBtn.disabled = false;
+    arriveBtn.disabled = true;
+  }
 
-
-
-        departBtn.disabled = false;
-        arriveBtn.disabled = true;
-    }
-
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
