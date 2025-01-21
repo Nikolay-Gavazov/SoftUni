99a645123fea2125fb8fa081b4335860a1456997
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-21 21:47:42 +0200

    Update app.js

--- JS Applications/2.Homework/04.Locked-Profile/app.js
+++ JS Applications/2.Homework/04.Locked-Profile/app.js
@@ -1,17 +1,18 @@
 async function lockedProfile() {
-    const main = document.getElementById('main');
-    
+  const main = document.getElementById("main");
 
-    try {
-        const responce = await fetch('http://localhost:3030/jsonstore/advanced/profiles/');
-        if(responce.ok == false) throw Error;
-        const data = await responce.json();
-        main.innerHTML = '';
-        
-        if(data._id){
-            const div = document.createElement('div');
-            div.className = 'profile';
-            div.innerHTML = `
+  try {
+    const responce = await fetch(
+      "http://localhost:3030/jsonstore/advanced/profiles/"
+    );
+    if (responce.ok == false) throw Error;
+    const data = await responce.json();
+    main.innerHTML = "";
+
+    if (data._id) {
+      const div = document.createElement("div");
+      div.className = "profile";
+      div.innerHTML = `
             <img src="./iconProfile2.png" class="userIcon" />
 				<label>Lock</label>
 				<input type="radio" name='user1Locked' value="lock" checked>
@@ -29,15 +30,15 @@ async function lockedProfile() {
 				</div>
 				
 				<button>Show more</button>
-            `
-            
-            main.appendChild(div);
-            document.querySelector(`.user1Username`).style.display = 'none';
+            `;
 
-        }else{Object.entries(data).forEach((el, i )=> {
-            const div = document.createElement('div');
-            div.className = 'profile';
-            div.innerHTML = `
+      main.appendChild(div);
+      document.querySelector(`.user1Username`).style.display = "none";
+    } else {
+      Object.entries(data).forEach((el, i) => {
+        const div = document.createElement("div");
+        div.className = "profile";
+        div.innerHTML = `
             <img src="./iconProfile2.png" class="userIcon" />
 				<label>Lock</label>
 				<input type="radio" name='user${i + 1}Locked' value="lock" checked>
@@ -45,46 +46,53 @@ async function lockedProfile() {
 				<input type="radio" name='user${i + 1}Locked' value="unlock"><br>
 				<hr>
 				<label>Username</label>
-				<input type="text" name="user${i+1}Username" value="${el[1].username}" disabled readonly />
-				<div id ="user${i+1}HiddenFields">
+				<input type="text" name="user${i + 1}Username" value="${
+          el[1].username
+        }" disabled readonly />
+				<div id ="user${i + 1}HiddenFields">
 					<hr>
 					<label>Email:</label>
-					<input type="email" name="user${i+1}Email" value="${el[1].email}" disabled readonly />
+					<input type="email" name="user${i + 1}Email" value="${
+          el[1].email
+        }" disabled readonly />
 					<label>Age:</label>
-					<input type="email" name="user${i+1}Age" value="${el[1].age}" disabled readonly />
+					<input type="email" name="user${i + 1}Age" value="${
+          el[1].age
+        }" disabled readonly />
 				</div>
 				
 				<button>Show more</button>
-            `
-            
-            main.appendChild(div);
-            document.getElementById(`user${i+1}HiddenFields`).style.display = 'none';
-        });}
-        
-    } catch (error) {
-        console.log(error);
+            `;
+
+        main.appendChild(div);
+        document.getElementById(`user${i + 1}HiddenFields`).style.display =
+          "none";
+      });
     }
-    const buttons = Array.from(document.querySelectorAll('button'))
+  } catch (error) {
+    console.log(error);
+  }
+  const buttons = Array.from(document.querySelectorAll("button"));
 
-    buttons.forEach((button, i) => {
+  buttons.forEach((button, i) => {
+    button.addEventListener(
+      "click",
+      (func = () => {
+        const lock = document.getElementsByName(`user${i + 1}Locked`)[0]
+          .checked;
+        const hiddenInfo = document.getElementById(`user${i + 1}HiddenFields`);
 
-        button.addEventListener('click', func = () => {
-            const lock = document.getElementsByName(`user${i+1}Locked`)[0].checked;
-            const hiddenInfo = document.getElementById(`user${i+1}HiddenFields`)
-            
-            if (lock) {
-
-            } else {
-                if (button.textContent == 'Show more') {
-                    button.textContent = 'Hide it'
-                    hiddenInfo.style.display = 'block';
-                } else {
-                    button.textContent = 'Show more';
-                    hiddenInfo.style.display = 'none';
-                }
-            }
-        })
-
-    });
-    
+        if (lock) {
+        } else {
+          if (button.textContent == "Show more") {
+            button.textContent = "Hide it";
+            hiddenInfo.style.display = "block";
+          } else {
+            button.textContent = "Show more";
+            hiddenInfo.style.display = "none";
+          }
+        }
+      })
+    );
+  });
 }
