b077b5b5853327c7e8bb76b453aa27b64bb86802
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-01-25 21:50:59 +0200

    Update accordion.js

--- JS Applications/2.Homework/05. Accordion/accordion.js
+++ JS Applications/2.Homework/05. Accordion/accordion.js
@@ -1,16 +1,18 @@
-window.addEventListener('load', solution);
+window.addEventListener("load", solution);
 async function solution() {
-    const section = document.getElementById('main');
+  const section = document.getElementById("main");
 
-    try {
-        const responce = await fetch(`http://localhost:3030/jsonstore/advanced/articles/list`);
-        if (responce.ok == false) throw Error;
+  try {
+    const responce = await fetch(
+      `http://localhost:3030/jsonstore/advanced/articles/list`
+    );
+    if (responce.ok == false) throw Error;
 
-        const data = await responce.json();
-        data.forEach(el => {
-            const div = document.createElement('div');
-            div.className = 'accordion';
-            div.innerHTML = `
+    const data = await responce.json();
+    data.forEach((el) => {
+      const div = document.createElement("div");
+      div.className = "accordion";
+      div.innerHTML = `
             <div class="head">
             <span>${el.title}</span>
             <button class="button" id="${el._id}">More</button>
@@ -20,38 +22,37 @@ async function solution() {
             </div>
             `;
 
-            section.appendChild(div);
-            document.getElementById(`${el._id}`).addEventListener('click', more);
-        });
+      section.appendChild(div);
+      document.getElementById(`${el._id}`).addEventListener("click", more);
+    });
 
-        async function more(e) {
-            const parrent = e.target.parentElement;
-            const button = document.getElementById(`${e.target.id}`)
-            const div = parrent.parentElement.children[1];
-            let text = ''
-            try {
-                const responce = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${e.target.id}`);
+    async function more(e) {
+      const parrent = e.target.parentElement;
+      const button = document.getElementById(`${e.target.id}`);
+      const div = parrent.parentElement.children[1];
+      let text = "";
+      try {
+        const responce = await fetch(
+          `http://localhost:3030/jsonstore/advanced/articles/details/${e.target.id}`
+        );
 
-                if (responce.ok == false) throw Error;
+        if (responce.ok == false) throw Error;
 
-                const data = await responce.json();
-                text = data.content;
-            } catch (error) {
-                console.log(error);
-            }
-            if (button.textContent == 'More') {
-                div.children[0].textContent = text;
-                button.textContent = 'Less';
-                div.style.display = 'block';
-                
-
-            } else {
-                button.textContent = 'More';
-                div.style.display = 'none';
-            }
-
-        }
-    } catch (error) {
+        const data = await responce.json();
+        text = data.content;
+      } catch (error) {
         console.log(error);
+      }
+      if (button.textContent == "More") {
+        div.children[0].textContent = text;
+        button.textContent = "Less";
+        div.style.display = "block";
+      } else {
+        button.textContent = "More";
+        div.style.display = "none";
+      }
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
