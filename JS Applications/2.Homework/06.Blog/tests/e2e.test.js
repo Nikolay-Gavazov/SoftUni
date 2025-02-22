3022930e27dd6b9e5728296e10ee2bbc9156da39
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2025-02-22 08:58:54 +0200

    Update e2e.test.js

--- JS Applications/2.Homework/06.Blog/tests/e2e.test.js
+++ JS Applications/2.Homework/06.Blog/tests/e2e.test.js
@@ -1,7 +1,7 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("playwright-chromium");
+const { expect } = require("chai");
 
-const host = 'http://localhost:3000'; // Application host (NOT service host - that can be anything)
+const host = "http://localhost:3000"; // Application host (NOT service host - that can be anything)
 
 const DEBUG = false;
 const slowMo = 500;
@@ -9,35 +9,35 @@ const slowMo = 500;
 const mockData = {
   blog: [
     {
-      body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis maiores eligendi quos quidem ex numquam hic. Eos quos similique voluptates accusamus quae voluptas magni ad a ipsum, quia enim debitis cumque quibusdam exercitationem architecto sint nostrum dolorum dolor repudiandae nulla deserunt, dolorem itaque!',
-      id: '1001',
-      title: 'Unit Testing And Modules',
+      body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis maiores eligendi quos quidem ex numquam hic. Eos quos similique voluptates accusamus quae voluptas magni ad a ipsum, quia enim debitis cumque quibusdam exercitationem architecto sint nostrum dolorum dolor repudiandae nulla deserunt, dolorem itaque!",
+      id: "1001",
+      title: "Unit Testing And Modules",
     },
   ],
   comments: [
     {
-      id: '-MSgyQMjBNfYjW2m6r97',
-      postId: '1001',
-      text: 'A very interesting post!',
+      id: "-MSgyQMjBNfYjW2m6r97",
+      postId: "1001",
+      text: "A very interesting post!",
     },
     {
-      id: '-MSgySbWEFw3rhCfIIns',
-      postId: '1001',
-      text: 'Unit Testing is a useful testing technique in programming.',
+      id: "-MSgySbWEFw3rhCfIIns",
+      postId: "1001",
+      text: "Unit Testing is a useful testing technique in programming.",
     },
   ],
 };
 
 const endpoints = {
-  blog: '/jsonstore/blog/posts',
-  comments: '/jsonstore/blog/comments',
+  blog: "/jsonstore/blog/posts",
+  comments: "/jsonstore/blog/comments",
 };
 
 let browser;
 let context;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   // Setup
   this.timeout(DEBUG ? 120000 : 7000);
   before(
@@ -58,27 +58,27 @@ describe('E2E tests', function () {
   });
 
   // Test proper
-  describe('Blog Info', () => {
-    it('Load Posts correct API call', async () => {
+  describe("Blog Info", () => {
+    it("Load Posts correct API call", async () => {
       const { get } = await handle(endpoints.blog);
       const isCalled = get().isHandled;
 
       await page.goto(host);
-      await page.waitForSelector('#btnLoadPosts');
-      await page.click('text=Load Posts');
-      await page.waitForSelector('#posts');
+      await page.waitForSelector("#btnLoadPosts");
+      await page.click("text=Load Posts");
+      await page.waitForSelector("#posts");
 
       expect(isCalled()).to.be.true;
     });
 
-    it('Load Posts', async () => {
+    it("Load Posts", async () => {
       const data = mockData.blog;
       const { get } = await handle(endpoints.blog);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('#btnLoadPosts');
-      await page.click('text=Load Posts');
+      await page.waitForSelector("#btnLoadPosts");
+      await page.click("text=Load Posts");
 
       const post = await page.$$eval(`#posts`, (t) =>
         t.map((s) => s.textContent)
@@ -86,7 +86,7 @@ describe('E2E tests', function () {
       expect(post.length).to.equal(data.length);
     });
 
-    it('Check post title', async () => {
+    it("Check post title", async () => {
       const data = mockData.blog;
       const comment = mockData.comments;
       const { get } = await handle(endpoints.blog);
@@ -97,11 +97,11 @@ describe('E2E tests', function () {
       get2(comment);
 
       await page.goto(host);
-      await page.waitForSelector('#btnLoadPosts');
-      await page.click('text=Load Posts');
+      await page.waitForSelector("#btnLoadPosts");
+      await page.click("text=Load Posts");
 
-      await page.waitForSelector('#btnViewPost');
-      await page.click('text=View');
+      await page.waitForSelector("#btnViewPost");
+      await page.click("text=View");
 
       const post = await page.$$eval(`#post-title`, (t) =>
         t.map((s) => s.textContent)
@@ -109,7 +109,7 @@ describe('E2E tests', function () {
       expect(post[0]).to.equal(data[0].title);
     });
 
-    it('Check post body', async () => {
+    it("Check post body", async () => {
       const data = mockData.blog;
       const comment = mockData.comments;
 
@@ -119,11 +119,11 @@ describe('E2E tests', function () {
 
       get2(comment);
       await page.goto(host);
-      await page.waitForSelector('#btnLoadPosts');
-      await page.click('text=Load Posts');
+      await page.waitForSelector("#btnLoadPosts");
+      await page.click("text=Load Posts");
 
-      await page.waitForSelector('#btnViewPost');
-      await page.click('text=View');
+      await page.waitForSelector("#btnViewPost");
+      await page.click("text=View");
 
       const post = await page.$$eval(`#post-body`, (t) =>
         t.map((s) => s.textContent)
@@ -136,40 +136,40 @@ describe('E2E tests', function () {
 async function setupContext(context) {
   // Catalog and Details
   await handleContext(context, endpoints.blog, { get: mockData.blog });
-  await handleContext(context, endpoints.comments('1001'), {
+  await handleContext(context, endpoints.comments("1001"), {
     get: mockData.comments[0],
   });
 
-  await handleContext(context, endpoints.details('1001'), {
+  await handleContext(context, endpoints.details("1001"), {
     get: mockData.catalog[0],
   });
-  await handleContext(context, endpoints.details('1002'), {
+  await handleContext(context, endpoints.details("1002"), {
     get: mockData.catalog[1],
   });
-  await handleContext(context, endpoints.details('1003'), {
+  await handleContext(context, endpoints.details("1003"), {
     get: mockData.catalog[2],
   });
 
   await handleContext(
-    endpoints.profile('0001'),
+    endpoints.profile("0001"),
     { get: mockData.catalog.slice(0, 2) },
     context
   );
 
-  await handleContext(endpoints.total('1001'), { get: 6 }, context);
-  await handleContext(endpoints.total('1002'), { get: 4 }, context);
-  await handleContext(endpoints.total('1003'), { get: 7 }, context);
+  await handleContext(endpoints.total("1001"), { get: 6 }, context);
+  await handleContext(endpoints.total("1002"), { get: 4 }, context);
+  await handleContext(endpoints.total("1003"), { get: 7 }, context);
 
-  await handleContext(endpoints.own('1001', '0001'), { get: 1 }, context);
-  await handleContext(endpoints.own('1002', '0001'), { get: 0 }, context);
-  await handleContext(endpoints.own('1003', '0001'), { get: 0 }, context);
+  await handleContext(endpoints.own("1001", "0001"), { get: 1 }, context);
+  await handleContext(endpoints.own("1002", "0001"), { get: 0 }, context);
+  await handleContext(endpoints.own("1003", "0001"), { get: 0 }, context);
 
   // Block external calls
   await context.route(
     (url) => url.href.slice(0, host.length) != host,
     (route) => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -187,20 +187,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -213,7 +213,7 @@ async function handleRaw(match, handlers) {
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -257,10 +257,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
